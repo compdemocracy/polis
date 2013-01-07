b800b6f378a7fbe3795c5ed25c563f49be7c83f4
colinmegill <colinmegill@gmail.com>
2013-01-07 01:32:21 -0800

    first rough sketch of pca visualization... next will add foci, clusters and their representatives

--- surveyD3/uglyalgo.js
+++ surveyD3/uglyalgo.js
@@ -45,6 +45,15 @@ comparisonobject = {
 }
 
 
+
+
+
+
+
+
+
+
+
 Possibility 1: 
 
 CHARGE will define the distance between separate force graphs and be a function of the net difference between 
@@ -55,17 +64,36 @@ ALPHA will be normal
 TICKS will be normal
 GRAVITY will be normal
 
+with regards to our force graph: ... the most basic thing to do is set the link distance (all 4000 of them) based on the number
+	of comments disagreed upon IF THAT NUMBER IS BELOW N, where N is , then make the links disappear. 
+
 
 Possibility 2: 
 
 CHARGE will be normal and just separate collisions between nodes
 LINKS will be ommitted
 ALPHA will move nodes to different points of 
-GRAVITY of which there will be multiple [x,y] coordinates for [calculated on some metric]
+GRAVITY of which there will be multiple [x,y] coordinates for simply separates the two predefined clusters
 TICKS will define the motion of nodes towards those gravity points while alpha has a value.
 
+with regards to slightly smarter clusters, we could do some basic javascript data analysis that would spit out: 
 
 
+thought leader approach: if you look at each individiuals relationsihp to others there will be a curve for each. assume
+	this curve is ordered left to right, lowest disagreement to highest disagreement. the average of the first 50 and the average
+	of the last 50 give you the extremes... compare these numbers to find the 4 people with the largest difference... cluster to them.
+
+
+
+... but the dat a is SPARSE. that has always been the problem.
+
+
+
+this is some code that is a conditional statement within the for loop, and pushes to a new object 
+
+
+this is some set interval code that simulates the nested for loops only looking for the first 10 random columns, then 20 random 
+	different random columns, to simulate comparisons moving through time and how the visualization will update
 
 
 
