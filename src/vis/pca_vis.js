f1a9218aa2cbc903c1d05f8f82ee064fdf1b65e5
Mike Bjorkegren <m@bjorkegren.com>
2013-02-16 12:30:19 -0800

    PcaVis becomes a singleton with addNode method

--- src/vis/pca_vis.js
+++ src/vis/pca_vis.js
@@ -1,124 +1,127 @@
-var tree = Arboreal.parse(survey200, 'children');
 
+var PcaVis = (function(){
 
-//the h and w values should be locked at a 1:2 ratio of h to w
-var h = 450;
-var w = 900;
+    //the h and w values should be locked at a 1:2 ratio of h to w
+    var h = 450;
+    var w = 900;
+    var nodes = [];
+    var visualization;
+    var force;
 
-function setupVis(){
+    function initialize() {
+        //create svg, appended to a div with the id #visualization_div, w and h values to be computed by jquery later
+        //to connect viz to responsive layout if desired
+        visualization = d3.select('#visualization_div').append('svg')
+                      .attr('width', w)
+                      .attr('height', h)
+                      .attr('class', 'visualization');
 
-//create svg, appended to a div with the id #visualization_div, w and h values to be computed by jquery later
-//to connect viz to responsive layout if desired
-var visualization = d3.select('#visualization_div').append('svg')
-              .attr('width', w)
-              .attr('height', h)
-              .attr('class', 'visualization');
+        setupOverlays();
+        setupPlot();
+    }
 
-// setup overlays
-(function() {
+    function setupOverlays() {
 
     //add four directional arrows, scalable on resize of parent container which must be a square to preserve dimensions of viz.
     visualization.append('line')
-            .attr('x1', w * 0.5)
-            .attr('y1', h * 0.25)
-            .attr('x2', w * 0.5)
-            .attr('y2', 15)
-            .attr('id', 'toparrow')
-            .attr('marker-end', "url(#Triangle)");
+        .attr('x1', w * 0.5)
+        .attr('y1', h * 0.25)
+        .attr('x2', w * 0.5)
+        .attr('y2', 15)
+        .attr('id', 'toparrow')
+        .attr('marker-end', "url(#Triangle)");
     visualization.append('line')
-            .attr('x1', w * 0.75)
-            .attr('y1', h * 0.5)
-            .attr('x2', w - 15)
-            .attr('y2', h * 0.5)
-            .attr('id', 'rightarrow')
-            .attr('marker-end', "url(#Triangle)");
+        .attr('x1', w * 0.75)
+        .attr('y1', h * 0.5)
+        .attr('x2', w - 15)
+        .attr('y2', h * 0.5)
+        .attr('id', 'rightarrow')
+        .attr('marker-end', "url(#Triangle)");
     visualization.append('line')
-            .attr('x1', w * 0.25)
-            .attr('y1', h * 0.5)
-            .attr('x2', 15)
-            .attr('y2', h * 0.5)
-            .attr('id', 'leftarrow')
-            .attr('marker-end', "url(#Triangle)");
+        .attr('x1', w * 0.25)
+        .attr('y1', h * 0.5)
+        .attr('x2', 15)
+        .attr('y2', h * 0.5)
+        .attr('id', 'leftarrow')
+        .attr('marker-end', "url(#Triangle)");
     visualization.append('line')
-            .attr('x1', w * 0.5)
-            .attr('y1', h * 0.75)
-            .attr('x2', w * 0.5)
-            .attr('y2', h - 15)
-            .attr('id', 'bottomarrow')
-            .attr('marker-end', "url(#Triangle)");
+        .attr('x1', w * 0.5)
+        .attr('y1', h * 0.75)
+        .attr('x2', w * 0.5)
+        .attr('y2', h - 15)
+        .attr('id', 'bottomarrow')
+        .attr('marker-end', "url(#Triangle)");
     // add the center circle
     visualization.append('circle')
-            .attr('cx', w * 0.5)
-            .attr('cy', h * 0.5)
-            .attr('r', 7)
-            .attr('id', 'centercircle');
+        .attr('cx', w * 0.5)
+        .attr('cy', h * 0.5)
+        .attr('r', 7)
+        .attr('id', 'centercircle');
     // add four hover circles on lines
     visualization.append('circle')
-            .attr('cx', w * 0.5)
-            .attr('cy', h * 0.1)
-            .attr('r', 10)
-            .attr('id', 'top_circle');
+        .attr('cx', w * 0.5)
+        .attr('cy', h * 0.1)
+        .attr('r', 10)
+        .attr('id', 'top_circle');
     visualization.append('circle')
-            .attr('cx', w * 0.9)
-            .attr('cy', h * 0.5)
-            .attr('r', 10)
-            .attr('id', 'right_circle');
+        .attr('cx', w * 0.9)
+        .attr('cy', h * 0.5)
+        .attr('r', 10)
+        .attr('id', 'right_circle');
     visualization.append('circle')
-            .attr('cx', w * 0.5)
-            .attr('cy', h * 0.9)
-            .attr('r', 10)
-            .attr('id', 'bottom_circle');
+        .attr('cx', w * 0.5)
+        .attr('cy', h * 0.9)
+        .attr('r', 10)
+        .attr('id', 'bottom_circle');
     visualization.append('circle')
-            .attr('cx', w * 0.1)
-            .attr('cy', h * 0.5)
-            .attr('r', 10)
-            .attr('id', 'left_circle');
+        .attr('cx', w * 0.1)
+        .attr('cy', h * 0.5)
+        .attr('r', 10)
+        .attr('id', 'left_circle');
     // add four boxes that will come up on hover to show PILLARS which will show comments with 
     // most PCA SUPPORT both positively and negatively
 
 
 
 
-      $('#toparrow').hover(function(){ 
-          $('.visualization').addClass('.toparrow_hover');
-        }, function() { 
+    $('#toparrow').hover(function(){ 
+        $('.visualization').addClass('.toparrow_hover');
+    }, function() { 
+    });
 
-        });
+    $('#centercircle').hover(function(){});
 
-      $('#centercircle').hover(function(){});
+    //make this more robust using conditionals to make sure the class isn't misapplied:
 
-      //make this more robust using conditionals to make sure the class isn't misapplied:
-      
-      $('#centercircle').click(function(){
+    $('#centercircle').click(function(){
         $('#centercircle_content').removeClass('hidden'); 
-      });
+    });
 
-      $('#centercircle_content, #top_pillar, #right_pillar, #bottom_pillar, #left_pillar').click(function(){
+    $('#centercircle_content, #top_pillar, #right_pillar, #bottom_pillar, #left_pillar').click(function(){
         $(this).addClass('hidden');
-      });
+    });
 
-      $('#top_circle').click(function(){
+    $('#top_circle').click(function(){
         $('#top_pillar').toggleClass('hidden');
-      });
-      $('#right_circle').click(function(){
+    });
+    $('#right_circle').click(function(){
         $('#right_pillar').toggleClass('hidden');
-      });
-      $('#bottom_circle').click(function(){
+    });
+    $('#bottom_circle').click(function(){
         $('#bottom_pillar').toggleClass('hidden');
-      });
-      $('#left_circle').click(function(){
+    });
+    $('#left_circle').click(function(){
         $('#left_pillar').toggleClass('hidden');
-      });
+    });
 
-}()); // End setup overlays
+} // End setup overlays
 
 
 // pca plotting setup
-(function() {
+function setupPlot() {
 
-    var nodes = [];
 
-    var force = d3.layout.force()
+    force = d3.layout.force()
         .nodes(nodes)
         .links([])
         .gravity(0)
@@ -146,57 +149,68 @@ var visualization = d3.select('#visualization_div').append('svg')
     });
 
 
-    function hashCode(s){
-        var hash = 0,
-            i,
-            char;
-        if (s.length === 0) {
-            return hash;
-        }
-        for (i = 0; i < s.length; i++) {
-            char = s.charCodeAt(i);
-            hash = ((hash<<5)-hash)+char;
-            hash = hash & hash; // Convert to 32bit integer
-        }
+
+} // end setupPlot
+
+
+function hashCode(s){
+    var hash = 0,
+        i,
+        char;
+    if (s.length === 0) {
         return hash;
     }
+    for (i = 0; i < s.length; i++) {
+        char = s.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
 
-    var colorFromString = _.compose(d3.scale.category20(), function(s) {
-        return hashCode(s) % 20;
-    });
+var colorFromString = _.compose(d3.scale.category20(), function(s) {
+    return hashCode(s) % 20;
+});
 
-    //principlecomponents
+function addNode(node) {
+    nodes.push(node);
+  force.start();
 
-    setInterval(function(){
-      if  (dataFromPca.length === 0) {
-        return;
-      }
-      nodes.push(dataFromPca.shift());
-      force.start();
+  visualization.selectAll("circle.node")
+      .data(nodes)
+    .enter().append("svg:circle")
+      .attr("class", "node")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", 8)
+      .style("fill", function(d) {
+            if (d.children && d.children.length) {
+                // only render leaves - may change? render large transucent circles? 
+                return "rgba(0,0,0,0)";
+            }
+            var color = colorFromString(d.data && d.data.meta && d.data.meta.country || "");
+            if (!color) {
+                console.error(29384723897);
+                return "black";
+            }
+            return color;
+      })
+      .style("stroke-width", 1.5)
+      .call(force.drag);
+}
 
-      visualization.selectAll("circle.node")
-          .data(nodes)
-        .enter().append("svg:circle")
-          .attr("class", "node")
-          .attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; })
-          .attr("r", 8)
-          .style("fill", function(d) {
-                if (d.children && d.children.length) {
-                    // only render leaves - may change? render large transucent circles? 
-                    return "rgba(0,0,0,0)";
-                }
-                var color = colorFromString(d.data && d.data.meta && d.data.meta.country || "");
-                if (!color) {
-                    console.error(29384723897);
-                    return "black";
-                }
-                return color;
-          })
-          .style("stroke-width", 1.5)
-          .call(force.drag);
-    }, 10);
+    return {
+        initialize: initialize,
+        addNode: addNode
+    };
+}());
 
+
+// TODO use almond.js to manage dependencies
+$(document).ready( function() {
+    PcaVis.initialize();
+
+    var tree = Arboreal.parse(survey200, 'children');
 
 
     // Normalize to [-1,1]
@@ -211,8 +225,10 @@ var visualization = d3.select('#visualization_div').append('svg')
 
     var dataFromPca = tree.toArray();
 
-}()); // end pca plot setup
-
-}
-
-$(document).ready(setupVis);
+    setInterval(function(){
+      if  (dataFromPca.length === 0) {
+        return;
+      }
+      PcaVis.addNode(dataFromPca.shift());
+    }, 10);
+});
