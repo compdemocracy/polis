e27702088ad79feab65d6e3f54bbe8722ad737d6
Mike Bjorkegren <m@bjorkegren.com>
2013-02-16 18:21:17 -0800

    Adding polisUtils.js

---
+++ src/lib/js/polisUtils.js
@@ -0,0 +1,19 @@
+
+function isPersonId(x) {
+    return _.isString(x);
+}
+
+
+var bad = (function(errorLogger){ 
+    return function bad(obj, key, optionalChecker) {
+        var ok = obj && obj[key];
+        if (optionalChecker) {
+            ok &= optionalChecker(obj[key]);
+        }
+        if (!ok) {
+            errorLogger("missing or invalid: " + key);
+        }
+        return ok;
+    };
+}());
+
