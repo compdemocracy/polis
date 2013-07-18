0b4452ec84003355cd248bbad696783b6745ffc7
Mike Bjorkegren <m@bjorkegren.com>
2013-07-17 23:39:29 -0700

    cross origin calls for dev environments - TODO revisit

--- server/server.js
+++ server/server.js
@@ -361,6 +361,18 @@ function writeDefaultHead(req, res) {
 
 //app.use(writeDefaultHead);
 app.use(express.logger());
+app.all("/v3/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  return next();
+});
+app.all("/v3/*", function(req, res, next) {
+  if (req.method.toLowerCase() !== "options") {
+    return next();
+  }
+  return res.send(204);
+});
 
 app.get("/v2/math/pca",
     logPath,
