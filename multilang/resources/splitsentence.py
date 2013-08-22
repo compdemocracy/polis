68a0ce3ac53f871e49d9e0424bbdf8bc1bdda32c
forrest <light24bulbs@gmail.com>
2013-08-22 15:05:06 -0700

    added swo to gitignore

--- multilang/resources/splitsentence.py
+++
@@ -1,9 +0,0 @@
-import storm
-
-class SplitSentenceBolt(storm.BasicBolt):
-    def process(self, tup):
-        words = tup.values[0].split(" ")
-        for word in words:
-          storm.emit([word])
-
-SplitSentenceBolt().run()
