1c2103b2b112dc319754a2645fe610dcba06ad88
forrest <light24bulbs@gmail.com>
2013-09-29 15:20:27 -0700

    doseq in pca bolt not splitting by each reaction

--- src/clj/polis_storm/online_pca.clj
+++ src/clj/polis_storm/online_pca.clj
@@ -8,7 +8,7 @@
   (spout
     (nextTuple []
       ;(let [json-data (json/read-str @(read-channel incoming-reactions))]         
-      (let [json-data (get (json/read-str (slurp "data/legalization_conv.json") "reactions"))]
+      (let [json-data (get (json/read-str (slurp "data/legalization_conv.json")) "reactions")]
         (def reactions (partition 3 (apply interleave (vals json-data))))
         (emit-spout! collector [reactions])
        ))
@@ -16,28 +16,28 @@
     ;;error handling here..requires tuple to have id, do research
     )))
 
+
+(defn update-rating-matrix [new-reactions rating-matrix ptpts cmts] 
+  (doseq [reaction new-reactions]
+    (println "the reaction is : " new-reactions)
+    (if (not (some #(= (nth reaction 0) %) @ptpts))
+      (swap! ptpts (conj @ptpts (nth reaction 2))))
+    (if (not (some #(= (nth reaction 1) %) @cmts))
+      (swap! cmts (conj @cmts (nth reaction 1)))))
+  (swap! rating-matrix (map #(into % (repeat (- (count cmts) (count (get rating-matrix 0)) 0)) new-reactions))) 
+  (swap! rating-matrix (into rating-matrix (into [] (repeat (- (count ptpts) (count rating-matrix) ) 0))))
+  (doseq [reaction new-reactions]
+    (let [row (.indexOf cmts (nth reaction 1)) column (.indexOf  (nth reaction 0))]
+      (swap! rating-matrix (replace {row (replace {column (nth reaction 2)} (nth rating-matrix row))} rating-matrix)))))  
+
+
 (defbolt pca ["pca"] {:prepare true}
   [conf context collector]
-  (let [rating-matrix (atom (matrix [[]])) ptpts (atom []) cmts (atom [])])         
+  (let [rating-matrix (atom (matrix [[]])) ptpts (atom []) cmts (atom [])]         
     (bolt
       (execute [tuple]
-        (update-rating-matrix [tuple rating-matrix ptpts cmts])       
-               )))
-
-;does this support multithreading at present?
-(defn update-id-index [new-reactions rating-matrix ptpts cmts] 
-  (doseq [reaction new-reactions]
-    (if (not (some #(= (nth reaction 0) %) ptpts))
-      (swap! ptpts (conj ptpts (nth reaction 0))))
-    (if (not (some #(= (nth reaction 1) %) cmts))
-      (swap! cmts (conj (nth reaction 1) cmts))))
-  (swap! rating-matrix (map #(into % (repeat (- (count cmts) (count (get pca-data 0)) 0)) new-reactions))) 
-  (swap! rating-matrix (into rating-matrix (into [] (repeat (- (count ptpts) (count pca-data) ) 0))))
-  (doseq [reaction new-reactions]
-    (let [row (indexOf. cmts (nth reaction 1)) column (indexOf. ptpts (nth reaction 0))]
-      (replace (nth 
-      
-   
+        (update-rating-matrix tuple rating-matrix ptpts cmts)       
+        (println @rating-matrix)))))
 
 (defn mk-topology [] 
   (topology
@@ -45,9 +45,10 @@
                     "polis.io/whatever-controller-action") 
                     :p 1)}
      
-    {
+    {"2" (bolt-spec {"1" :all}
+                    pca
+                    :p 1)}))
 
-     }))
 
 (defn run-local! []
   (let [cluster (LocalCluster.)]
