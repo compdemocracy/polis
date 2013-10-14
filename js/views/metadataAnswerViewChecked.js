dbf2222079249c94869e816807e68dc48fc5b077
Mike Bjorkegren <m@bjorkegren.com>
2013-10-13 17:46:37 -0700

    metadata answers pre-checked versions

---
+++ js/views/metadataAnswerViewChecked.js
@@ -0,0 +1,14 @@
+define([
+  'templates/metadataAnswerChecked',
+  'views/metadataAnswerView'
+], function (
+  template,
+  MetadataAnswerView
+) {
+
+return MetadataAnswerView.extend({
+  name: 'metadataAnswerViewChecked',
+  template: template,
+});
+
+});
