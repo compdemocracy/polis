b83f72efb1c431560ddfa44ffcbbb7aa06169718
Christopher Small <metasoarous@gmail.com>
2013-10-29 00:47:40 -0700

    Fixed matrix updating functionality

--- src/matrix_utils.clj
+++ src/matrix_utils.clj
@@ -1,24 +1,26 @@
 (ns matrix-utils)
-;(use 'clojure.tools.trace)
 
 
 (defrecord RatingMatrix [ptpts cmts matrix])
 
 
+; This shouldn't have to take an existing rating-matrix for fresh data
 (defn update-rating-matrix [rating-matrix new-reactions]
   "This is a recursive function for taking new reactions and inserting them into the rating matrix."
-  (let [rating-matrix (or rating-matrix (->RatingMatrix [] [] [[]]))]
+  (let [rating-matrix (or rating-matrix (->RatingMatrix [] [] []))]
     ; be careful about first v last wrt spout order; want to make sure the last thing in overrides
     ; anything earlier... If there are new reaactions, proceed
     (if-let [[ptpt cmt rating] (first new-reactions)]
       ; Long series of let assignments leading up to the recursive call
       (let [{:keys [ptpts cmts matrix]} rating-matrix
-            [ptpt-index cmt-index] (map #(.indexOf %1 %2) [ptpts cmts] [ptpt cmt])
+            [ptpt-index cmt-index] (map 
+                                     #(.indexOf %1 %2)
+                                     [ptpts cmts] [ptpt cmt])
             [ptpts cmts] (map (fn [xs x i] (if (= -1 i) (conj xs x) xs))
                               [ptpts cmts] [ptpt cmt] [ptpt-index cmt-index])
             ; If necessary, insert new columns and/or rows of zeros
-            matrix (if (= -1 ptpt-index) (conj matrix (into [] (repeat (count cmts) 0))) matrix)
             matrix (if (= -1 cmt-index) (mapv #(conj % 0) matrix) matrix)
+            matrix (if (> (count ptpts) (count matrix)) (conj matrix (into [] (repeat (count cmts) 0))) matrix)
             ; Get the indices of the matrix position to be updated (note that .indexOf above gives
             ; us -1 if the item is not in the collection)
             indices (mapv (fn [xs i] (if (= -1 i) (- (count xs) 1) i))
@@ -29,12 +31,13 @@
       rating-matrix)))
 
 
-(defn random-reactions [n-ptpts n-cmts n-reactions]
-  (letfn [(generator [wrapper-fn range]
-            (take n-reactions (repeatedly #(wrapper-fn (rand-int range)))))]
-    (mapv list
-      (generator #(str "p" %) n-ptpts)
-      (generator #(str "c" %) n-cmts)
-      (generator #(- % 1) 3))))
+(defn random-reactions [n-ptpts n-cmts & [n-reactions]]
+  (let [n-reactions (or n-reactions (* n-ptpts n-cmts))]
+    (letfn [(generator [wrapper-fn range]
+              (take n-reactions (repeatedly #(wrapper-fn (rand-int range)))))]
+      (mapv list
+        (generator #(str "p" %) n-ptpts)
+        (generator #(str "c" %) n-cmts)
+        (generator #(- % 1) 3)))))
 
 
--- test/matrix_utils_test.clj
+++ test/matrix_utils_test.clj
@@ -24,57 +24,75 @@
 (deftest create-matrix
   (testing "Creation of matrix from scratch"
     (let [rating-matrix (->RatingMatrix [] [] [[]])
-          {:keys [ptpts cmts matrix]} (update-rating-matrix' reactions rating-matrix)]
-      (is (= ptpts (real-rating-matrix :ptpts)))
-      (is (= cmts (real-rating-matrix :cmts)))
-      (is (= matrix (real-rating-matrix :matrix))))))
-
+          {:keys [ptpts cmts matrix]} (update-rating-matrix rating-matrix reactions)]
+      (is (= ptpts (:ptpts real-rating-matrix)))
+      (is (= cmts (:cmts real-rating-matrix)))
+      (is (= matrix (:matrix real-rating-matrix))))))
 
 ; These could all be DRYer, and likely highlight the need to change the way we are
 ; treating the identity of these atoms
+(deftest update-matrix
+  (testing "Updating an existing matrix"
+    (testing "with existing comments/ptpts"
+      (let [rating-matrix (update-rating-matrix
+                            real-rating-matrix
+                            ['("p1" "c3" 1) '("p3" "c3" -1)])]
+        (is (= (:matrix rating-matrix)
+               [[ 0 1  1]
+                [-1 1  1]
+                [-1 0 -1]]))
+        (is (= (:ptpts rating-matrix)
+               (:ptpts real-rating-matrix))
+            "Nothing should change ptpts")
+        (is (= (:cmts rating-matrix)
+               (:cmts real-rating-matrix))
+            "Nothing should change with cmts")))
 
-;(deftest update-matrix
-  ;(testing "Updating an existing matrix"
-    ;(let [rating-matrix (atom real-rating-matrix)
-          ;ptpts (atom real-ptpts)
-          ;cmts (atom real-cmts)]
-      ;(testing "only with existing comments/ptpts"
-        ;(update-rating-matrix
-          ;['("p1" "c3" 1) '("p3" "c3" -1)]
-          ;rating-matrix ptpts cmts)
-        ;(is (= @rating-matrix
-               ;[[ 0 1  1]
-                ;[-1 1  1]
-                ;[-1 0 -1]]))
-        ;(is (= @ptpts real-ptpts) "Nothing should change")
-        ;(is (= @cmts real-cmts) "Nothing should change")))
+    (testing "with new ptpts"
+      (let [rating-matrix (update-rating-matrix
+                            real-rating-matrix
+                            ['("p4" "c3" 1) '("p3" "c3" -1)])]
+        (is (= (:matrix rating-matrix)
+               [[ 0 1  0]
+                [-1 1  1]
+                [-1 0 -1]
+                [ 0 0  1]]))
+        (is (= (:ptpts rating-matrix)
+               ["p1" "p2" "p3" "p4"])
+            "the new participant should be in ptpts")
+        (is (= (:cmts rating-matrix)
+               (:cmts real-rating-matrix))
+            "Nothing should change with cmts")))
 
-    ;(let [rating-matrix (atom real-rating-matrix)
-          ;ptpts (atom real-ptpts)
-          ;cmts (atom real-cmts)]
-      ;(testing "with new ptpts"
-        ;(update-rating-matrix
-          ;['("p4" "c3" 1) '("p3" "c3" -1)]
-          ;rating-matrix ptpts cmts)
-        ;(is (= @rating-matrix
-               ;[[ 0 1  0]
-                ;[-1 1  1]
-                ;[-1 0 -1]
-                ;[ 0 0  1]]))
-        ;(is (= @ptpts ["p1" "p2" "p3" "p4"]) "the new participant should be in ptpts")
-        ;(is (= @cmts real-cmts) "nothing should change")))
+    (testing "with new cmts"
+      (let [rating-matrix (update-rating-matrix
+                            real-rating-matrix
+                            ['("p3" "c4" 1) '("p2" "c3" -1)])]
+        (is (= (:matrix rating-matrix)
+               [[ 0 1  0 0]
+                [-1 1 -1 0]
+                [-1 0  0 1]]))
+        (is (= (:ptpts rating-matrix)
+               (:ptpts real-rating-matrix))
+            "Nothing should change with ptpts")
+        (is (= (:cmts rating-matrix)
+               ["c1" "c2" "c3" "c4"])
+            "the new cmts should be in cmts")))
 
-    ;(let [rating-matrix (atom real-rating-matrix)
-          ;ptpts (atom real-ptpts)
-          ;cmts (atom real-cmts)]
-      ;(testing "with new ptpts"
-        ;(update-rating-matrix
-          ;['("p4" "c3" 1) '("p3" "c3" -1)]
-          ;rating-matrix ptpts cmts)
-        ;(is (= @rating-matrix
-               ;[[ 0 1  0]
-                ;[-1 1  1]
-                ;[-1 0 -1]
-                ;[ 0 0  1]]))
-        ;(is (= @ptpts ["p1" "p2" "p3" "p4"]) "the new participant should be in ptpts")
-        ;(is (= @cmts real-cmts) "nothing should change")))))
+    (testing "with new cmts and ptpts"
+      (let [rating-matrix (update-rating-matrix
+                            real-rating-matrix
+                            ['("p4" "c3" 1) '("p3" "c4" -1)])]
+        (is (= (:matrix rating-matrix)
+               [[ 0 1  0  0]
+                [-1 1  1  0]
+                [-1 0  0 -1]
+                [ 0 0  1  0]]))
+        (is (= (:ptpts rating-matrix)
+               ["p1" "p2" "p3" "p4"])
+            "the new participant should be in ptpts")
+        (is (= (:cmts rating-matrix)
+               ["c1" "c2" "c3" "c4"])
+            "there new participants should be in ptpts")))))
+
+
