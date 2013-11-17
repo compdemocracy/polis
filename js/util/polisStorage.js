b8dc38eac06cba72be749932169520e31fa33766
Mike Bjorkegren <m@bjorkegren.com>
2013-11-16 19:00:36 -0800

    Migrate from localStorage to cookies
    IE 10 can get into a mode where it throws exceptions when you try to
    reference localStorage.
    
    Cookies work though.
    Downside is that they get sent to the server repeatedly, so we have to
    avoid storing lots of stuff here.
    
    http://stackoverflow.com/questions/13102116/access-denied-for-localstorage-in-ie10
    https://github.com/meteor/meteor/issues/1291

--- js/util/polisStorage.js
+++ js/util/polisStorage.js
@@ -1,10 +1,76 @@
 define([], function() {
+
+
+var store = (function() {
+    // Using cookies because IE can have crazy security settings that make localStorage off-limits.
+    // We may want
+
+    //http://stackoverflow.com/questions/19189785/is-there-a-good-cookie-library-for-javascript
+    function getCookie(sName)
+    {
+        sName = sName.toLowerCase();
+        var oCrumbles = document.cookie.split(';');
+        for(var i=0; i<oCrumbles.length;i++)
+        {
+            var oPair= oCrumbles[i].split('=');
+            var sKey = oPair[0].trim().toLowerCase();
+            var sValue = oPair.length>1?oPair[1]:'';
+            if(sKey == sName)
+                return decodeURIComponent(sValue);
+        }
+        return '';
+    }
+
+    function setCookie(sName,sValue)
+    {
+        var oDate = new Date();
+        oDate.setYear(oDate.getFullYear()+1);
+        var sCookie = encodeURIComponent(sName) + '=' + encodeURIComponent(sValue) + ';expires=' + oDate.toGMTString() + ';path=/';
+        document.cookie= sCookie;
+    }
+
+    function clearCookie(sName)
+    {
+        setCookie(sName,'');
+    }
+    // We might want to use localStorage for browsers that don't throw exceptions when you try to use their localStorage implementation.
+    // return {
+    //     set: localStorage.setItem,
+    //     get: localStorage.getItem,
+    //     clear: localStorage.clear
+    // };
+    return {
+        set: setCookie,
+        get: function(key) {
+            var cookieVal = getCookie(key);
+            if (!cookieVal.length) {
+                // Beta Migration
+                // Should be OK to remove this block (and simply call getCookie) sometime early 2014                
+                try {
+                    // Initially we used localStorage, but switched to cookies because of IE10 localStorage exceptions for certain security configurations.
+                    // This is here for now so existing user sessions will keep working.
+                    // previously, localStorage keys were prefixed with "p_", removing now to minimize extra cookie traffic.
+                    var lsVal = localStorage.getItem("p_" + key);
+                    if (lsVal !== null) {
+                        setCookie(key, lsVal)
+                    }
+                    return lsVal;
+                } catch(e) {
+                    // probably IE with localStorage disabled, nothing to migrate here anyway.
+                }
+            }
+            return cookieVal;
+        },
+        clear: clearCookie,
+    };
+}());
+
     var cache = {
     };
     function saveAndCache(k, v, temporary) {
         if (cache[k] !== v) {
             if (!temporary) {
-                window.localStorage.setItem(k, v);
+                store.set(k, v);
             }
             cache[k] = v;
         }
@@ -14,14 +80,14 @@ define([], function() {
         if (cached) {
             return cached;
         } else {
-            var v = window.localStorage.getItem(k);
+            var v = store.get(k);
             cache[k] = v;
             return v;
         }
     }
     function clear(k) {
         delete cache[k];
-        window.localStorage.clear(k);
+        store.clear(k);
     }
 
     function makeAccessor(k) {
@@ -41,11 +107,11 @@ define([], function() {
     function makeMapAccessor(accessor) {
         var oldGet = accessor.get;
         accessor.get = function(key) {
-            return (JSON.parse(oldGet()) || {})[key];
+            return ( oldGet() && JSON.parse(oldGet()) || {})[key];
         };
         var oldSet = accessor.set;
         accessor.set = function(key, val, temporary) {
-            var o = JSON.parse(oldGet()) || {};
+            var o = oldGet() && JSON.parse(oldGet()) || {};
             o[key] = val;
             oldSet(JSON.stringify(o), temporary);
         };
@@ -70,13 +136,13 @@ define([], function() {
 
     var x = {
         clearAll: clearAll,
-        comments: makeAccessor('p_comments'), // TODO use a real db
-        reactionsByMe: makeAccessor('p_reactions_by_me'), // TODO use a real db
-        email: makeAccessor('p_email'),
-        username: makeAccessor('p_username'),
-        uid: makeAccessor('p_uid'),
-        pids: makeMapAccessor(makeAccessor('p_pids')),
-        token: makeAccessor('p_authToken')
+        //comments: makeAccessor('p_comments'), // TODO use a real db
+        //reactionsByMe: makeAccessor('p_reactions_by_me'), // TODO use a real db
+        email: makeAccessor('email'),
+        //username: makeAccessor('p_username'),
+        uid: makeAccessor('uid'),
+        pids: makeMapAccessor(makeAccessor('pids')),
+        //token: makeAccessor('p_authToken')
     };
     return x;
 }); //();
