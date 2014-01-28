d08dd5b16b4acf43fdc84a281dc1425a0b544f87
Christopher Small <metasoarous@gmail.com>
2014-01-27 22:14:20 -0800

    Up to date project.clj and .gitignore from 7-clojure-poller

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject polis-storm "0.1.0-SNAPSHOT"
   :source-paths ["src/"
-                 ".lein-git-deps/monger/src/"]
+                 ".lein-git-deps/debug-repl/src/"]
   ;:aot :all
   ; faster run time in exchange for slower startup time
   :jvm-opts ^:replace []
@@ -8,13 +8,15 @@
   :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]
             [lein-environ "0.4.0"]]
   :git-dependencies [["https://github.com/Prismatic/hiphip.git" "master"]
-                     ["alex-and-georges.debug-repl" "master"]]
+                     ["https://github.com/GeorgeJahad/debug-repl.git" "master"]]
   :dependencies [[commons-collections/commons-collections "3.2.1"]
                  [aleph "0.3.0-rc2"]
                  [org.clojure/data.json "0.2.2"]
                  [lamina "0.5.0"]
                  [gloss "0.2.2"]
-                 ;[prismatic/hiphip "0.1.0"]
+                 [net.mikera/core.matrix "0.19.0"]
+                 [net.mikera/core.matrix.stats "0.3.0"]
+                 [net.mikera/vectorz-clj "0.19.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [storm "0.8.2"]
