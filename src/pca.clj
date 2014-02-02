a220921b51cd67ad377162c49225ad9251981d5d
Christopher Small <metasoarous@gmail.com>
2014-02-01 17:10:08 -0800

    PCA code switched over to core.matrix; tests passing

--- src/pca.clj
+++ src/pca.clj
@@ -1,30 +1,20 @@
-(ns pca)
+(ns pca
+  (:refer-clojure :exclude [* - + == /])
+  (:use utils)
+  (:use clojure.core.matrix)
+  (:use clojure.core.matrix.stats)
+  (:use clojure.core.matrix.operators)
+  (:use utils matrix-utils))
+
+(set-current-implementation :vectorz)
+
 
 (set! *unchecked-math* true)
-
-(use 'utils 'matrix-utils 'criterium.core)
-(require '(incanter
-            [core :as ic.core]
-            [stats :as ic.stats]
-            [datasets :as ic.data]
-            [io :as ic.io])
-         ;['hiphip.double :as 'dbl]
-         ['clojure.tools.trace :as 'tr])
-
-
-(defn dot [^doubles xs ^doubles ys]
-  "Returns the dot (or Euclidean inner) product of vectors xs and ys"
-  (ic.core/sum (ic.core/mult xs ys)))
-
-
-(defn norm [xs]
-  "Returns the norm (length) of vector xs"
-  (Math/sqrt (apply + (map #(Math/pow % 2) xs))))
 
 
 (defn repeatv [n x]
   "Utility function for making a vector of length n composed entirely of x"
-  (into [] (repeat n x)))
+  (matrix (into [] (repeat n x))))
 
 
 ; Should maybe hide this inside power-iteration and have that function wrap it's current
@@ -32,13 +22,12 @@
 (defn xtxr [data start-vec]
   "Will need to rename this and some of the inner variables to be easier to read...
   Computes an inner step of the power-iteration process"
-  (let [n-rows (count (first data))
-        curr-vec (ic.core/trans (repeatv n-rows 0))]
-    (loop [data data curr-vec curr-vec]
+  (let [n-cols (dimension-count data 1)
+        curr-vec (transpose (repeatv n-cols 0))]
+    (loop [data (rows data) curr-vec curr-vec]
       (if-let [row (first data)]
         (recur (rest data)
-               (ic.core/plus (tr/trace curr-vec)
-                             (tr/trace (ic.core/mult (dot row start-vec) row))))
+               (+ curr-vec (* (inner-product start-vec row) row)))
         curr-vec))))
 
 
@@ -50,11 +39,13 @@
         n-cols (count (first data))
         start-vector (or start-vector (repeatv n-cols 1))
         ; XXX - this add extra cols to the start vector if we have new comments... should test
-        start-vector (into [] (concat start-vector (repeatv (- n-cols (count start-vector)) 1)))]
+        start-vector (matrix
+                       (concat start-vector
+                               (repeatv (- n-cols (dimension-count start-vector 0)) 1)))]
     (loop [iters iters start-vector start-vector last-eigval 0]
       (let [product-vector (xtxr data start-vector)
-            eigval (norm product-vector)
-            normed (ic.core/div product-vector eigval)]
+            eigval (length product-vector)
+            normed (normalise product-vector)]
         (if (or (= iters 0) (= eigval last-eigval))
           normed
           (recur (dec iters) normed eigval))))))
@@ -63,24 +54,17 @@
 (defn proj-vec [xs ys]
   "This computes the projection of ys orthogonally onto the vector spanned by xs"
   (let [coeff (/ (dot xs ys) (dot xs xs))]
-    (ic.core/mult coeff xs)))
+    (* coeff xs)))
 
 
 (defn factor-matrix [data xs]
   "As in the Gram-Shmidt process; we can 'factor out' the vector xs from all the vectors in data,
   such that there is no remaining variance in the xs direction within the data."
-  (ic.core/matrix (mapv #(ic.core/minus % (proj-vec xs %)) data)))
-
-
-(defn mean-vector [data] 
-  (if (= (first (ic.core/dim data)) 1)
-    (ic.core/matrix data)
-    (ic.core/matrix (mapv ic.stats/mean (zip (tr/trace data))))))
+  (matrix (mapv #(- % (proj-vec xs %)) data)))
 
 
 (defn centered-data [data]
-  (let [data-cntr (ic.core/trans (mean-vector data))]
-    (mapv #(ic.core/minus % data-cntr) data)))
+  (- data (mean data)))
 
 
 ; Will eventually also want to add last-pcs
@@ -89,7 +73,7 @@
   power-iteration"
   (let [cntrd-data (centered-data data)
         start-vectors (or start-vectors [])
-        data-dim (apply min (ic.core/dim cntrd-data))]
+        data-dim (min (row-count cntrd-data) (column-count cntrd-data))]
     (loop [data' cntrd-data n-comps' (min n-comps data-dim) pcs [] start-vectors start-vectors]
       (let [pc (power-iteration data' iters (first start-vectors)) ; may eventually want to return eigenvals...
             pcs (conj pcs pc)]
@@ -109,9 +93,6 @@
 (defn pca-project [data pcs]
   "Apply the principal component projection specified by pcs to the data"
   ; Here we map each row of data to it's projection
-  (mapv
-    ; each projection is the map of the pcs to the dot of a pc and the data-row
-    (fn [data-row] (mapv #(dot data-row %) pcs))
-    data))
+  (mmul data (transpose pcs)))
 
 
