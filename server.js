f2d6600b8d5f0a9d0766bd4bf3df556a9d224735
Mike Bjorkegren <m@bjorkegren.com>
2014-02-04 13:12:35 -0800

    stringify cookies

--- server.js
+++ server.js
@@ -991,14 +991,14 @@ function getUidByEmail(email, callback) {
 
 
 function clearCookies(req, res) {
-    console.log("before clear req: " + req.cookies);
-    console.log("before clear res: " + res.cookies);
+    console.log("before clear req: " + JSON.stringify(req.cookies));
+    console.log("before clear res: " + JSON.stringify(res.cookies));
     res.cookies = {};
     res.signedCookies = {};
     // for (var cookieName in req.cookies) {
         // res.clearCookie(cookieName, {path: "/"});
     // }
-    console.log("after clear res: " + res.cookies);
+    console.log("after clear res: " + JSON.stringify(res.cookies));
     // cookieNames.forEach(function(name) {
     //     res.clearCookie(name, {path: "/"});
     // });
