3c5880a4ac6d132e94f36ae2bce8cfc3558d7283
Mike Bjorkegren <m@bjorkegren.com>
2014-02-06 20:13:20 -0800

    gulp shim fixes

--- gulpfile.js
+++ gulpfile.js
@@ -23,68 +23,70 @@ gulp.task('scripts', function() {
         // 'd3': '../bower_components/d3/d3',
         // 'r2d3': '../bower_components/r2d3/r2d3',
 
-            jquery: {
-              path: "lib/jquery", // Note: these jquery files were generated from this fork: https://github.com/mbjorkegren/jquery/tree/1.9-callbacks-array-fix
-              exports: "$",
-            },
+        // no need to shim jQuery
+
+            // jquery: {
+            //   path: "lib/jquery", // Note: these jquery files were generated from this fork: https://github.com/mbjorkegren/jquery/tree/1.9-callbacks-array-fix
+            //   exports: "$",
+            // },
             originalhandlebars: {
-              path : '../bower_components/handlebars/handlebars.runtime', //original handlebars
+              path : 'bower_components/handlebars/handlebars.runtime.js', //original handlebars
               exports: 'Handlebars',
             },
             originalbackbone: {
-              path: '../bower_components/backbone/backbone', // backbone before modifications
+              path: 'bower_components/backbone/backbone', // backbone before modifications
               depends: { jquery: '$', underscore: '_' },  
               exports: 'Backbone',
             },
             backbone: {
-              path: 'net/backbonePolis', // polis-specific backbone modifications
+              path: 'js/net/backbonePolis', // polis-specific backbone modifications
               depends: { originalbackbone: "Backbone" },
               exports: "Backbone",
             },
             underscore: {
-              path: '../bower_components/underscore/underscore',
+              path: 'bower_components/underscore/underscore',
               exports: '_',
             },
             thorax: {
-              path: '../bower_components/thorax/thorax',
+              path: 'bower_components/thorax/thorax',
               exports: 'Thorax',
               depends: { handlebars: 'Handlebars', backbone: 'Backbone' },             
             },
             bootstrap_alert: {  //all bootstrap files need to be added to the dependency array of js/main.js
-              path: '../bower_components/bootstrap/js/alert',
+              path: 'bower_components/bootstrap/js/alert',
             },
             bootstrap_tab: {
-              path : '../bower_components/bootstrap/js/tab',
+              path : 'bower_components/bootstrap/js/tab',
             },
             bootstrap_popover: {
-              path: '../bower_components/bootstrap/js/popover',
+              path: 'bower_components/bootstrap/js/popover',
             },
             bootstrap_collapse: {
-              path: '../bower_components/bootstrap/js/collapse',
+              path: 'bower_components/bootstrap/js/collapse',
             },
             bootstrap_dropdown: {
-              path: '../bower_components/bootstrap/js/dropdown',
+              path: 'bower_components/bootstrap/js/dropdown',
             },
             bootstrap_affix: {
-              path: '../bower_components/bootstrap/js/affix',
+              path: 'bower_components/bootstrap/js/affix',
             },
-            d3tooltips {
-              path: '../bower_components/d3-tip/index',
+            d3tooltips: {
+              path: 'bower_components/d3-tip/index',
             },
             bootstrap_tooltip: {
-              path: '../bower_components/bootstrap/js/tooltip',
+              path: 'bower_components/bootstrap/js/tooltip',
             },
             bootstrap_button: { 
-              path: '../bower_components/bootstrap/js/button',
+              path: 'bower_components/bootstrap/js/button',
             },
             bootstrap_transition: {
-              path: '../bower_components/bootstrap/js/transition',
+              path: 'bower_components/bootstrap/js/transition',
             },
             polis: {
-              path: 'lib/polis',
+              path: 'js/lib/polis',
             },
             VisView: {
-              path: 'lib/VisView',
+              path: 'js/lib/VisView',
               depends: { d3tooltips: 'foo' }, // added to d3 object as d3.tip
               exports: 'VisView',
             },
