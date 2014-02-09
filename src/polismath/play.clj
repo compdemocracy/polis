3f7ef7ad0c5013c96b54a50ddccebec80875668b
Christopher Small <metasoarous@gmail.com>
2014-02-09 15:06:06 -0800

    Updated play namespace with some smaller examples

--- src/polismath/play.clj
+++ src/polismath/play.clj
@@ -2,40 +2,29 @@
   (:use clojure.data.csv
         clojure.java.io
         criterium.core
+        polismath.conversation
         polismath.pca
         polismath.utils
-        polismath.matrix-utils
+        polismath.named-matrix
         polismath.simulation))
 
-(require '(incanter
-            [core :as ic.core]
-            [stats :as ic.stats]
-            [datasets :as ic.data]
-            [io :as ic.io]))
+(def nmat {:rows '[a b c]
+           :cols '[x y z v]
+           :matrix [[1 0  1 -1]
+                    [0 1 -1  1]
+                    [1 1 -1  0]]})
+
+(def conv {:rating-mat nmat})
+
+(def results (small-conv-update {:conv conv :opts {}
+                             :votes [
+                                     {:pid 'a :tid 'x :vote 1}
+                                     {:pid 'b :tid 'q :vote 1}
+                                     {:pid 'd :tid 'y :vote 1}
+                                     ]}))
+
+(println results)
 
 (defn -main []
-  (let [width  100
-        height 50]
-    (println width "x" height)
-    ; Note that either this needs to be updated to account for how random-reactions works now, or
-    ; random-reactions and the reaction generator in the storm spec needs to be updated to be
-    ; compatible with this (The reactions expected below are (ptpt, cmt, reaction) triples; currently
-    ; random-reactions gives (conv, ptpt, cmt, reaction) 4ples)
-    (def reactions (random-reactions width height))
-    (def data (ic.core/matrix (:matrix (update-rating-matrix nil reactions))))
-    (with-open [wrtr (writer "data.csv")]
-      (write-csv wrtr data))
-    )
-  ;(def data (ic.core/sel (ic.core/to-matrix (ic.data/get-dataset :iris)) :cols (range 4)))
-  ;(def data (centered-data data))
-
-  (println "\nComputing powerit-pca")
-  (time (def pi-comps (powerit-pca data 2 :iters 300)))
-  (println "\nComputing incanter pca")
-  (time (def ic-pca (ic.stats/principal-components (ic.core/matrix data))))
-
-  (def pc1 (first pi-comps))
-  (println "\nComputing recentered powerit-pca")
-  (time (powerit-pca data 2 :start-vectors pi-comps))
-)
+  (println "Running main"))
 
