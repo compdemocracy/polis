c58c77643778cf97066b426689c38b5c9861599a
Mike Bjorkegren <m@bjorkegren.com>
2014-02-09 22:32:13 -0800

    cheshire json encoding

--- src/polismath/play.clj
+++ src/polismath/play.clj
@@ -1,6 +1,9 @@
 (ns polismath.play
   (:require [plumbing.core :as plmb]
-            [plumbing.graph :as graph])
+            [plumbing.graph :as graph]
+            [cheshire.core :refer :all]
+            [cheshire.generate :refer [add-encoder encode-seq remove-encoder]]
+            )
   (:use clojure.data.csv
         clojure.java.io
         clojure.pprint
@@ -27,8 +30,20 @@
                                      {:pid 'b :tid 'q :vote 1}
                                      {:pid 'd :tid 'y :vote 1}
                                      ]}))
-
 (pprint results)
+
+(add-encoder mikera.vectorz.Vector
+             (fn [v jsonGenerator]
+               (encode-seq (into-array v) jsonGenerator)))
+
+; CAREFUL - make sure we don't lose dimentions of the N-Dimensional array.
+(add-encoder clojure.core.matrix.impl.ndarray.NDArray
+             (fn [v jsonGenerator]
+               (encode-seq (into-array v) jsonGenerator)))
+
+
+(pprint (generate-string results))
+
 
 (defn -main []
   (println "Running main"))
