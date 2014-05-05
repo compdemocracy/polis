07f7c7d6fdd04f39794a2e14a2f8b797f1792292
Mike Bjorkegren <m@bjorkegren.com>
2014-05-05 12:49:37 -0700

    checkbox for each comment in power view

--- js/views/analyze-comment.js
+++ js/views/analyze-comment.js
@@ -6,9 +6,17 @@ var CommentModel = require("../models/comment");
 module.exports = Handlebones.ModelView.extend({
     name: "analyze-comment-view",
     template: template,
+    context: function() {
+      var ctx = Handlebones.ModelView.prototype.context.apply(this, arguments);
+      ctx.showCheckbox = true;
+      return ctx;
+    },
     events: {
       click: function() {
         eb.trigger(eb.commentSelected, this.model.get("tid"));
+      },
+      "click .powerCheckbox": function() {
+        this.model.set("unchecked", !this.model.get("unchecked"));
       },
       mouseover: function() {
         this.$(".query_result_item").addClass("hover");
