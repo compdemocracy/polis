0ccc2d03046f75641e67fda06f599257fc09091e
Mike Bjorkegren <m@bjorkegren.com>
2014-05-05 12:55:00 -0700

    summary route

--- server.js
+++ server.js
@@ -3798,6 +3798,7 @@ var fetchIndex = function(req, res) {
 
 app.get(/^\/[0-9]+.*/, fetchIndex); // conversation view
 app.get(/^\/power\/[0-9]+.*/, fetchIndex); // power view
+app.get(/^\/summary\/[0-9]+.*/, fetchIndex); // summary view
 app.get(/^\/ot\/[0-9]+.*/, fetchIndex); // conversation view, one-time url
 // TODO consider putting static files on /static, and then using a catch-all to serve the index.
 app.get(/^\/conversation\/create.*/, fetchIndex);
