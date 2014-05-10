b83fe3340608a9cdd03d9404e12a8203ed0ea0fb
Christopher T Small <metasoarous@gmail.com>
2014-05-10 15:36:54 -0700

    Indentation

--- src/polismath/metrics.clj
+++ src/polismath/metrics.clj
@@ -1,13 +1,16 @@
 (ns polismath.metrics)
 
+
 (defn make-socket 
 	([] (new java.net.DatagramSocket))
 	([port] (new java.net.DatagramSocket port)))
+
 
 (defn send-data [send-socket ip port data]
   (let [ipaddress (java.net.InetAddress/getByName ip),
         send-packet (new java.net.DatagramPacket (.getBytes data) (.length data) ipaddress port)]
   (.send send-socket send-packet)))
+
 
 (defn receive-data [receive-socket]
   (let [receive-data (byte-array 1024),
@@ -15,19 +18,21 @@
   (.receive receive-socket receive-packet)
   (new java.lang.String (.getData receive-packet) 0 (.getLength receive-packet))))
 
+
 (defn make-receive [receive-port]
 	(let [receive-socket (make-socket receive-port)]
 		(fn [] (receive-data receive-socket))))
 
+
 (defn make-send [hostname port]
 	(let [send-socket (make-socket)]
-          (fn [data]
-            (println "sending " data " to " hostname ":" port)
-            (send-data send-socket hostname port data))))
+    (fn [data]
+      (println "sending " data " to " hostname ":" port)
+      (send-data send-socket hostname port data))))
+
 
 (defn make-metric-sender [hostname port carbon-api-key]
-  (let
-      [dosend (make-send hostname port)]
+  (let [dosend (make-send hostname port)]
     (fn
       ([name value optionalTimestampMillis]
          (let [s (str carbon-api-key "." name " " value " " (long (/ optionalTimestampMillis 1000)) "\n")]
@@ -36,11 +41,6 @@
       ([name value]
          (let [s (str carbon-api-key "." name " " value "\n")]
            (println s)
-           (dosend s))
-         )
-      )
-    )
-  )
-
+           (dosend s))))))
 
 
