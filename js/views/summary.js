d09880790c41bdd9385f96899dfe50e9a8a6067d
Mike Bjorkegren <m@bjorkegren.com>
2014-05-16 14:50:48 -0700

    remove vis from summary

--- js/views/summary.js
+++ js/views/summary.js
@@ -18,11 +18,8 @@ var template = require('../tmpl/summary');
 var TutorialController = require("../controllers/tutorialController");
 var UserModel = require("../models/user");
 var Utils = require("../util/utils");
-var VisView = require("../lib/VisView");
 var VoteModel = require("../models/vote");
 var VotesCollection = require("../collections/votes");
-
-var VIS_SELECTOR = "#visualization_div";
 
 var isIE8 = navigator.userAgent.match(/MSIE [89]/);
 
@@ -52,28 +49,14 @@ module.exports =  Handlebones.ModelView.extend({
   onClusterTapped : function(gid) {
     this.destroyPopovers();
     var that = this;
-      // if (window.isMobile()) {
-      //    window.scrollTo(0, $("#visualization_div").offset().top);
-      // }
   },  
   onAnalyzeTabPopulated: function() {
     $('.query_result_item').first().trigger('click');
-  },
-  hideVis: function() {
-    $("#visualization_div").hide();
-  },
-  showVis: function() {
-    $("#visualization_div").show();
   },
   allowMetadataFiltering: function() {
     return true;
   },
 
-  emphasizeParticipants: function() {
-    if (this.vis) {
-      this.vis.emphasizeParticipants.apply(this, arguments);
-    }
-  },
   initialize: function(options) {
     Handlebones.ModelView.prototype.initialize.apply(this, arguments);
     var that = this;
@@ -81,7 +64,6 @@ module.exports =  Handlebones.ModelView.extend({
     var pid = this.pid = options.pid;
     var zinvite = this.zinvite = this.model.get("zinvite");
     var is_public = this.model.get("is_public");
-    var vis;
 
     function doFetch(o) {
       var thatCollection = this;
@@ -236,7 +218,6 @@ module.exports =  Handlebones.ModelView.extend({
         if (that.allowMetadataFiltering()) {
           console.log(enabledAnswers);
           serverClient.queryParticipantsByMetadata(enabledAnswers).then(
-            that.emphasizeParticipants.bind(that),
             function(err) {
               console.error(err);
             });
@@ -258,9 +239,7 @@ module.exports =  Handlebones.ModelView.extend({
     // HTTP PATCH - model.save({patch: true})
 
     function onPersonUpdate() {
-      if (vis) {
-        vis.upsertNode.apply(vis, arguments);
-      }
+
     }
 
 
@@ -272,92 +251,6 @@ module.exports =  Handlebones.ModelView.extend({
       }
     }
 
-
-    function initPcaVis() {
-      if (!Utils.supportsVis()) {
-        // Don't show vis for weird devices (Gingerbread, etc)
-        return;
-      }
-
-      $(VIS_SELECTOR).html("").height(0);
-      // $(VIS_SELECTOR).parent().css("display", "none");
-
-
-
-      var w = $(VIS_SELECTOR).width();
-      if (isIE8) {
-        w = 500;
-        $(VIS_SELECTOR).width(w);
-      }
-      var h = w/2;
-      $(VIS_SELECTOR).height(h);
-      that.serverClient.removePersonUpdateListener(onPersonUpdate);
-      vis = that.vis = new VisView({
-          getPid: function() {
-            if (!_.isId(pid)) {
-              //alert("bad pid: " + pid);
-            }
-            return pid;
-          },
-          isIE8: isIE8,
-          getCommentsForProjection: serverClient.getCommentsForProjection,
-          getCommentsForGroup: serverClient.getCommentsForGroup,
-          getReactionsToComment: serverClient.getReactionsToComment,
-          getPidToBidMapping: serverClient.getPidToBidMapping,
-          w: w,
-          h: h,
-          computeXySpans: Utils.computeXySpans,
-          el_queryResultSelector: ".query_results_div",
-          el: VIS_SELECTOR,
-          el_raphaelSelector: VIS_SELECTOR, //"#raphael_div",
-      });
-
-      that.serverClient.addPersonUpdateListener(onPersonUpdate)
-
-      that.tutorialController.setHandler("blueDot", function(){
-        that.$blueDotPopover = that.$(VIS_SELECTOR).popover({
-          title: "DOTS ARE PEOPLE",
-          content: "Each dot represent one or more people. The blue circle represents you. By reacting to a comment, you have caused your dot to move. As you and other participants react, you will move closer to people who reacted similarly to you, and further from people who reacted differently. <button type='button' id='blueDotPopoverButton' class='btn btn-lg btn-primary' style='display: block; margin-top:20px'> Ok, got it </button>",
-          html: true,
-          trigger: "manual",
-          placement: "bottom"
-        }).popover("show");
-        $('#blueDotPopoverButton').click(function(){
-          that.$blueDotPopover.popover("destroy");
-        });
-      });
-      that.tutorialController.setHandler("shadedGroup", function(){
-        that.$shadedGroupPopover = that.$(VIS_SELECTOR).popover({
-          title: "CLICK ON GROUPS",
-          content: "Shaded areas represent groups. Click on a shaded area to show comments that most represent this group's opinion, and separate this group from the other groups.<button type='button' id='shadedGroupPopoverButton' class='btn btn-lg btn-primary' style='display: block; margin-top:20px'> Ok, got it </button>",
-          html: true, 
-          trigger: "manual",
-          placement: "bottom"
-        }).popover("show");
-        $('#shadedGroupPopoverButton').click(function(){
-          that.$shadedGroupPopover.popover("destroy");
-        });
-      });
-      that.tutorialController.setHandler("analyzePopover", function(){
-        setTimeout(function(){
-          if (!that.$el) {
-            return;
-          }
-          that.$analyzeViewPopover = that.$('.query_results > li').first().popover({
-            title: "COMMENTS FOR THIS GROUP",
-            content: "Clicking on a shaded area brings up the comments that brought this group together: comments that were agreed upon, and comments that were disagreed upon. Click on a comment to see which participants agreed (green/up) and which participants disagreed (red/down) across the whole conversation. Participants who haven't reacted to the selected comment disappear. <button type='button' id='analyzeViewPopoverButton' class='btn btn-lg btn-primary' style='display: block; margin-top:20px'> Ok, got it </button>",
-            html: true,
-            trigger: "manual",
-            placement: "bottom"  
-          });
-          that.$('.query_result_item').first().trigger('click');
-          that.$analyzeViewPopover.popover("show");
-          that.$('#analyzeViewPopoverButton').click(function(){
-            that.$analyzeViewPopover.popover("destroy");
-          })      
-        },1500)
-      }) 
-    }  
 
     // just a quick hack for now.
     // we may need to look into something more general
@@ -382,9 +275,7 @@ module.exports =  Handlebones.ModelView.extend({
       }));
 
       eb.on(eb.commentSelected, function(tid) {
-        if (vis) {
-          vis.selectComment(tid);
-        }
+ 
       });
 
       // this.votesByMe.on("all", function(x) {
@@ -429,30 +320,9 @@ module.exports =  Handlebones.ModelView.extend({
 
 
       scrollTopOnFirstShow();
-
-      // $("#visualization_div").affix({
-      //   offset: {
-      //     top: 150 //will be set dynamically
-      //   }
-      // });
-      function deselectHulls() {
-        if (vis) {
-          vis.deselect();
-        }
-      }      
+ 
       configureGutters();
-      if (isIE8) {
-        // Can't listen to the "resize" event since IE8 fires a resize event whenever a DOM element changes size.
-        // http://stackoverflow.com/questions/1852751/window-resize-event-firing-in-internet-explorer
-       setTimeout(initPcaVis, 10); // give other UI elements a chance to load
-        // document.body.onresize = _.debounce(initPcaVis, 1000)
-      } else {
-        setTimeout(initPcaVis, 10); // give other UI elements a chance to load        
-        $(window).resize(_.debounce(function() {
-          configureGutters();
-          initPcaVis();
-        }, 100));
-      }
+
 
 
 
