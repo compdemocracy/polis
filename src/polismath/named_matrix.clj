9a65324eb969fbd1def72915172603640a4fac8d
Christopher Small <metasoarous@gmail.com>
2014-05-19 00:30:08 -0700

    Bottom up rewrite of named-matrix for performance using smarter types

--- src/polismath/named_matrix.clj
+++ src/polismath/named_matrix.clj
@@ -1,74 +1,160 @@
 (ns polismath.named-matrix
-  (:require [clojure.core.matrix :as matrix])
+  (:require [clojure.core.matrix :as cm]
+            [taoensso.timbre.profiling :as profiling
+             :refer (pspy pspy* profile defnp p p*)])
   (:use polismath.utils))
 
 
-(defn named-matrix [& [rows cols matrix]]
-  "Convenience function for creating a named matrix (nmat). Shape of named matrix is {:rows :cols :matrix}.
-  Values of :rows and :cols are in same order as corresponding values (votes) in matrix and thus let us find a
-  position within the matrix based on rowname and colname."
-  (let [rows (or rows [])
-        cols (or cols [])
-        matrix (or matrix [[]])]
-    {:rows rows :cols cols :matrix matrix}))
+(defprotocol AutoIncIndex
+  "Things which implement this are intended to be used as data structures for use as rownames or colnames.
+  They are basically overglorified hash maps"
+  (get-names [this] "In order of index, the names associated with the data")
+  (next-index [this] "Get the next available index")
+  (index [this keyname] "Get the index corresponding to keyname")
+  (append [this keyname] "Append keyname")
+  (append-many [this keynames] "Append many")
+  (subset [this keynames] "Subset the index, and update index/hash"))
 
 
-(defn update-nmat [nmat values]
-  "This takes (rowname, colname, value) pairs and inserts them into a named matrix, adding rows and columns
-  if necessary."
-  (reduce
-    (fn [{:keys [rows cols matrix] :as nmat} [row col value]]
-      (let [[row-index col-index] (map #(typed-indexof %1 %2) [rows cols] [row col])
-            [rows cols] (map (fn [xs x i] (if (= -1 i) (conj xs x) xs))
-                          [rows cols] [row col] [row-index col-index])
-            ; If necessary, insert new columns and/or rows of nils
-            matrix (if (= -1 col-index) (mapv #(conj % nil) matrix) matrix)
-            matrix (if (> (count rows) (count matrix)) (conj matrix (into [] (repeat (count cols) nil))) matrix)
-            ; Get indices of matrix position to be updated (note typed-indexof above gives -1 if item not in coll)
-            indices (mapv (fn [xs i] (if (= -1 i) (- (count xs) 1) i))
-                          [rows cols] [row-index col-index])
-            matrix (assoc-in matrix indices value)]
-        (assoc nmat :rows rows :cols cols :matrix matrix)))
-    nmat values))
+(deftype IndexHash
+  [^java.util.Vector names ^clojure.lang.PersistentArrayMap index-hash]
+  AutoIncIndex
+    (get-names [this] (.names this))
+    (next-index [this] (count (.names this)))
+    (index [this keyname] (index-hash keyname))
+    (append [this keyname]
+      (if ((.index-hash this) keyname)
+        this
+        (IndexHash.
+          (conj (get-names this) keyname)
+          (assoc (.index-hash this) keyname (next-index this)))))
+    (append-many [this keynames]
+      ; potentially faster
+      ;(let [uniq-kns (distinct keynames)
+            ;new-kns (remove (.index-hash this) uniq-kns)]
+        ;(IndexHash.
+          ;(into (.names this) new-kns)
+          ;(into (.index-hash)
+            ;(map
+              ;#(vector %1 (+ (count (.names this)) %2))
+              ;new-kns
+              ;(range)))))
+      ; much simpler
+      (reduce append this keynames))
+    (subset [this keynames]
+      (let [kn-set (set keynames)
+            new-kns (filter kn-set (.names this))]
+        (IndexHash. new-kns (into {} (map vector new-kns (range)))))))
 
-(require '[taoensso.timbre.profiling :as profiling
-           :refer (pspy pspy* profile defnp p p*)])
-(defnp row-subset [nmat row-indices]
-  "Returns a new named matrix which has been subset to the given row-indices"
-  (assoc nmat
-    :rows (greedy (filter-by-index (:rows nmat) row-indices))
-    :matrix (greedy-false (filter-by-index (:matrix nmat) row-indices))))
+(defn index-hash
+  "Construct a new IndexHash with the given keynames"
+  [keynames]
+  (let [uniq-kns (into [] (distinct keynames))]
+    (IndexHash. uniq-kns
+      (into {} (map vector uniq-kns (range))))))
 
 
-(defnp rowname-subset [nmat row-names]
-  "Returns a new named matrix which has been subset based on the given row-names"
-  (let [nmat-rows (into [] (:rows nmat))]
-    (->> row-names
-      ; a bit of a hack with into [] ; shouold really fix upstream
-      (map #(typed-indexof nmat-rows %))
-      (greedy)
-      (row-subset nmat))))
+(defprotocol PNamedMatrix
+  ; should really just change to update, but for now...
+  (update-nmat [this values]
+    "Updates a nameable matrix given a seq of (rowname, colname, value) tuples")
+  (rownames [this] "Vector of row names, in order")
+  (colnames [this] "Vector of column names, in order")
+  (get-matrix [this] "Extract the matrix object")
+  (rowname-subset [this names] "Get a new PNamedMatrix subsetting to just the given rownames"))
+
+(defn- padding-dimcounts
+  "Generate paddinbg dimension counts vector suitable for core.matrix/broadcast"
+  [mat dim n]
+  (let [d (cm/dimensionality mat)
+        dcs (mapv #(cm/dimension-count mat %) (range d))]
+    (assoc dcs dim n)))
+
+(defn add-padding
+  "Add padding to a matrix. Note that if mat has nils, this function only works if value is also nil."
+  ; XXX - the nil thing might be able to be fixed (if we need it at some point) by manually turning the
+  ; padding potions into vectors via into in this sitation. Catch?
+  [mat dim n & [value]]
+  (let [dimcounts (padding-dimcounts mat dim n)
+        mat       (if (nil? value) (do (println "me") mat) (cm/matrix mat))
+        padding   (cm/broadcast value dimcounts)
+        padding   (if (number? value) (do (println "fuck") padding) (cm/matrix padding))]
+    (cm/matrix (cm/join-along dim mat padding))))
 
 
-(defnp safe-rowname-subset [nmat row-names]
+(deftype NamedMatrix
+  [^IndexHash row-index ^IndexHash col-index ^java.util.List matrix]
+  PNamedMatrix
+    (update-nmat [this values]
+      (let [values (into [] values)]
+        (println "count" (count values))
+        (p :update-nmat
+          ; First find the row and column names that aren't yet in the data
+          (let [[missing-rows missing-cols]
+                  (reduce
+                    (fn [[missing-rows missing-cols] [row col value]]
+                      (when (nil? (index (.row-index this) row))
+                        (append missing-rows row))
+                      (when (nil? (index (.col-index this) row))
+                        (append missing-cols col)))
+                    [[] []]
+                    values)]
+            ; Construct a new NamedMatrix
+            (.NamedMatrix
+              ; Construct new rowname and colname hash-indices
+              (append-many (.row-index this) missing-rows)
+              (append-many (.col-index this) missing-cols)
+              ; Construct new matrix
+              (as-> (.matrix this) mat
+                ; First add padding of nils for new rows/cols
+                (add-padding mat 1 (count missing-cols))
+                (add-padding mat 0 (count missing-rows))
+                ; Next assoc-in all of the new votes
+                (reduce
+                  (fn [mat' [row col value]]
+                    (let [row-i (index (.row-index this) row)
+                          col-i (index (.col-index this) col)]
+                      (assoc-in [row-i col-i] value)))
+                  mat
+                  values)))))))
+    (rownames [this] (get-names (.row-index this)))
+    (colnames [this] (get-names (.col-index this)))
+    (get-matrix [this] (.matrix this))
+    (rowname-subset [this names]
+      (let [row-indices (map (partial index (.row-index this)) names)
+            row-index (subset (.row-index this) names)]
+        (NamedMatrix.
+          row-index
+          (.col-index this)
+          (filter-by-index (.matrix this) row-indices)))))
+
+(defn named-matrix
+  "Generator function for a new named matrix"
+  [& [rows cols matrix]]
+  (NamedMatrix.
+    (index-hash (or rows []))
+    (index-hash (or cols []))
+    (or matrix [[]])))
+
+
+; Put in interface? ...
+
+(defnp safe-rowname-subset [nmat names]
   "This version of rowname-subset filters out negative indices, so that if not all names in row-names
   are in nmat, it just subsets to the rownames that are. Should scrap other one?"
-  (let [nmat-rows (into [] (:rows nmat))]
-    (->> row-names
-      ; a bit of a hack with into [] ; shouold really fix upstream
-      (map #(typed-indexof nmat-rows %))
-      (filterv #(>= % 0))
-      (row-subset nmat))))
-
+  (let [safe-names (filter
+                     ; XXX This is kinda bad; should only be using protocol
+                     (partial index (.row-index nmat))
+                     names)]
+    (rowname-subset nmat names)))
 
 (defn get-row-by-name [nmat row-name]
-  (matrix/get-row (:matrix nmat) (typed-indexof (:rows nmat) row-name)))
-
+  ; XXX This is kinda bad; should only be using protocol
+  (cm/get-row (get-matrix nmat) (index (.row-index nmat) row-name)))
 
 (defn inv-rowname-subset [nmat row-names]
   "Returns named matrix which has been subset to all the rows not in row-names"
-  (row-subset nmat
-    (remove (set (map #(typed-indexof (:rows nmat) %) row-names))
-      (range (count (:rows nmat))))))
+  (rowname-subset nmat
+    (remove (set row-names) (rownames nmat))))
 
 
---
+++ test/index_hash_test.clj
@@ -0,0 +1,52 @@
+(ns index-hash-test
+  (:require [clojure.test :refer :all]
+            [polismath.named-matrix :refer :all]))
+
+
+(defn ih-equals [ih1 ih2]
+  (letfn [(hash-rep [h] (sort (into [] h)))
+          (hash-equal [h1 h2] (= (hash-rep h1) (hash-rep h2)))]
+    (and (= (get-names ih1) (get-names ih2))
+      (= (hash-equal (.index-hash ih1) (.index-hash ih2))))))
+
+
+; XXX - should really go through here and clean up not to use .names
+(deftest test-index-hash-creation
+  (testing "Creating a new index-hash using `index-hash`"
+    (testing "with no elements"
+      (let [empty-ih (index-hash [])]
+        (is (= (.names empty-ih) []))
+        (is (= (.index-hash empty-ih) {}))))
+    (testing "with some thing"
+      (let [ih (index-hash ["this" "that"])]
+        (is (= (.names ih) ["this" "that"]))
+        (is (= (.index-hash ih) {"this" 0 "that" 1}))))
+    (testing "with duplicate things"
+      (let [ih (index-hash ["this" "that" "this"])]
+        (is (= (.names ih) ["this" "that"]))
+        (is (= (.index-hash ih) {"this" 0 "that" 1}))))
+    (testing "with another ordering"
+      (let [ih (index-hash ["that" "this"])]
+        (is (= (.names ih) ["that" "this"]))
+        (is (= (.index-hash ih) {"that" 0 "this" 1}))))))
+
+(deftest test-append
+  (testing "Appending a new item to the index"
+    (let [base (index-hash ["this" "that"])]
+      (is (ih-equals (index-hash ["this" "that" "more"]) (append base "more")))
+      (testing "when it's already in the index"
+        (is (ih-equals base (append base "this")))))))
+
+(deftest test-append-many
+  (testing "Appending several items to an index"
+    (let [base (index-hash ["this"])
+          expected (index-hash ["this" "that" "more"])]
+      (is (ih-equals expected (append-many base ["that" "more"])))
+      (testing "when some are duplicates"
+        (is (ih-equals expected (append-many base ["that" "this" "more" "that"])))))))
+
+(deftest test-subset
+  (testing "Creatign a subset"
+    (let [base (index-hash ["this" "that" "stuff" "great"])]
+      (testing "should give the right stuff"
+        (is (ih-equals (index-hash ["this" "stuff"]) (subset base ["this" "stuff"])))))))
