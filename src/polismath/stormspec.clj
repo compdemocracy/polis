466da6029523f4ceffb4d8067277f21869d59e93
Christopher Small <metasoarous@gmail.com>
2014-07-04 14:31:04 -0700

    Sketch of actual reaction spout

--- src/polismath/stormspec.clj
+++ src/polismath/stormspec.clj
@@ -10,7 +10,7 @@
   (:gen-class))
 
 
-(defspout reaction-spout ["conv-id" "reaction"] {:prepare true}
+(defspout sim-reaction-spout ["conv-id" "reaction"] {:prepare true}
   [conf context collector]
   (let [at-a-time 10
         interval 1000
@@ -35,12 +35,41 @@
       (ack [id]))))
 
 
+(defspout reaction-spout ["conv-id" "reaction"] {:prepare true}
+  [conf context collector]
+  (let [poll-interval   1000
+        pg-spec         (heroku-db-spec (env/env :database-url))
+        mg-db           (mongo-connect! (env/env :mongo-url))
+        last-timestamp  (atom 0)]
+    (spout
+      (nextTuple []
+        (Thread/sleep poll-interval)
+        (println "poll >" @last-timestamp)
+        (let [new-votes (poll pg-spec @last-timestamp)
+              grouped-votes (group-by :zid)]
+          (doseq [[conv-id rxns] grouped-votes]
+            (emit-spout! collector [conv-id rxns]))
+          (swap! last-timestamp (fn [_] (:created (last new-votes))))))
+      (ack [id]))))
+
+; Some thoughts here
+; * need to pass through lastVoteTimestamp?
+; * need to think about atoms vs other reftypes more; do we want to be spawning extra threads in this case?
+;   storm gives us threads for free so... But the queueing behaviour is nice there. What refs will tell us
+;   when things are waiting for us? Can build a queue and only start firing updates when we aren't already
+;   running. But then likely need a watcher... so starting to look messier.
+
 (defbolt conv-update-bolt ["conv"] {:prepare true}
   [conf context collector]
   (let [conv (agent {:rating-mat (named-matrix)})]
     (bolt (execute [tuple]
       (let [[conv-id rxns] (.getValues tuple)]
+        ; Perform the computational update
         (send conv conv-update rxns)
+        ; Format and upload results
+        (->> (format-conv-for-mongo conv zid lastVoteTimestamp)
+          (mongo-upsert-results "polismath_bidToPid_april9" zid lastVoteTimestamp))
+        ; Storm stuff...
         (emit-bolt! collector
                     [@conv]
                     :anchor tuple)
