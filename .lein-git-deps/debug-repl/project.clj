61bf227a4d990b163fd8f2549279cdd759bd0a1b
Christopher T Small <metasoarous@gmail.com>
2014-08-31 18:16:03 -0700

    Added debug-repl so that we can leave requires in (safely tucked in when-not prod)

---
+++ .lein-git-deps/debug-repl/project.clj
@@ -0,0 +1,4 @@
+(defproject org.clojars.gjahad/debug-repl "0.3.3"
+  :description "Clojure REPL that is aware of surrounding lexical scope"
+  :dependencies [[org.clojure/clojure "1.5.1"]])
+
--- project.clj
+++ project.clj
@@ -10,10 +10,9 @@
   :repositories {"twitter4j" "http://twitter4j.org/maven2"}
   :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]
             [lein-environ "0.4.0"]]
-  :git-dependencies [
-    ["https://github.com/Prismatic/hiphip.git" "master"]
-    ["https://github.com/metasoarous/tools.cli.git" "master"]
-  ]
+  :git-dependencies [["https://github.com/Prismatic/hiphip.git" "master"]
+                     ["https://github.com/metasoarous/tools.cli.git" "master"]
+                     ["https://github.com/GeorgeJahad/debug-repl.git" "master"]]
   :dependencies [[commons-collections/commons-collections "3.2.1"]
                  [cheshire "5.3.1"]
                  [org.clojure/tools.reader "0.8.4"]
@@ -21,11 +20,14 @@
                  [net.mikera/core.matrix "0.23.0"]
                  [net.mikera/core.matrix.stats "0.3.0"]
                  [net.mikera/vectorz-clj "0.19.0"]
+                 [criterium "0.4.2"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/core.match "0.2.1"]
                  [org.clojure/tools.logging "0.3.0"]
+                 [org.clojure/tools.trace "0.7.6"]
                  ;[org.clojure/tools.cli "0.3.1"]
                  [storm "0.9.0.1"]
                  [bigml/sampling "2.1.0"]
@@ -37,30 +39,10 @@
                  [prismatic/plumbing "0.2.0"]
                  [environ "0.4.0"]]
   :min-lein-version "2.3.0"
-  
   :profiles {
     :dev {
-      :dependencies [
-        [org.clojure/tools.trace "0.7.6"]
-        [criterium "0.4.2"]
-        [org.clojure/clojure "1.5.1"]
-      ]
-      :git-dependencies [
-        ["https://github.com/GeorgeJahad/debug-repl.git" "master"]
-      ]
       :env {
-        :mongo-url "db/mongo.db"
-      }
-    }
+        :mongo-url "db/mongo.db"}}
     :production {
-      :dependencies [
-        [org.clojure/tools.trace "0.7.6"]
-        [criterium "0.4.2"]
-        [org.clojure/clojure "1.5.1"]
-      ]
-      :env {
-      }
-    }
-  }
-)
+      :env {}}})
 
