a19d509031d2171a74b8e50b34e7c9382d98a3e0
Christopher T Small <metasoarous@gmail.com>
2014-09-08 23:47:47 -0700

    Get ALL users from intercom, recursively

--- src/polismath/intercom.clj
+++ src/polismath/intercom.clj
@@ -12,21 +12,46 @@
 (defn get-intercom-users
   "Get the list of users from intercom (don't want to create intercom users for users that haven't
   actually signed up"
-  []
-  (->
-    (client/get "https://api.intercom.io/users"
-                {:accept :json
-                 :basic-auth ["nb5hla8s" (env/env :intercom-api-key)]})
-    :body
-    (ch/parse-string)
-    (->>
-      (into {}))))
+  [& [page]]
+  (let [return-data
+          (->
+            (or page "https://api.intercom.io/users")
+            (client/get
+              {:accept :json
+               :basic-auth ["nb5hla8s" (env/env :intercom-api-key)]})
+            :body
+            (ch/parse-string)
+            (->>
+              (into {})))
+        next-page (get-in return-data ["pages" "next"])
+        users (get return-data "users")]
+    (if next-page
+      (into users (get-intercom-users next-page))
+      users)))
+
+
+(defn user-id
+  [user]
+  (try
+    (-> user
+        (get "user_id")
+        (Integer/parseInt))
+    (catch Exception e
+      nil)))
+
+
+(defn user-id-str
+  [user]
+  (get user "user_id"))
 
 
 (defn -main
   []
-  (->>
+  (->
     (get-intercom-users)
-    (get "users")
+    (->>
+      (sort-by #(get % "created_at"))
+      (map #(vector (get % "created_at") (user-id-str %))))
     (pp/wide-pp)))
+
 
