6726a119030eaaa1a17e971ad7587e164fbc4487
Mike Bjorkegren <m@bjorkegren.com>
2014-09-24 16:16:26 -0700

    name

--- server.js
+++ server.js
@@ -6190,7 +6190,7 @@ function(req, res) {
 var xml = '' +
 '<cartridge_basiclti_link xmlns="http://www.imsglobal.org/xsd/imslticc_v1p0" xmlns:blti="http://www.imsglobal.org/xsd/imsbasiclti_v1p0" xmlns:lticm="http://www.imsglobal.org/xsd/imslticm_v1p0" xmlns:lticp="http://www.imsglobal.org/xsd/imslticp_v1p0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/imslticc_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticc_v1p0.xsd http://www.imsglobal.org/xsd/imsbasiclti_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imsbasiclti_v1p0.xsd http://www.imsglobal.org/xsd/imslticm_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticm_v1p0.xsd http://www.imsglobal.org/xsd/imslticp_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticp_v1p0.xsd">' +
 
-'<blti:title>Polis Setup 1</blti:title>' +
+'<blti:title>Polis Setup Assignment</blti:title>' +
 '<blti:description>based on Minecraft LMS integration</blti:description>' +
 '<blti:icon>' +
 'http://minecraft.inseng.net:8133/minecraft-16x16.png' +
