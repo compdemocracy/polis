b23c8d44d3ed212b40305800f13c5c96fc6f9edc
Mike Bjorkegren <m@bjorkegren.com>
2014-09-27 17:32:59 -0700

    customize oauth_rfc5849

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var badwords = require('badwords/object'),
     bcrypt = require('bcrypt'),
     crypto = require('crypto'),
     Intercom = require('intercom.io'), // https://github.com/tarunc/intercom.io
-    oauth_rfc5849 = require('oauth-toolkit'),
+    // oauth_rfc5849 = require('oauth-toolkit'),
     p3p = require('p3p'),
     Pushover = require( 'pushover-notifications' ),
     pushoverInstance = new Pushover( {
@@ -6442,6 +6442,248 @@ function sendGrades(lis_outcome_service_url, lis_result_sourcedid, body, headers
     // // });
 }
 
+
+
+var url = require('url');
+var qs = require('querystring');
+var crypto = require('crypto');
+
+var ALPHANUM = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+
+function nonce(length, chars) {
+    chars = chars || ALPHANUM;
+    var result = "";
+    for (var i = 0; i < length; i++) {
+        var at = Math.floor(Math.random() * chars.length);
+        result += chars.charAt(at);
+    }
+    return result;
+}
+
+function timestamp(date) {
+    date = date || new Date();
+    return Math.floor(date);
+}
+
+function encode(s) {
+    if (! s) return '';
+    return encodeURIComponent(s)
+        .replace(/\!/g, "%21")
+        .replace(/\*/g, "%2A")
+        .replace(/\'/g, "%27")
+        .replace(/\(/g, "%28")
+        .replace(/\)/g, "%29");
+}
+
+function protocolParameters(options) {
+    var params = {};
+    // oauth_consumer_key
+    //   The identifier portion of the client credentials (equivalent to
+    //   a username).  The parameter name reflects a deprecated term
+    //   (Consumer Key) used in previous revisions of the specification,
+    //   and has been retained to maintain backward compatibility.
+    params.oauth_consumer_key = options.consumerKey;
+
+    // oauth_token
+    //   The token value used to associate the request with the resource
+    //   owner.  If the request is not associated with a resource owner
+    //   (no token available), clients MAY omit the parameter.
+    if (options.token) params.oauth_token = options.token;
+
+    // oauth_signature_method
+    //   The name of the signature method used by the client to sign the
+    //   request, as defined in Section 3.4.
+    params.oauth_signature_method = options.signatureMethod.toUpperCase();
+
+    // oauth_timestamp
+    //   The timestamp value as defined in Section 3.3.  The parameter
+    //   MAY be omitted when using the "PLAINTEXT" signature method.
+    if (params.oauth_signature_method !== 'PLAINTEXT')
+        params.oauth_timestamp = options.timestamp || timestamp();
+    else if (options.timestamp)
+        params.oauth_timestamp = params.timestamp;
+
+    // oauth_nonce
+    //   The nonce value as defined in Section 3.3.  The parameter MAY
+    //   be omitted when using the "PLAINTEXT" signature method.
+    if (params.oauth_signature_method !== 'PLAINTEXT')
+        params.oauth_nonce = options.nonce || nonce(6);
+    else if (options.nonce)
+        params.oauth_nonce = options.nonce;
+
+    // oauth_version
+    //   OPTIONAL.  If present, MUST be set to "1.0".  Provides the
+    //   version of the authentication process as defined in this
+    //   specification.
+    if (options.version) params.oauth_version = '1.0';
+
+    // Follows are situational
+    if (options.callback) params.oauth_callback = options.callback;
+    if (options.verifier) params.oauth_verifier = options.verifier;
+
+    return params;
+}
+
+//
+// Signature Base String
+// http://tools.ietf.org/html/rfc5849#section-3.4.1
+//
+function signatureBaseString(requestMethod, uri, body, protocolParams) {
+    var baseSTring =  [
+        encode(requestMethod.toUpperCase()),
+        encode(baseStringUri(uri)),
+        encode(normalizedRequestParameters(uri, body, protocolParams))
+    ].join('&');
+    console.log("baseSTring", baseSTring);
+    return baseSTring;
+}
+
+//
+// Base String URI
+// http://tools.ietf.org/html/rfc5849#section-3.4.1.2
+//
+function baseStringUri(uri) {
+    var parsed = url.parse(uri);
+
+    //  The port MUST be included if it is not the default port for the
+    //  scheme, and MUST be excluded if it is the default.
+    if (parsed.protocol === 'https:/') {
+        parsed.host = parsed.host.replace(/:443$/, '');
+    } else if (parsed.protocol === 'http:/') {
+        parsed.host = parsed.host.replace(/:80$/, '');
+    }
+
+    return url.format({
+        protocol: parsed.protocol,
+        host: parsed.host,
+        auth: parsed.auth,
+        pathname: parsed.pathname
+    });
+}
+
+//
+// Maps object to bi-dimensional array
+//
+// @example
+// toArray({ foo: 'A', bar: [ 'b', 'B' ]})
+// will be
+// [ ['foo', 'A'],
+//   ['bar', 'b'],
+//   ['bar', 'B'] ]
+//
+function toArray(obj) {
+    var key, val, arr = [];
+    for (key in obj) {
+        val = obj[key];
+        if (Array.isArray(val))
+            for (var i = 0; i < val.length; i++)
+        arr.push([ key, val[i] ]);
+        else
+            arr.push([ key, val ]);
+    }
+    return arr;
+}
+
+//
+// Parameter Sources
+// http://tools.ietf.org/html/rfc5849#section-3.4.1.3.1
+//
+function paramSources(uri, body, protocolParams) {
+    var queryObj = url.parse(uri, true).query;
+    var bodyObj = querystring.parse(body);
+    return Array.prototype.concat.apply([], [
+        queryObj, bodyObj, protocolParams
+    ].map(toArray));
+}
+
+//
+// comparing function for Array.sort()
+//
+function compare(a, b) {
+    return a > b ? 1 : a < b ? -1 : 0;
+}
+
+//
+// Parameters Normalization
+// http://tools.ietf.org/html/rfc5849#section-3.4.1.3.2
+//
+function normalizedRequestParameters(uri, body, protocolParams) {
+    // to multidimension array
+    return paramSources(uri, body, protocolParams)
+
+    // 1.  First, the name and value of each parameter are encoded
+    .map(function (p) {
+        return [ encode(p[0]), encode(p[1]) ];
+    })
+
+    // 2.  The parameters are sorted by name, using ascending byte value
+    //     ordering.  If two or more parameters share the same name, they
+    //     are sorted by their value.
+    .sort(function (a, b) {
+        return compare(a[0], b[0]) || compare(a[1], b[1]);
+    })
+
+    // 3.  The name of each parameter is concatenated to its corresponding
+    //     value using an "=" character (ASCII code 61) as a separator, even
+    //     if the value is empty.
+    .map(function (p) { return p.join('='); })
+
+    // 4.  The sorted name/value pairs are concatenated together into a
+    //     single string by using an "&" character (ASCII code 38) as
+    //     separator.
+    .join('&');
+}
+
+//
+// HMAC-SHA1 signing key
+//
+function signingKey(consumerSecret, tokenSecret) {
+    var key = encode(consumerSecret) + '&' + encode(tokenSecret);
+    console.log("signingKey", key);
+    return key;
+}
+
+//
+// get signature
+//
+function signature(consumerSecret, tokenSecret, requestMethod, url, body, protocolParams) {
+    var signatureMethod = protocolParams.oauth_signature_method;
+    if (signatureMethod === 'HMAC-SHA1') {
+        return crypto.createHmac('sha1', signingKey(consumerSecret, tokenSecret))
+            .update(signatureBaseString(requestMethod, url, body, protocolParams))
+            .digest('base64');
+    }
+}
+
+//
+// helper
+//
+function getSignature(requestMethod, url, body, oauthOpts) {
+    return signature(
+        oauthOpts.consumerSecret,
+        oauthOpts.tokenSecret,
+        requestMethod,
+        url,
+        body,
+        protocolParameters(oauthOpts)
+    );
+}
+
+var oauth_rfc5849 = {
+    _protocolParameters: protocolParameters,
+    _signature: signature,
+    _nonce: nonce,
+    _timestamp: timestamp,
+    _encode: encode,
+    _decode: decodeURIComponent,
+    _baseString: signatureBaseString,
+    _baseStringUri: baseStringUri,
+    _normalizedParameters: normalizedRequestParameters,
+    signature: getSignature,
+};
+
+
+
 app.post("/api/v3/LTI/conversation_assignment",
     need("oauth_consumer_key", getStringLimitLength(1, 9999), assignToP), // for now, this will be the professor, but may also be the school    need("oauth_consumer_key", getStringLimitLength(1, 9999), assignToP), // for now, this will be the professor, but may also be the school
     need("oauth_signature_method", getStringLimitLength(1, 9999), assignToP), // probably "HMAC-SHA-1"
@@ -6595,7 +6837,7 @@ required parameters, per sections 6.1.1, 6.3.1, and 7
         var oauthHeaders = {
             oauth_consumer_key: req.p.oauth_consumer_key,
             oauth_signature_method: req.p.oauth_signature_method,
-            oauth_timestamp: req.p.oauth_timestamp, // or generate one?
+            oauth_timestamp: ''+(Date.now()/1000>>0),
             oauth_nonce: req.p.oauth_nonce,
             oauth_version: req.p.oauth_version,
             oauth_callback: req.p.oauth_callback,
@@ -6607,15 +6849,15 @@ required parameters, per sections 6.1.1, 6.3.1, and 7
             req.p.lis_outcome_service_url,  //'http://example.com/request?b5=%3D%253D&a3=a&c%40=&a2=r%20b', // url
             replaceResultRequestBody, //c2&a3=2+q', // body
             {
-                consumerKey: req.p.oauth_consumer_key, //'9djdj82h48djs9d2',
+                consumerKey: oauthHeaders.oauth_consumer_key, //'9djdj82h48djs9d2',
                 consumerSecret: consumerSecret, //'j49sk3j29djd',
                 // token: '', //kkk9d7dh3k39sjv7',  maps to oauth_token, optional
                 tokenSecret: '', // dh893hdasih9', // this is the "shared secret" may be empty? (see 3.4.2 key) http://tools.ietf.org/html/rfc5849
-                nonce: req.p.oauth_nonce, //'7d8f3e4a',
-                signatureMethod: req.p.oauth_signature_method, //'HMAC-SHA1',
-                timestamp: req.p.oauth_timestamp, //'137131201',
-                version: req.p.oauth_version, // must be '1.0'
-                callback: req.p.oauth_callback, // needed?
+                nonce: oauthHeaders.oauth_nonce, //'7d8f3e4a',
+                signatureMethod: oauthHeaders.oauth_signature_method, //'HMAC-SHA1',
+                timestamp: oauthHeaders.oauth_timestamp, //'137131201',
+                version: oauthHeaders.oauth_version, // must be '1.0'
+                callback: oauthHeaders.oauth_callback, // needed?
             }
         );
 
