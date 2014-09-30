b6dc67be45ea1a0d148684fc58adf4ad8d8a50ec
Mike Bjorkegren <m@bjorkegren.com>
2014-09-29 19:07:22 -0700

    Revert "stripe WIP"
    This reverts commit a13b8d4fc70294abf8c807bb5b23d22de9caca1f.
    
    not needed yet - doing student-pays later

--- js/views/settings.js
+++ js/views/settings.js
@@ -50,31 +50,13 @@ module.exports = Handlebones.ModelView.extend({
 
 
         // New stripe hotness with fully custom forms for canvas integration
-        $('#stripeSubmitButton').on('click', function(event) {
-          var $form = $('#payment-form');
+        $('#payment-form').submit(function(event) {
+          var $form = $(this);
 
           // Disable the submit button to prevent repeated clicks
           $form.find('button').prop('disabled', true);
 
-          Stripe.card.createToken($form, function(status, response) {
-            alert(status);
-            alert(JSON.stringify(response));
-            /*
-            status should look like a code from https://stripe.com/docs/api#errors
-            */
-
-            /* Response should look like this:
-              {
-                id: "tok_u5dg20Gra", // String of token identifier,
-                card: {...}, // Dictionary of the card used to create the token
-                created: 1411706790, // Integer of date token was created
-                currency: "usd", // String currency that the token was created in
-                livemode: true, // Boolean of whether this token was created with a live or test API key
-                object: "token", // String identifier of the type of object, always "token"
-                used: false // Boolean of whether this token has been used
-              }
-            */
-          });
+          Stripe.card.createToken($form, stripeResponseHandler);
 
           // Prevent the form from submitting with the default action
           return false;
