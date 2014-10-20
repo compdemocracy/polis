67f871beac1b1ba6e7c6c7e795b50ee4d8599f57
Christopher Small <metasoarous@gmail.com>
2014-10-20 00:38:31 -0700

    Cleaned up microscope code

--- src/polismath/microscope.clj
+++ src/polismath/microscope.clj
@@ -13,12 +13,6 @@
             [clojure.newtools.cli :refer [parse-opts]]))
 
 
-
-(def cli-options
-  [["-z" "--zid ZID" "ZID on which to do a rerun" :parse-fn #(Integer/parseInt %)]
-   ["-Z" "--zinvite ZINVITE" "ZINVITE code on which to perform a rerun"]])
-
-
 (defn conv-poll
   [zid]
   (kdb/with-db (db/db-spec)
@@ -39,10 +33,8 @@
 
 
 (defn recompute
-  [& args]
-  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)
-        {:keys [zid zinvite]} options
-        _          (assert (xor zid zinvite))
+  [& {:keys [zid zinvite] :as args}]
+  (let [_          (assert (xor zid zinvite))
         zid        (or zid (get-zid-from-zinvite zinvite))
         new-votes  (conv-poll zid)
         conv-agent ((cm/new-conv-agent-builder zid))]
@@ -61,6 +53,18 @@
   [& {:keys [zid zinvite] :as args}]
   (assert (xor zid zinvite))
   (let [zid (or zid (get-zid-from-zinvite zinvite))]
-    (load-conv zid)))
+    (db/load-conv zid)))
+
+
+(def cli-options
+  [["-z" "--zid ZID" "ZID on which to do a rerun" :parse-fn #(Integer/parseInt %)]
+   ["-Z" "--zinvite ZINVITE" "ZINVITE code on which to perform a rerun"]])
+
+
+(defn -main
+  [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (apply-kwargs recompute options)))
+
 
 
