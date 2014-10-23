6e82dd4e7cd51410702ebffe9b4287bbf65bfd43
Mike Bjorkegren <m@bjorkegren.com>
2014-10-22 17:02:40 -0700

    Revert "move writing tips to a separate view"
    This reverts commit 7302d49ad60e57ef231c714cf84a08fbc0df74aa.

--- js/views/writingTips.js
+++
@@ -1,7 +0,0 @@
-var template = require("../tmpl/writingTips");
-var Handlebones = require("handlebones");
-
-module.exports = Handlebones.View.extend({
-  name: "writingTips",
-  template: template
-});
