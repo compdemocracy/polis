511c95e3cf6cea7530e0d29309e0eedba90d9621
Mike Bjorkegren <m@bjorkegren.com>
2014-11-06 17:17:44 +0000

    disable facebook auth until we fix it

--- js/views/create-user-form.js
+++ js/views/create-user-form.js
@@ -350,7 +350,9 @@ var ModelView = Handlebones.ModelView;
     Handlebones.ModelView.prototype.initialize.apply(this, arguments);
     this.authStyleHeader = true;
     var that = this;
-    this.fb = !this.model.get("showEmailWelcome");
+    
+    this.fb = false; //!this.model.get("showEmailWelcome");
+
     // this.model = options.model;
     this.listenTo(this, "render", function() {
       var email = that.model.get("email");
