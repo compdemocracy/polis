cb49c0b1d90bdd86392f89ab747fa0ff281a3047
Mike Bjorkegren <m@bjorkegren.com>
2014-12-08 17:08:24 -0800

    fix typo

--- server.js
+++ server.js
@@ -6968,7 +6968,7 @@ function(req, res) {
                 "friends_count," +
                 "verified," +
                 "profile_image_url_https," +
-                "location" +
+                "location," +
                 "response" +
                 ") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);",[
                     uid,
