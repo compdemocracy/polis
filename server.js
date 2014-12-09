f429ef81f123d157d1ba7438ec1f700b09967ee7
Mike Bjorkegren <m@bjorkegren.com>
2014-12-08 23:16:20 -0800

    logging

--- server.js
+++ server.js
@@ -3801,6 +3801,8 @@ function getUserByEmail(email) {
 }
 
 function createFacebookUserRecord(o) {
+    console.log("createFacebookUserRecord");
+    console.dir(o);
     // Create facebook user record
     return pgQueryP("insert into facebook_users (uid, fb_user_id, fb_public_profile, fb_login_status, fb_access_token, fb_granted_scopes, fb_location_id, location, fb_friends_response, response) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);", [
         o.uid,
