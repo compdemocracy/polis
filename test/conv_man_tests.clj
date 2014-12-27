1e5ef247df1145fa8465d9d6ff6fa76bd79e30ce
Christopher T Small <metasoarous@gmail.com>
2014-12-26 18:37:09 -0700

    Added some more batch split tests

--- test/conv_man_tests.clj
+++ test/conv_man_tests.clj
@@ -10,5 +10,19 @@
     (let [messages [[:votes [{:a 4} {:b 5} {:c 6}]]
                     [:votes [{:d 7} {:e 8} {:f 9}]]]]
       (is (= (split-batches messages)
-             {:votes [{:a 4} {:b 5} {:c 6} {:d 7} {:e 8} {:f 9}]})))))
+             {:votes [{:a 4} {:b 5} {:c 6} {:d 7} {:e 8} {:f 9}]}))
+      (is (nil? (:moderation (split-batches messages))))))
+  (testing "of one message"
+    (let [messages [[:votes [{:a 4} {:b 5} {:c 6}]]]]
+      (is (= (split-batches messages)
+             {:votes [{:a 4} {:b 5} {:c 6}]}))))
+  (testing "of combined messages"
+    (let [messages [[:votes [{:a 4} {:b 5} {:c 6}]]
+                    [:votes [{:d 7} {:e 8} {:f 9}]]
+                    [:moderation [{:g 4} {:h 5} {:i 6}]]
+                    [:moderation [{:j 7} {:k 8} {:l 9}]]]]
+      (is (= (split-batches messages)
+             {:votes [{:a 4} {:b 5} {:c 6} {:d 7} {:e 8} {:f 9}]
+              :moderation [{:g 4} {:h 5} {:i 6} {:j 7} {:k 8} {:l 9}]})))))
+
 
