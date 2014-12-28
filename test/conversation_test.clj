8ce85c7445552942f406a6a1cf377aa58397e038
Christopher T Small <metasoarous@gmail.com>
2014-12-27 19:33:08 -0700

    More conversation update test refactoring, and some simple moderation sanity check tests

--- test/conversation_test.clj
+++ test/conversation_test.clj
@@ -16,7 +16,20 @@
                      {:created 700 :pid :a, :tid :y, :vote 1}]
       several-votes (concat single-vote wonky-votes)
       full-votes    (for [pid [:a :b :c] tid [:x :y :z]]
-                     {:created 100 :pid pid :tid tid :vote (rand)})]
+                     {:created 100 :pid pid :tid tid :vote (rand)})
+
+      big-conv {:rating-mat
+                  (named-matrix
+                    [:p1 :p2 :p3 :p4 :p5]
+                    [:c1 :c2 :c3 :c4]
+                    [[ 0  1  0 -1  1]
+                     [-1  0 -1 -1  0]
+                     [ 1  0  1  0  1]
+                     [ 1 -1  0 -1  0]
+                     [-1 -1  0  1  0]])
+                :pca {:center [0.0 -0.2 -0.0  0.4] ;actually calculated this...
+                      :comps [[0.4  0.2 -0.3  0.7] ;this is faked...
+                              [0.1 -0.5  0.2  0.2]]}}]
 
   (deftest init-conv-update-test
     (testing "empty matrix and one vote"
@@ -44,34 +57,29 @@
       (is (conv-update small-conv wonky-votes))))
 
   ; Test that iterating on previous pca/clustering results makes sense
-  (deftest small-conv-iterative-test
-    (let [fleshed-conv (conv-update small-conv single-vote)]
+  (let [fleshed-conv (conv-update small-conv single-vote)]
+    (deftest small-conv-iterative-test
       (testing "fleshed conv and full matrix"
         (is (conv-update fleshed-conv full-votes)))
       (testing "fleshed conv and wonky vote"
-        (is (conv-update fleshed-conv [{:created 999 :pid :j :tid :k :vote -1}]))))))
+        (is (conv-update fleshed-conv [{:created 999 :pid :j :tid :k :vote -1}]))))
+  
+    (deftest moderation-test
+      (testing "from scratch with fleshed out conv"
+        (is (:mod-out (mod-update fleshed-conv [{:tid :x}]))))
+      (testing "based on previous moderations"
+        (is (= (:mod-out (mod-update (assoc big-conv :mod-out [:x])
+                                     [{:tid :y}]))
+               (set [:x :y]))))))
 
-
-(deftest large-conv-update-test
-  (let [data (named-matrix
-               [:p1 :p2 :p3 :p4 :p5]
-               [:c1 :c2 :c3 :c4]
-               [[ 0  1  0 -1  1]
-                [-1  0 -1 -1  0]
-                [ 1  0  1  0  1]
-                [ 1 -1  0 -1  0]
-                [-1 -1  0  1  0]])
-        conv {:rating-mat data
-              :pca {:center [0.0 -0.2 -0.0  0.4] ;actually calculated this...
-                    :comps [[0.4  0.2 -0.3  0.7] ;this is faked...
-                            [0.1 -0.5  0.2  0.2]]}}]
-    (testing "should work with votes for only existing ptpts/cmts"
-      (is (large-conv-update {:conv conv :opts {} 
-                              :votes [{:created 100 :pid :p1 :tid :c1 :vote  1}
-                                      {:created 200 :pid :p3 :tid :c3 :vote -1}]})))
-    (testing "should work with votes for new cmts"
-      (is (large-conv-update {:conv conv :opts {}
-                              :votes [{:created 100 :pid :p3 :tid :c5 :vote  1}
-                                      {:created 200 :pid :p5 :tid :c5 :vote -1}]})))))
+  (deftest large-conv-update-test
+      (testing "should work with votes for only existing ptpts/cmts"
+        (is (large-conv-update {:conv big-conv :opts {} 
+                                :votes [{:created 100 :pid :p1 :tid :c1 :vote  1}
+                                        {:created 200 :pid :p3 :tid :c3 :vote -1}]})))
+      (testing "should work with votes for new cmts"
+        (is (large-conv-update {:conv big-conv :opts {}
+                                :votes [{:created 100 :pid :p3 :tid :c5 :vote  1}
+                                        {:created 200 :pid :p5 :tid :c5 :vote -1}]})))))
 
 
