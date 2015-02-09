1f41f2cc4975a821ddefb7222d41cfb491a1de6f
Mike Bjorkegren <m@bjorkegren.com>
2015-02-08 23:57:31 -0800

    fix fonts for iOS

--- js/lib/VisView.js
+++ js/lib/VisView.js
@@ -1793,7 +1793,7 @@ function upsertNode(updatedNodes, newClusters, newParticipantCount, comments) {
         .classed("summaryLabel", true)
         // .classed("help", true)
         // .classed("help_text_you", true)
-        .style("font-family", "Tahoma") // For the "AGREED"/"DISAGREED" label: Tahoma should be good at small sizes http://ux.stackexchange.com/questions/3330/what-is-the-best-font-for-extremely-limited-space-i-e-will-fit-the-most-readab
+        .style("font-family", "Tahoma, Helvetica, sans-serif") // For the "AGREED"/"DISAGREED" label: Tahoma should be good at small sizes http://ux.stackexchange.com/questions/3330/what-is-the-best-font-for-extremely-limited-space-i-e-will-fit-the-most-readab
         .style("font-size", "12px")
         .style("font-weight", "bold")
         .attr("text-anchor", "middle")
@@ -1810,7 +1810,7 @@ function upsertNode(updatedNodes, newClusters, newParticipantCount, comments) {
         g.filter(isSummaryBucket)
         .append("text")
         .classed("summaryLabelBottom", true)
-        .style("font-family", "Tahoma") // For the "AGREED"/"DISAGREED" label: Tahoma should be good at small sizes http://ux.stackexchange.com/questions/3330/what-is-the-best-font-for-extremely-limited-space-i-e-will-fit-the-most-readab
+        .style("font-family", "Tahoma, Helvetica, sans-serif") // For the "AGREED"/"DISAGREED" label: Tahoma should be good at small sizes http://ux.stackexchange.com/questions/3330/what-is-the-best-font-for-extremely-limited-space-i-e-will-fit-the-most-readab
         .style("font-size", "10px")
         // .style("font-weight", "bold")
         .style("fill", "gray")
