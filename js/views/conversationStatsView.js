0ecfb063d2b651efeb16574d4cc067561510aaa1
Mike Bjorkegren <m@bjorkegren.com>
2015-04-06 23:14:29 -0700

    add zooming and date formatting to admin stats graph

--- js/views/conversationStatsView.js
+++ js/views/conversationStatsView.js
@@ -26,11 +26,11 @@ module.exports =  PolisModelView.extend({
       bottom: 20,
       left: 50
     };
+    var strokeWidth = 2;
     var ptpts = data;
     var first = ptpts[0];
     var last = ptpts[ptpts.length-1];
-
-    var xScale = d3.scale.linear()
+    var xScale = d3.time.scale()
       .range([margins.left, w - margins.right])
       .domain([first.created, last.created]);
 
@@ -39,19 +39,37 @@ module.exports =  PolisModelView.extend({
       .domain([ptpts[0].count, ptpts[ptpts.length-1].count]);
 
     var xAxis = d3.svg.axis()
-      .scale(xScale);
+      .scale(xScale)
+      .orient("bottom")
+      .tickSize(-h, 0)
+      .tickPadding(6);
 
     var yAxis = d3.svg.axis()
       .scale(yScale)
+      .tickSize(-w, 0)
+      .tickPadding(6)
       .orient("left");
 
+    vis.append("clipPath")
+        .attr("id", "clip")
+      .append("rect")
+        .attr("x", xScale(first.created) - strokeWidth)
+        // .attr("y", yScale(1))
+        .attr("width", w)
+        .attr("height", h);
+
     vis.append("g")
-      .attr("transform", "translate(0," + (h - margins.bottom) + ")")
-      .call(xAxis);
+      .classed("x", true)
+      .classed("axis", true)
+      .attr("transform", "translate(0," + (h - margins.bottom) + ")");
+
 
     vis.append("svg:g")
-      .attr("transform", "translate(" + (margins.left) + ",0)")
-      .call(yAxis);
+      .classed("y", true)
+      .classed("axis", true)
+      .attr("transform", "translate(" + (margins.left) + ",0)");
+
+
 
     var lineGen = d3.svg.line()
       .x(function(d) {
@@ -61,11 +79,44 @@ module.exports =  PolisModelView.extend({
         return yScale(d.count);
       });
 
-    vis.append('svg:path')
-      .attr('d', lineGen(ptpts))
+// var line = d3.svg.line()
+//     .interpolate("step-after")
+//     .x(function(d) { return x(d.date); })
+//     .y(function(d) { return y(d.value); });
+
+
+
+    vis.append('path')
+      .classed('line', true)
+      .attr("clip-path", "url(#clip)")
       .attr('stroke', color)
-      .attr('stroke-width', 2)
+      .attr('stroke-width', strokeWidth)
       .attr('fill', 'none');
+
+    vis.select('path.line').data([ptpts]);
+
+
+    var zoom = d3.behavior.zoom()
+      .on("zoom", draw);
+    zoom.x(xScale);
+
+    vis.append("rect")
+      .attr("class", "pane")
+      .attr("width", w)
+      .attr("height", h)
+      .style("cursor", "move")
+      .style("fill", "rgba(0,0,0,0)")
+      .style("pointer-events", "all")
+      .call(zoom);
+
+    function draw() {
+      vis.select("g.x.axis").call(xAxis);
+      vis.select("g.y.axis").call(yAxis);
+      // vis.select("path.area").attr("d", area);
+      vis.select("path.line").attr("d", lineGen);
+    }
+
+    draw();
 
   },
   checkForLatestStats: function() {
@@ -73,7 +124,9 @@ module.exports =  PolisModelView.extend({
     $.get("/api/v3/conversationStats?conversation_id=" + this.model.get("conversation_id")).then(function(stats) {
       
       // loop over each array, and create objects like {count: ++i, created}, then create a line plot with count as y, and created as x
-      _.each(_.keys(stats), function(key) {
+      var keys = _.keys(stats);
+      keys = [keys[0]]; // TODO remove
+      _.each(keys, function(key) {
         var vals = stats[key];
         var i = 1;
         var data = _.map(vals, function(created) {
@@ -82,8 +135,12 @@ module.exports =  PolisModelView.extend({
             created: created
           };
         });
+        var now = _.extend({}, data[data.length-1]);
+        now.created = Date.now()+0; // straight line to current time
+        data.push(now);
         that.model.set(key, data);
-        that.renderParticipantGraph(data, "green");
+
+        that.renderParticipantGraph(data, "black");
       });
 
 
