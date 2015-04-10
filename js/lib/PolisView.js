5abd37504a9c83600931877902f6f204f011616f
Mike Bjorkegren <m@bjorkegren.com>
2015-04-09 22:57:03 -0700

    fix spinner

--- js/lib/PolisView.js
+++ js/lib/PolisView.js
@@ -1,11 +1,13 @@
+var eb = require("../eventBus");
 var Handlebones = require("handlebones");
 
 var PolisView = Handlebones.View.extend({ 
 
     render: function() { 
-       this.trigger("beforeRender");
+       eb.trigger(eb.firstRender);
+       // this.trigger("beforeRender");
        Handlebones.View.prototype.render.apply(this, arguments);
-       this.trigger("afterRender");
+       // this.trigger("afterRender");
     }, 
 
 });
