db18906dc9aab97a9b0aa2aff908181abbc1ef7d
Mike Bjorkegren <m@bjorkegren.com>
2015-07-15 13:20:54 -0700

    tabs to spaces

--- js/views/groupSelectionView.js
+++ js/views/groupSelectionView.js
@@ -14,12 +14,12 @@ module.exports =  Handlebones.ModelView.extend({
     "click .infoPaneButton": "onClickInfoPaneButton",
   },
   onClick: function(e) {
-  	var $target = $(e.target);
-  	var gid = Number($target.data("gid"));
-  	if (!_.isNaN(gid)) {
-	  	this.model.set("selectedGid", gid);
-	  	this.onChangedCallbacks.fire(gid);
-  	}
+    var $target = $(e.target);
+    var gid = Number($target.data("gid"));
+    if (!_.isNaN(gid)) {
+      this.model.set("selectedGid", gid);
+      this.onChangedCallbacks.fire(gid);
+    }
   },
   onClickInfoPaneButton: function(e) {
     this.onClickInfoPaneButtonClickedCallbacks.fire();
@@ -29,13 +29,13 @@ module.exports =  Handlebones.ModelView.extend({
     this.model.set("infoSlidePaneViewActive", true);
   },
   show: function() {
-  	this.model.set("visible", true);
+    this.model.set("visible", true);
   },
   setSelectedGroup: function(gid) {
-  	this.gid = gid;
+    this.gid = gid;
   },
   addSelectionChangedListener: function(f) {
-  	this.onChangedCallbacks.add(f);
+    this.onChangedCallbacks.add(f);
   },
   addInfoPaneButtonClickedListener: function(f) {
     this.onClickInfoPaneButtonClickedCallbacks.add(f);
@@ -44,11 +44,11 @@ module.exports =  Handlebones.ModelView.extend({
     var ctx = Handlebones.ModelView.prototype.context.apply(this, arguments);
     var infoSlidePaneViewActive = ctx.infoSlidePaneViewActive;
     ctx.groups2 = ctx.groups.map(function(g) {
-    	g = $.extend({}, g);
-    	if (g.gid === ctx.selectedGid && !infoSlidePaneViewActive) {
-    		g.selected = true;
-    	}
-    	return g;
+      g = $.extend({}, g);
+      if (g.gid === ctx.selectedGid && !infoSlidePaneViewActive) {
+        g.selected = true;
+      }
+      return g;
     });
     return ctx;
   },
