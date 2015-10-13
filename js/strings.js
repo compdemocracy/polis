7b45deee87c002abdc33888aea5c9971b65f66c0
Mike Bjorkegren <m@bjorkegren.com>
2015-10-13 16:47:00 -0700

    wait to fix up strings until we have acceptLanguage available

--- js/strings.js
+++ js/strings.js
@@ -1,4 +1,4 @@
-
+var preloadHelper = require("./util/preloadHelper");
 
 var en_us = require("./strings/en_us.js");
 var ja = require("./strings/ja.js");
@@ -9,32 +9,36 @@ var zh_Hant = require("./strings/zh_Hant.js");
 // zh-Hans is Simplified Chinese. (CN, SG and MY can use the same file.)
 var zh_Hans = require("./strings/zh_Hans.js");
 
-var acceptLanguage = preload.acceptLanguage || "";
 
 var strings = en_us;
 
-var prioritized = acceptLanguage.split(";");
-prioritized = prioritized[0].split(",");
-prioritized = prioritized || [];
-prioritized.reverse();
+preloadHelper.acceptLanguagePromise.then(function() {
+  var acceptLanguage = preload.acceptLanguage || "";
+
+  var prioritized = acceptLanguage.split(";");
+  prioritized = prioritized[0].split(",");
+  prioritized = prioritized || [];
+  prioritized.reverse();
 
 
-prioritized.forEach(function(languageCode) {
-  if (languageCode.match(/^en/)) {
-    strings = _.extend(strings, en_us);
-  }
-  // else if (languageCode.match(/^ja/)) {
-  //   strings = _.extend(strings, ja);
-  // }
-  else if (
-    languageCode.match(/^zh-CN/) ||
-    languageCode.match(/^zh-SG/) ||
-    languageCode.match(/^zh-MY/)) {
-    strings = _.extend(strings, zh_Hans);
-  }
-  else if (languageCode.match(/^zh/)) { // TW, MO and HK
-    strings = _.extend(strings, zh_Hant);
-  }
+  prioritized.forEach(function(languageCode) {
+    if (languageCode.match(/^en/)) {
+      _.extend(strings, en_us);
+    }
+    // else if (languageCode.match(/^ja/)) {
+    //   strings = _.extend(strings, ja);
+    // }
+    else if (
+      languageCode.match(/^zh-CN/) ||
+      languageCode.match(/^zh-SG/) ||
+      languageCode.match(/^zh-MY/)) {
+      _.extend(strings, zh_Hans);
+    }
+    else if (languageCode.match(/^zh/)) { // TW, MO and HK
+      _.extend(strings, zh_Hant);
+    }
+  });
 });
+
 
 module.exports = strings;
