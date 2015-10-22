6d088513c361f4fb9ba201b6f9df0bb2934383b9
Mike Bjorkegren <m@bjorkegren.com>
2015-10-22 02:25:01 -0700

    catch unhandled errors

--- server.js
+++ server.js
@@ -11622,13 +11622,19 @@ function fetchIndexForConversation(req, res) {
     }
     var acceptLanguage = req.headers["accept-language"] || req.headers["Accept-Language"];
 
+
+
     // Kick off requests to twitter and FB to get the share counts.
     // This will be nice because we cache them so it will be fast when
     // client requests these later.
     // TODO actually store these values in a cache that is shared between
     // the servers, probably just in the db.
-    getTwitterShareCountForConversation(conversation_id);
-    getFacebookShareCountForConversation(conversation_id);
+    getTwitterShareCountForConversation(conversation_id).catch(function (err) {
+        console.log("fetchIndexForConversation/getTwitterShareCountForConversation err " + err);
+    });
+    getFacebookShareCountForConversation(conversation_id).catch(function (err) {
+        console.log("fetchIndexForConversation/getFacebookShareCountForConversation err " + err);
+    });
 
     getZidFromConversationId(conversation_id).then(function(zid) {
         return Promise.all([
