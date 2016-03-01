ecd24dc7105b3024b88cc1182da458572cb8848a
Mike Bjorkegren <m@bjorkegren.com>
2016-02-29 23:29:23 -0800

    add error string

--- src/strings/en_us.js
+++ src/strings/en_us.js
@@ -6,7 +6,7 @@ s.polis_err_param_parse_failed_email = "Please enter a valid email address.";
 s.polis_err_reg_bad_email = "Please enter a valid email address.";
 s.polis_err_param_parse_failed_password = "Please enter a valid password."
 s.polis_err_login_unknown_user_or_password_noresults = "Login failed: invalid username/password combination.";
-
+s.polis_err_login_unknown_user_or_password = "Login failed: invalid username/password combination.";
 
 
 
