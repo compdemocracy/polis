2780803825b8d7bd5b2887a2bef4d8aa215df555
Mike Bjorkegren <m@bjorkegren.com>
2016-03-02 13:15:53 -0800

    add error string

--- src/strings/en_us.js
+++ src/strings/en_us.js
@@ -1,13 +1,13 @@
 
 var s = {};
 
-// login strings
+// login/createuser strings
 s.polis_err_param_parse_failed_email = "Please enter a valid email address.";
 s.polis_err_reg_bad_email = "Please enter a valid email address.";
 s.polis_err_param_parse_failed_password = "Please enter a valid password."
 s.polis_err_login_unknown_user_or_password_noresults = "Login failed: invalid username/password combination.";
 s.polis_err_login_unknown_user_or_password = "Login failed: invalid username/password combination.";
-
-
+s.polis_err_reg_user_with_that_email_exists = "Email address already in use, Try logging in instead.";
+s.polis_err_reg_need_name = "Please include your name.";
 
 export default s;
