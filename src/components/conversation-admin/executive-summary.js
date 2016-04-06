df5d6e64c91c4b7a5d7a97c1f8dc9ffc97be5ccd
Colin Megill <colinmegill@gmail.com>
2016-04-06 10:24:45 -0700

    executive summary rough draft

--- src/components/conversation-admin/executive-summary.js
+++ src/components/conversation-admin/executive-summary.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import Radium from 'radium';
-// import _ from 'lodash';
-// import Flex from './framework/flex';
-// import { connect } from 'react-redux';
-// import { FOO } from '../actions';
+import React from "react";
+import Radium from "radium";
+import _ from "lodash";
+// import Flex from "./framework/flex";
+import { connect } from "react-redux";
+import {
+  populateCommentsStore,
+  populateMathStore,
+  populateParticipantsStore
+} from "../../actions";
+import ParticipantHeader from "./participant-header";
 
-
-// @connect(state => {
-//   return state.FOO;
-// })
+@connect((state) => {
+  return {
+    comments: state.comments,
+    math: state.math,
+    participants: state.participants
+  };
+})
 @Radium
 class ExecutiveSummary extends React.Component {
   constructor(props) {
@@ -19,15 +27,22 @@ class ExecutiveSummary extends React.Component {
   }
   static propTypes = {
     /* react */
-    // dispatch: React.PropTypes.func,
+    dispatch: React.PropTypes.func,
     params: React.PropTypes.object,
     routes: React.PropTypes.array,
     /* component api */
     style: React.PropTypes.object,
+    math: React.PropTypes.object,
+    comments: React.PropTypes.object
     // foo: React.PropTypes.string
   }
   static defaultProps = {
     // foo: "bar"
+  }
+  componentWillMount() {
+    this.props.dispatch(populateCommentsStore(this.props.params.conversation_id));
+    this.props.dispatch(populateMathStore(this.props.params.conversation_id));
+    this.props.dispatch(populateParticipantsStore(this.props.params.conversation_id));
   }
   getStyles() {
     return {
@@ -42,74 +57,87 @@ class ExecutiveSummary extends React.Component {
         WebkitBoxShadow: "3px 3px 6px -1px rgba(220,220,220,1)",
         BoxShadow: "3px 3px 6px -1px rgba(220,220,220,1)"
       },
+      comment: {
+        fontWeight: 300,
+        borderLeft: "3px solid rgb(150,150,150)",
+        paddingLeft: 10,
+        marginLeft: 20
+      }
     };
   }
-  render() {
+  getConsensusAgreeComments() {
+    const comments = this.props.comments.comments;
+    const math = this.props.math.math;
     const styles = this.getStyles();
+    return math.consensus.agree.map((comment) => {
+      return (
+        <div>
+          {
+            comments[comment.tid].social ?
+              <ParticipantHeader {...comments[comment.tid].social} /> :
+                "Anonymous"
+          }
+          <p style={styles.comment}>{comments[comment.tid].txt}</p>
+        </div>
+      );
+    });
+  }
+  getConsensusDisagreeComments() {
+    const comments = this.props.comments.comments;
+    const math = this.props.math.math;
+    const styles = this.getStyles();
+    return math.consensus.disagree.map((comment) => {
+      return (
+        <div>
+          {
+            comments[comment.tid].social ?
+              <ParticipantHeader {...comments[comment.tid].social} /> :
+                "Anonymous"
+          }
+          <p style={styles.comment}>{comments[comment.tid].txt}</p>
+        </div>
+      );
+    });
+  }
+  getGroupComments() {
+    // const comments = this.props.comments.comments;
+    const math = this.props.math.math;
+    return math["group-clusters"].map((group, i) => {
+      return (
+        <p> Group {i} </p>
+      );
+    });
+  }
+  summary() {
+    const comments = this.props.comments.comments;
+    const math = this.props.math.math;
+    return (
+      <div>
+        <p> Out of {math["n-cmts"]} comments submitted, the most agreed upon comment[s]: </p>
+        {this.getConsensusAgreeComments()}
+        The most disagreed upon comment[s]:
+        {this.getConsensusDisagreeComments()}
+        There were {math["group-clusters"].length} groups
+        {this.getGroupComments()}
+      </div>
+    );
+  }
+  render() {
+    console.log('summary',this.props)
+    const styles = this.getStyles();
+    const comments = this.props.comments.comments;
+    const math = this.props.math.math;
     return (
       <div style={[
         styles.card,
         this.props.style
       ]}>
-        There was broad consensus across all readers
+        {this.props.math.loading || this.props.comments.loading ? "Loading... " : ""}
+        {this.props.math.error || this.props.comments.error ? "Error loading data" : ""}
+        {!this.props.math.loading && !this.props.comments.loading ? this.summary() : ""}
       </div>
     );
   }
 }
 
 export default ExecutiveSummary;
-
-/*
-
-propTypes: {
-    // You can declare that a prop is a specific JS primitive. By default, these
-    // are all optional.
-    optionalArray: React.PropTypes.array,
-    optionalBool: React.PropTypes.bool,
-    optionalFunc: React.PropTypes.func,
-    optionalNumber: React.PropTypes.number,
-    optionalObject: React.PropTypes.object,
-    optionalString: React.PropTypes.string,
-
-    // Anything that can be rendered: numbers, strings, elements or an array
-    // (or fragment) containing these types.
-    optionalNode: React.PropTypes.node,
-
-    // A React element.
-    optionalElement: React.PropTypes.element,
-
-    // You can also declare that a prop is an instance of a class. This uses
-    // JS's instanceof operator.
-    optionalMessage: React.PropTypes.instanceOf(Message),
-
-    // You can ensure that your prop is limited to specific values by treating
-    // it as an enum.
-    optionalEnum: React.PropTypes.oneOf(['News', 'Photos']),
-
-    // An object that could be one of many types
-    optionalUnion: React.PropTypes.oneOfType([
-      React.PropTypes.string,
-      React.PropTypes.number,
-      React.PropTypes.instanceOf(Message)
-    ]),
-
-    // An array of a certain type
-    optionalArrayOf: React.PropTypes.arrayOf(React.PropTypes.number),
-
-    // An object with property values of a certain type
-    optionalObjectOf: React.PropTypes.objectOf(React.PropTypes.number),
-
-    // An object taking on a particular shape
-    optionalObjectWithShape: React.PropTypes.shape({
-      color: React.PropTypes.string,
-      fontSize: React.PropTypes.number
-    }),
-
-    // You can chain any of the above with `isRequired` to make sure a warning
-    // is shown if the prop isn't provided.
-    requiredFunc: React.PropTypes.func.isRequired,
-
-    // A value of any data type
-    requiredAny: React.PropTypes.any.isRequired,
-
-*/
