8f3c3fe8d7f1b3b3356a1d15753378461681c546
Mike Bjorkegren <m@bjorkegren.com>
2016-05-21 15:34:22 -0700

    Fix missing social profiles on comments

--- server.js
+++ server.js
@@ -6043,7 +6043,6 @@ Email verified! You can close this tab or hit the back button.
         return getSocialInforForUsers(uids).then(function(socialInfos) {
           let uidToSocialInfo = {};
           socialInfos.forEach(function(info) {
-            info.uid = info.coalesced_uid;
             // whitelist properties to send
             let infoToReturn = _.pick(info, [
               // fb
@@ -9359,6 +9358,7 @@ Email verified! You can close this tab or hit the back button.
   // }
 
   function getSocialInforForUsers(uids) {
+    uids = _.uniq(uids);
     uids.forEach(function(uid) {
       if (!_.isNumber(uid)) {
         throw "polis_err_123123_invalid_uid got:" + uid;
@@ -9368,7 +9368,7 @@ Email verified! You can close this tab or hit the back button.
       return Promise.resolve([]);
     }
     let uidString = uids.join(",");
-    return pgQueryP_metered_readOnly("getSocialInforForUsers", "with fb as (select * from facebook_users where uid in (" + uidString + ")), tw as (select * from twitter_users where uid in (" + uidString + ")) select * from fb full outer join tw on tw.uid = fb.uid;", []);
+    return pgQueryP_metered_readOnly("getSocialInforForUsers", "with fb as (select * from facebook_users where uid in (" + uidString + ")), tw as (select * from twitter_users where uid in (" + uidString + ")) select *, coalesce(fb.uid, tw.uid) as uid from fb full outer join tw on tw.uid = fb.uid;", []);
   }
 
   function updateVoteCount(zid, pid) {
