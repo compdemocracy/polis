81dec5511ee7337299484b2662dc29882899f808
Ricardo Poppi <ricardo@cidadedemocratica.org.br>
2016-09-30 19:16:05 -0300

    Adding Brazilian Portuguese language file for ClientAdmin strings

---
+++ src/strings/pt_br.js
@@ -0,0 +1,21 @@
+var s = {};
+
+// login/createuser strings
+s.polis_err_param_parse_failed_email = "Por favor insira um endereço de email válido.";
+s.polis_err_reg_bad_email = "Por favor insira um endereço de email válido.";
+s.polis_err_param_parse_failed_password = "Por favor insira uma senha válida."
+s.polis_err_login_unknown_user_or_password_noresults = "Login falhou: usuário/senha inválida.";
+s.polis_err_login_unknown_user_or_password = "Login falhou: usuário/senha inválida.";
+s.polis_err_reg_user_with_that_email_exists = "Email já cadastrado, tente fazer login.";
+s.polis_err_reg_need_name = "Por favor inclua seu nome.";
+
+s.polis_err_post_comment_duplicate = "Erro ao publicar. Este comentário já existe!";
+s.waitinglist_add_success = "Você já foi incluído na lista de email! Manteremos contato.";
+
+s.polis_err_fetching_tweet = "Erro buscando tuíte. Verifique se esse link é mesmo de um tuíte.";
+
+s.share_but_no_comments_warning = "Essa discussão não tem comentários. Nós recomendamos adicionar alguns comentários antes de convidar participantes. Isso irá ajudar os participantes a começar. No menu da direita, vá para a opção 'comments' e depois 'seed'.";
+s.share_but_no_visible_comments_warning = "Essa discussão não tem comentários visíveis. Nós recomendamos acrescentar alguns comentários ou moderar comentários que estejam na fila antes de convidar os participantes, já que isso os ajudará a entender que tipo de comentários eles devem enviar.";
+
+
+export default s;
