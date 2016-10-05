a2f3bd4e695aa12a5c26d5013d0ab7b08580fd0c
Mike Bjorkegren <m@bjorkegren.com>
2016-10-04 20:46:01 -0700

    test for missing uid (new/anon user)

--- server.js
+++ server.js
@@ -8338,7 +8338,7 @@ Email verified! You can close this tab or hit the back button.
     return Promise.all([
       pgQueryP_readOnly("select * from conversations left join  (select uid, site_id from users) as u on conversations.owner = u.uid where conversations.zid = ($1);", [zid]),
       getConversationHasMetadata(zid),
-      getUserInfoForUid2(uid),
+      (_.isUndefined(uid) ? Promise.resolve({}) : getUserInfoForUid2(uid)),
     ]).then(function(results) {
       let conv = results[0] && results[0][0];
       let convHasMetadata = results[1];
