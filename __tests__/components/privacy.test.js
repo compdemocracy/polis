043c43dcdcb52ee07290573e6ebd5e93a99dded0
DylanGuedes <djmgguedes@gmail.com>
2016-11-10 16:40:43 -0200

    Reverts privacy component changes
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- __tests__/components/privacy.test.js
+++ __tests__/components/privacy.test.js
@@ -7,21 +7,4 @@ describe('Privacy class', function() {
     // expect the render() method to be defined in the Privacy class
     expect(privacy.render).toBeDefined();
   });
-
-  it('should call the correct text', () => {
-    let privacy = new Privacy;
-
-    // stub the personalInformation() method from privacy instance to be a jest mock
-    privacy.personalInformation = jest.fn();
-    privacy.passiveInformationCollection = jest.fn();
-    privacy.collectionOfInformation = jest.fn();
-
-    // call the render method
-    privacy.render();
-
-    // expect render (from previous line) to call the correct text functions
-    expect(privacy.personalInformation).toHaveBeenCalled();
-    expect(privacy.passiveInformationCollection).toHaveBeenCalled();
-    expect(privacy.collectionOfInformation).toHaveBeenCalled();
-  });
 });
