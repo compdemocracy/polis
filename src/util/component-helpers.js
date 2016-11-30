c0127c1c4f027309af1ca19550599bd06955091c
Mike Bjorkegren <m@bjorkegren.com>
2016-11-29 20:40:48 -0800

    temp fix

--- src/util/component-helpers.js
+++ src/util/component-helpers.js
@@ -3,7 +3,8 @@
 const helpers = {};
 
 helpers.shouldShowPermissionsError = (props) => {
-  return props.zid_metadata && !props.zid_metadata.is_owner && !props.zid_metadata.is_mod;
+  return false;
+  //return props.zid_metadata && !props.zid_metadata.is_owner && !props.zid_metadata.is_mod;
 };
 
 
