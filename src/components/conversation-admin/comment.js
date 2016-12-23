5783a379b498656393c97c233d91a454462cfae2
Colin Megill <colinmegill@gmail.com>
2016-12-22 22:25:53 -0500

    comment gets barchart

--- src/components/conversation-admin/comment.js
+++ src/components/conversation-admin/comment.js
@@ -5,7 +5,13 @@ import Radium from "radium";
 import Flex from "../framework/flex";
 import Button from "../framework/generic-button";
 import ParticipantHeader from "./participant-header";
+import { connect } from "react-redux";
 
+@connect((state) => {
+  return {
+    conversation: state.zid_metadata.zid_metadata
+  }
+})
 @Radium
 class Comment extends React.Component {
   static propTypes = {
@@ -80,9 +86,61 @@ class Comment extends React.Component {
     }
     return "";
   }
+  createBarChart() {
+
+    const rectStartX = 70;
+    const barHeight = 12;
+    const leftTextOffset = 63;
+
+    const arr = [
+      {
+        label: "voted",
+        percent: this.props.comment.count / this.props.conversation.participant_count * 100,
+        fill: "rgb(180,180,180)"
+      },
+      {
+        label: "agreed",
+        percent: this.props.comment.agree_count / this.props.comment.count * 100,
+        fill: "rgb(46, 204, 113)"
+      },
+      {
+        label: "disagreed",
+        percent: this.props.comment.disagree_count / this.props.comment.count * 100,
+        fill: "rgb(231, 76, 60)"
+      },
+      {
+        label: "passed",
+        percent: this.props.comment.pass_count / this.props.comment.count * 100,
+        fill: "rgb(230,230,230)"
+      }
+    ];
+    return (
+      <g>
+        {arr.map((d, i) => {
+          return (
+            <g key={i}>
+              <text x={leftTextOffset} y={(i+1) * 15} fontFamily="Helvetica" fontSize="10" textAnchor={"end"}>
+                {d.label}
+              </text>
+              <rect
+                width={d.percent}
+                height={barHeight}
+                x={rectStartX}
+                y={((i+1) * 15) - 9}
+                fill={d.fill}/>
+              <text x={leftTextOffset + d.percent + 10} y={(i+1) * 15} fontFamily="Helvetica" fontSize="10" textAnchor={"start"}>
+                {Math.floor(d.percent) + "%"}
+              </text>
+            </g>
+          )
+        })}
+      </g>
+    )
+  }
   render() {
     const styles = this.getStyles();
     const showAsAnon = !this.props.comment.social || this.props.comment.anon || this.props.comment.is_seed;
+
     return (
       <div style={styles.card}>
         <Flex
@@ -95,20 +153,24 @@ class Comment extends React.Component {
               "Anonymous" :
               <ParticipantHeader {...this.props.comment.social} />
           }
-          <Flex grow={1}>
+          <Flex styleOverrides={{width: "100%"}}>
             <p style={styles.commentBody}>{ this.props.comment.txt }</p>
           </Flex>
-          <p style={styles.info}>
-            Created on
-            <span style={{fontWeight: 500}}> {this.getDate()}</span>,
-            voted on a total of
-            <span style={{fontWeight: 500}}> {this.props.comment.count} </span>
-            times.
-            {this.getVoteBreakdown(this.props.comment)}
-          </p>
-          <Flex>
+          <Flex justifyContent="space-between" alignItems="flex-end" styleOverrides={{width: "100%"}}>
+            <svg width={250} height={70}>
+              <line
+                x1="120"
+                y1="0"
+                x2="120"
+                y2="65"
+                strokeWidth="2"
+                stroke="rgb(245,245,245)"/>
+              {this.createBarChart()}
+            </svg>
+            <div>
             { this.props.acceptButton ? this.makeAcceptButton() : "" }
             { this.props.rejectButton ? this.makeRejectButton() : "" }
+            </div>
           </Flex>
         </Flex>
       </div>
@@ -122,4 +184,13 @@ export default Comment;
   todo
     show stats per comment
     sort by number of votes time submitted etc
+
+    <p style={styles.info}>
+      Created on
+      <span style={{fontWeight: 500}}> {this.getDate()}</span>,
+      voted on a total of
+      <span style={{fontWeight: 500}}> {this.props.comment.count} </span>
+      times.
+      {this.getVoteBreakdown(this.props.comment)}
+    </p>
 */
