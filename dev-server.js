986c20ea93704921911c1d1a349378ec69680983
Mike Bjorkegren <m@bjorkegren.com>
2017-01-03 13:27:03 -0800

    set up ajax, use port 5010

--- dev-server.js
+++ dev-server.js
@@ -7,7 +7,7 @@ var request = require("request");
 var app = express();
 var compiler = webpack(config);
 
-app.set('port', 7000);
+app.set('port', 5010);
 
 app.use('/data', express.static('data'))
 
@@ -17,6 +17,39 @@ app.use(require("webpack-dev-middleware")(compiler, {
 }));
 
 app.use(require("webpack-hot-middleware")(compiler));
+
+
+
+
+
+
+
+const serviceUrl = process.env.SERVICE_URL ? process.env.SERVICE_URL : "https://preprod.pol.is";
+
+function proxy (req, res) {
+  const hostHeader = serviceUrl.replace(/.*\/\//,"");
+  const headers =  Object.assign(req.headers, {"origin": serviceUrl, "Origin": serviceUrl, "host": hostHeader, "Host": hostHeader});
+
+  var x = request({
+    url: serviceUrl + req.path,
+    qs: req.query,
+    headers: headers,
+    rejectUnauthorized:false,
+  });
+  req.pipe(x);
+  x.pipe(res);
+  x.on("error", function(err) {
+    console.log("error", err);
+    res.status(500).send("proxy error");
+  });
+}
+
+// proxy everything else
+app.get(/^\/api\//, proxy);
+app.put(/^\/api\//, proxy);
+app.post(/^\/api\//, proxy);
+
+
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
