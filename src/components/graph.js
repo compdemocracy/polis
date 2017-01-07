1439a480e9c3a1e8d83b070636a9563c6389d10a
Colin Megill <colinmegill@gmail.com>
2017-01-07 15:41:32 -0500

    svg

---
+++ src/components/graph.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Radium from "radium";
+// import _ from "lodash";
+// import Flex from "./framework/flex";
+
+@Radium
+class Graph extends React.Component {
+
+  render() {
+    return (
+      <svg width={1000} height={1000}>
+        
+      </svg>
+    );
+  }
+}
+
+export default Graph;
