814d7dc19a077671843546465b30630ee9de82c0
Colin Megill <colinmegill@gmail.com>
2017-01-07 20:09:48 -0500

    scatter

--- src/components/graphParticipants.js
+++ src/components/graphParticipants.js
@@ -2,11 +2,14 @@ import React from "react";
 import _ from "lodash";
 import * as globals from "./globals";
 
-const GraphParticipants = ({math, xScale, yScale}) => {
+const GraphParticipants = ({math}) => {
 
   if (!math) {
     return null
   }
+
+  const xx = d3.scaleLinear().domain([_.min(math["base-clusters"].x),_.max(math["base-clusters"].x)]).range([-(globals.side / 2 - 50), globals.side / 2 - 50]);
+  const yy = d3.scaleLinear().domain([_.min(math["base-clusters"].y),_.max(math["base-clusters"].y)]).range([-(globals.side / 2 - 50), globals.side / 2 - 50]);
 
   return (
     <g transform={`translate(${globals.side / 2},${globals.side / 2})`}>
@@ -15,8 +18,8 @@ const GraphParticipants = ({math, xScale, yScale}) => {
           r={3}
           fill={"red"}
           key={i}
-          cx={xScale(math["base-clusters"].x[cluster])}
-          cy={yScale(math["base-clusters"].y[cluster])}/>
+          cx={xx(math["base-clusters"].x[cluster])}
+          cy={yy(math["base-clusters"].y[cluster])}/>
       })}
     </g>
   );
