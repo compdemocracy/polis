7188826597d84ee4a676c9374843925cc6a09ec8
Christopher T. Small <metasoarous@gmail.com>
2017-02-02 15:46:33 -0800

    user testbench example of pure conversation update testing

--- dev/user.clj
+++ dev/user.clj
@@ -3,10 +3,15 @@
             [polismath.system :as system]
             [polismath.meta.microscope :as microscope]
             [polismath.conv-man :as conv-man]
-            [polismath.components.postgres :as postgres]))
+            [polismath.math.conversation :as conv]
+            [polismath.components.postgres :as postgres]
+            [environ.core :as env]
+            [taoensso.timbre :as log]))
 
 
 (defn get-conv
+  "For this to work, we need something like `load-conv! that doesn't just load the conversation locally, but assocs in
+  to conv man as well."
   [{:keys [zid zinvite]}]
   (let [zid (or zid (postgres/get-zid-from-zinvite (:postgres runner/system) zinvite))]
     (-> runner/system :conversation-manager :conversations deref (get zid))))
@@ -18,8 +23,23 @@
 
 (comment
   (runner/run! system/base-system)
+  (runner/stop!)
   (def args {:zid 15228})
-  (load-conv args)
-  (get-conv args)
+
+  (def conv (load-conv args))
+
+  (:config runner/system)
+
+  (let [updated-conv (conv/conv-update conv [{:zid 15228 :pid 0 :tid 0 :vote 2.0 :created (System/currentTimeMillis)}])
+        _ (log/info "First update")
+        updated-conv' (conv/conv-update updated-conv [{:zid 15228 :pid 0 :tid 0 :vote -1 :created (System/currentTimeMillis)}])
+        _ (log/info "Second update")]
+    (log/info "Conv keys:" (keys conv))
+    (log/info "Updated conv keys:" (keys updated-conv))
+    (log/info "Previous conv key:" (boolean (:conv updated-conv)))
+    (log/info "Previous conv key:" (boolean (:conv (:conv updated-conv'))))
+    (:subgroup-clusters updated-conv'))
+    ;(:repness updated-conv'))
+  ;(get-conv args)
   (-> runner/system :conversation-manager :conversations deref)
   :end)
