4c6837d92e4365fa06f75f0764e10afc76baea1b
Colin Megill <colinmegill@gmail.com>
2017-02-21 22:11:22 -0800

    move scales out of child component

--- src/components/graphParticipants.js
+++ src/components/graphParticipants.js
@@ -2,7 +2,7 @@ import React from "react";
 import _ from "lodash";
 import * as globals from "./globals";
 
-const GraphParticipants = ({points, xx, yy}) => {
+const GraphParticipants = ({points}) => {
 
   if (!points) {
     return null
@@ -15,18 +15,11 @@ const GraphParticipants = ({points, xx, yy}) => {
           r={6}
           fill={globals.groupColor(pt.gid)}
           key={i}
-          cx={xx(pt.x)}
-          cy={yy(pt.y)}/>
+          cx={pt.x}
+          cy={pt.y}/>
       })}
     </g>
   );
 }
-      // <circle
-      //     r={8}
-      //     stroke={"purple"}
-      //     fill={"rgba(0,0,0,0)"}
-      //     key={9999}
-      //     cx={xx(0)}
-      //     cy={yy(0)}/>
 
 export default GraphParticipants;
