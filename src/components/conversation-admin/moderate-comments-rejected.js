dc3d931be1d8b46e079e2be8001e5d92e7ff692a
Mike Bjorkegren <m@bjorkegren.com>
2017-03-06 09:56:56 -0800

    Add button for accepting rejected comments
    The math worker now supports this

--- src/components/conversation-admin/moderate-comments-rejected.js
+++ src/components/conversation-admin/moderate-comments-rejected.js
@@ -1,8 +1,8 @@
 // Copyright (C) 2012-present, Polis Technology Inc. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import React from "react";
+import { changeCommentStatusToAccepted } from '../../actions';
 import { connect } from "react-redux";
-import { populateRejectedCommentsStore } from '../../actions'
 import Radium from "radium";
 import _ from "lodash";
 import Comment from "./comment";
@@ -12,16 +12,25 @@ import Flex from "../framework/flex";
 @connect(state => state.mod_comments_rejected)
 @Radium
 class ModerateCommentsRejected extends React.Component {
+
+  onCommentAccepted(comment) {
+    this.props.dispatch(changeCommentStatusToAccepted(comment))
+  }
+
   createCommentMarkup() {
     const comments = this.props.rejected_comments.map((comment, i)=>{
       return (
         <Comment
           key={i}
+          acceptButton
+          acceptButtonText="accept"
+          acceptClickHandler={this.onCommentAccepted.bind(this)}
           comment={comment}/>
       )
     })
     return comments;
   }
+
   renderSpinner() {
     return (
       <Flex>
@@ -34,6 +43,7 @@ class ModerateCommentsRejected extends React.Component {
       </Flex>
     )
   }
+
   render() {
     return (
       <div>
