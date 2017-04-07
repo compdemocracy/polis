973fe00db245b7738e83dcd8340be8f856d91966
Colin Megill <colinmegill@gmail.com>
2017-04-06 23:05:23 -0700

    move collection view hulls to hulls component

--- vis2/components/hull.js
+++ vis2/components/hull.js
@@ -8,9 +8,27 @@ const Hull = ({hull}) => {
   return (
     <path
       d={pathString}
-      fill={globals.groupColor(hull.group[0].gid)}
+      fill={/*globals.groupColor(hull.group[0].gid)*/ "rgb(200,200,200)"}
       fillOpacity={.4}/>
   );
 };
 
-export default Hull;
+const Hulls = ({hulls, showOnlyGroup}) => {
+  return (
+    <g>
+      {
+        hulls.map((hull) => {
+          let gid = hull.group[0].gid;
+          if (_.isNumber(showOnlyGroup)) {
+            if (gid !== showOnlyGroup) {
+              return "";
+            }
+          }
+          return <Hull key={gid} hull={hull}/>
+        })
+      }
+    </g>
+  )
+}
+
+export default Hulls;
