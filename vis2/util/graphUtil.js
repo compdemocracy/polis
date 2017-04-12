059d9e415a48fcd5e4045178ae0f3b0346e1dd87
Mike Bjorkegren <m@bjorkegren.com>
2017-04-11 21:18:19 -0700

    use individual projection

--- vis2/util/graphUtil.js
+++ vis2/util/graphUtil.js
@@ -44,17 +44,26 @@ const graphUtil = (comments, math, badTids) => {
       }
     }
 
+
+
+
     // participants
-    const clusterXs = math["base-clusters"].x;
-    const clusterYs = math["base-clusters"].y;
+    const clusterXs = [];//math["base-clusters"].x;
+    const clusterYs = []; //math["base-clusters"].y;
+    const proj = math.proj;
+    for (let i = 0; i < proj.length; i++) {
+      clusterXs.push(proj[i][0]);
+      clusterYs.push(proj[i][1]);
+    }
+
     const bids = math["base-clusters"].id;
     let baseClusters = [];
     for (let i = 0; i < clusterXs.length; i++) {
       baseClusters.push({
         x: clusterXs[i],
         y: clusterYs[i],
-        id: bids[i],
-        gid: baseClusterIdToGid(bids[i]),
+        id: i, //bids[i],
+        gid: 0, //baseClusterIdToGid(bids[i]),
       });
       allXs.push(clusterXs[i]);
       allYs.push(clusterYs[i]);
