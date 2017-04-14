fd3c8b7e5e57707d8f22fecd74aa9059a736b483
Colin Megill <colinmegill@gmail.com>
2017-04-14 11:15:25 -0700

    clip path on ptptois working

--- vis2/components/graphParticipants.js
+++ vis2/components/graphParticipants.js
@@ -2,39 +2,54 @@ import React from "react";
 import _ from "lodash";
 import * as globals from "./globals";
 
+const Participant = ({pt, ptptois, ptptoiScaleFactor}) => {
+  let ptptoi = ptptois[pt.bid];
+  let markup;
+  if (ptptoi) {
+    let picSize = ptptoi.picture_size;
+    markup = (
+      <g
+        transform={"translate(" + pt.x + "," + pt.y + ")"}>
+        <clipPath id={"social_image_clip_" + pt.bid}>
+          <circle cx={0} cy={0} r={10} />
+        </clipPath>
+        <image
+          clipPath={`url(#social_image_clip_${pt.bid})`}
+          x={-picSize/2 * ptptoiScaleFactor}
+          y={-picSize/2 * ptptoiScaleFactor}
+          href={ptptoi.picture}
+          width={picSize * ptptoiScaleFactor}
+          height={picSize * ptptoiScaleFactor}/>
+      </g>
+    )
+  } else {
+    markup = (<circle
+      r={4}
+      fill={/* globals.groupColor(pt.gid)*/ ptptoi ? "black" : "rgb(130,130,130)"}
+      key={pt.bid}
+      cx={pt.x}
+      cy={pt.y}/>)
+  }
+  return markup;
+}
+
 const GraphParticipants = ({points, ptptois, ptptoiScaleFactor}) => {
 
-  if (!points) {
-    return null
-  }
-// transform={`translate(${globals.side / 2},${globals.side / 2})`}>
+  if (!points) return null
+
   return (
-    <g>
-      {points.map((pt, i) => {
-        let ptptoi = ptptois[pt.bid];
-        if (ptptoi) {
-          console.log(ptptoi);
-          let picSize = ptptoi.picture_size;
-          return <g
-            transform={"translate(" + pt.x + "," + pt.y + ")"}>
-            <image
-              clipPath={"url(#clipCircleVis2)"}
-              x={-picSize/2 * ptptoiScaleFactor}
-              y={-picSize/2 * ptptoiScaleFactor}
-              href={ptptoi.picture}
-              width={picSize * ptptoiScaleFactor}
-              height={picSize * ptptoiScaleFactor}/>
-            </g>
-        } else {
-          return <circle
-            r={4}
-            fill={/* globals.groupColor(pt.gid)*/ ptptoi ? "orange" : "rgb(130,130,130)"}
-            key={pt.bid}
-            cx={pt.x}
-            cy={pt.y}/>
-        }
-      })}
     <g id="vis2_participants">
+      {
+        points.map((pt, i) => {
+          return (
+            <Participant
+              key={pt.bid}
+              ptptois={ptptois}
+              ptptoiScaleFactor={ptptoiScaleFactor}
+              pt={pt}/>
+          )
+        })
+      }
     </g>
   );
 }
