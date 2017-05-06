7cfce963c2ca4d06e1009ebe88e2672aa44943a2
Colin Megill <colinmegill@gmail.com>
2017-05-06 16:15:19 -0700

    only show the selected comment for now

--- vis2/components/graphComments.js
+++ vis2/components/graphComments.js
@@ -18,42 +18,40 @@ class GraphComment extends React.Component {
     return x;
   }
   render() {
+    if (!this.props.isSelected) return null;
     return (
       <g transform={ "translate(" +
         this.props.pt.x +
         ", " +
         this.props.pt.y +
-      ")"}>
-        {
-          this.props.isSelected ?
-            <rect
-              height="20"
-              width="20"
-              x={this.getRectX()}
-              y={-13}
-              fill="none"
-              rx="3"
-              ry="3"
-              strokeWidth="2"
-              strokeDasharray="4, 7, 10, 9, 10, 7, 12, 9, 100"
-              stroke="black" /> : null
-        }
-        <text
-          onMouseEnter={this.props.handleCommentHover(this.props.pt)}
-          onMouseLeave={this.props.handleCommentHover(this.props.pt)}
-          fill="rgba(0,0,0,0.7)"
-          style={{
-            fill: "black",
-            cursor: "pointer",
-            display: "block",
-            fontFamily: "Helvetica, sans-serif",
-            fontSize: 10,
-            fontWeight: this.props.isSelected ? 700 : 500,
-          }}>
-          {/*this.props.formatTid(this.props.pt.tid)*/}
-          {this.props.pt.tid}
-        </text>
-      </g>
+        ")"}>
+          <rect
+            height="20"
+            width="20"
+            x={this.getRectX()}
+            y={-13}
+            fill="none"
+            rx="3"
+            ry="3"
+            strokeWidth="2"
+            strokeDasharray="4, 7, 10, 9, 10, 7, 12, 9, 100"
+            stroke="black" />
+          <text
+            onMouseEnter={this.props.handleCommentHover(this.props.pt)}
+            onMouseLeave={this.props.handleCommentHover(this.props.pt)}
+            fill="rgba(0,0,0,0.7)"
+            style={{
+              fill: "black",
+              cursor: "pointer",
+              display: "block",
+              fontFamily: "Helvetica, sans-serif",
+              fontSize: 10,
+              fontWeight: this.props.isSelected ? 700 : 500,
+            }}>
+            {/*this.props.formatTid(this.props.pt.tid)*/}
+            {this.props.pt.tid}
+          </text>
+        </g>
     )
   }
 }
