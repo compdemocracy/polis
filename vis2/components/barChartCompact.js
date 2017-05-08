4720790024716883f5fa31debe48357e23f1a5ac
Colin Megill <colinmegill@gmail.com>
2017-05-08 15:48:17 -0700

    did not vote bar

--- vis2/components/barChartCompact.js
+++ vis2/components/barChartCompact.js
@@ -13,17 +13,17 @@ const BarChartCompact = ({selectedComment, groupVotes, translate}) => {
   let totalVotes = agrees + disagrees + passes;
   let nMembers = groupVotes["n-members"];
 
-  const agree = agrees / totalVotes * 100;
-  const disagree = disagrees / totalVotes * 100;
-  const pass = passes / totalVotes * 100;
-
-  console.log(agree, disagree, pass)
+  const agree = agrees / nMembers * 100;
+  const disagree = disagrees / nMembers * 100;
+  const pass = passes / nMembers * 100;
+  const blank = nMembers - sawTheComment / nMembers * 100;
 
   return (
     <g transform={translate ? translate : "translate(100, 100)"}>
       <rect x={0} width={agree} height={10} fill={"rgb(46, 204, 113)"} />
       <rect x={agree} width={disagree} height={10} fill={"rgb(231, 76, 60)"} />
       <rect x={agree + disagree} width={pass} height={10} fill={"rgb(230,230,230)"} />
+      <rect x={agree + disagree + pass} y={2} width={blank} height={8} fill={"white"} stroke={"rgb(130,130,130)"} />
     </g>
   )
 };
