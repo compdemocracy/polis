e6a8f97e59af2075aab4b4434b87f3a4234df16c
Mike Bjorkegren <m@bjorkegren.com>
2017-05-09 22:41:15 -0700

    cleanup barchart

--- vis2/components/barChartsForGroupVotes.js
+++ vis2/components/barChartsForGroupVotes.js
@@ -62,55 +62,12 @@ const BarChartsForGroupVotes = ({
 
 
 
-  const translations = {
-    nw: [40, 0],
-    ne: [40, 650],
-    sw: [500, 10],
-    se: [500, 650],
-  };
-
-  let cornerKeys = _.keys(groupCornerAssignments)
-  function getCorner(gid) {
-    for (let i = 0; i < cornerKeys.length; i++) {
-      let key = cornerKeys[i];
-      if (groupCornerAssignments[key].gid === gid) {
-        return {
-          corner: key,
-          target: groupCornerAssignments[key].pos,
-        };
-      }
-    }
-    console.error('no corner');
-    return null;
-  }
-
   const drawBarChartsForGroupVotesOnSelectedComment = () => {
     let arr = []
     _.each(groups, (group, i) => {
-      let {
-        corner,
-        target,
-      } = getCorner(group.id);
-      let translation = translations[corner];
-      let yOffset = 0;
-      if (corner === "nw" || corner === "sw") { /* WARNING! this is reversed for some reason, this should be ne not sw. */
-        yOffset = 70;
-      }
-      // else if (corner === "sw" || corner === "se") {
-      //   yOffset = -200;
-      // }
 
       const closestPair = getLabelAnchorForHull(hullElems[group.id]);
 
-      // <line
-      //   x1={translation[0] + 100}
-      //   y1={translation[1] + yOffset}
-      //   x2={closestPair[0]}
-      //   y2={closestPair[1]}
-      //   style={{
-      //     stroke: "rgb(130,130,130)",
-      //     strokeWidth:"1"
-      //   }}/>
       arr.push(
         <g key={group.id}>
           <BarChartCompact
@@ -118,7 +75,6 @@ const BarChartsForGroupVotes = ({
             selectedComment={selectedComment}
             groupVotes={group /* hardcode first group for debug */}
             translate={"translate(" + closestPair[0] + "," + closestPair[1] + ")"}
-            target={target}
             ptptCount={"ptptCount doesn't matter and isn't used because this barchart is for a group, not global"}/>
         </g>
       )
