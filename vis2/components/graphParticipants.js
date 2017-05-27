0f1d14d452377a15c468b607747f454e1ae423a3
Colin Megill <colinmegill@gmail.com>
2017-05-26 19:35:04 -0700

    smooth transitions on ptptois

--- vis2/components/graphParticipants.js
+++ vis2/components/graphParticipants.js
@@ -1,16 +1,27 @@
 import React from "react";
 import _ from "lodash";
 import * as globals from "./globals";
+import {VictoryAnimation} from "victory";
 
-const Participant = ({ptpt, ptptoiScaleFactor}) => {
+const Participant = ({ptpt, ptptoiScaleFactor, tweenX, tweenY}) => {
+  console.log(ptpt)
   let picSize = ptpt.picture_size;
   return (
     <g
-      transform={"translate(" + ptpt.x + "," + ptpt.y + ")"}>
+      transform={"translate(" + tweenX + "," + tweenY + ")"}>
       <clipPath id={"social_image_clip_" + ptpt.bid}>
         <circle cx={0} cy={0} r={10} />
       </clipPath>
-      {ptpt.isSelf ? <circle cx={0} cy={0} r={10} stroke={"#03a9f4"} strokeWidth={4}/> : ""}
+      {
+        ptpt.isSelf ?
+          <circle
+            cx={0}
+            cy={0}
+            r={10}
+            stroke={"#03a9f4"}
+            strokeWidth={4}/> :
+          ""
+      }
       <image
         clipPath={`url(#social_image_clip_${ptpt.bid})`}
         x={-picSize/2 * ptptoiScaleFactor}
@@ -40,10 +51,22 @@ const GraphParticipants = ({points, ptptois, ptptoiScaleFactor}) => {
       {
         ptptois.map((ptpt, i) => {
           return (
-            <Participant
+            <VictoryAnimation
+              easing={"quadOut"}
+              duration={1500}
               key={ptpt.bid*100000 + ptpt.pid}
-              ptpt={ptpt}
-              ptptoiScaleFactor={ptptoiScaleFactor}/>
+              data={{tweenX: ptpt.x, tweenY: ptpt.y}}>
+                {(tweenedProps, animationInfo) => {
+                  // if (animationInfo.animating && animationInfo.progress < 1) {
+                    return  <Participant
+                      tweenX={tweenedProps.tweenX}
+                      tweenY={tweenedProps.tweenY}
+                      key={ptpt.bid*100000 + ptpt.pid}
+                      ptpt={ptpt}
+                      ptptoiScaleFactor={ptptoiScaleFactor}/>
+                  // }
+                }}
+            </VictoryAnimation>
           )
         })
       }
