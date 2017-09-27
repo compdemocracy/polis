7aa9a85645c9ca52eb8db6ca35030fa40d232c93
Mike Bjorkegren <m@bjorkegren.com>
2017-09-27 11:19:51 -0700

    fix order in sql file

--- postgres/db_setup_draft.sql
+++ postgres/db_setup_draft.sql
@@ -84,28 +84,6 @@ CREATE INDEX users_uid_idx ON users USING btree (uid);
 -- alter table users add constraint users_site_id_index UNIQUE(site_id);
 
 
-CREATE TABLE twitter_users (
-    uid INTEGER NOT NULL REFERENCES users(uid),
-    twitter_user_id BIGINT NOT NULL,
-
-    -- Fields from here: https://api.twitter.com/1.1/users/lookup.json
-    -- NOTE: there are more fields we could fetch
-    -- [{"id":1131541,"id_str":"1131541","name":"mbjorkegren","screen_name":"mbjorkegren","location":"","profile_location":null,"description":"","url":null,"entities":{"description":{"urls":[]}},"protected":false,"followers_count":23,"friends_count":47,"listed_count":0,"created_at":"Wed Mar 14 01:52:44 +0000 2007","favourites_count":81,"utc_offset":-28800,"time_zone":"Pacific Time (US & Canada)","geo_enabled":false,"verified":false,"statuses_count":66,"lang":"en","status":{"created_at":"Wed Dec 19 00:58:20 +0000 2012","id":281201767100858369,"id_str":"281201767100858369","text":"Test http:\/\/t.co\/7D0KUDOj Two Systems","source":"\u003ca href=\"https:\/\/kindle.amazon.com\" rel=\"nofollow\"\u003eKindle\u003c\/a\u003e","truncated":false,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":null,"in_reply_to_user_id_str":null,"in_reply_to_screen_name":null,"geo":null,"coordinates":null,"place":null,"contributors":null,"retweet_count":0,"favorite_count":0,"entities":{"hashtags":[],"symbols":[],"user_mentions":[],"urls":[{"url":"http:\/\/t.co\/7D0KUDOj","expanded_url":"http:\/\/amzn.com\/k\/3a1lRsZGQzuI9xucpcrVNA","display_url":"amzn.com\/k\/3a1lRsZGQzuI\u2026","indices":[5,25]}]},"favorited":false,"retweeted":false,"possibly_sensitive":false,"lang":"en"},"contributors_enabled":false,"is_translator":false,"is_translation_enabled":false,"profile_background_color":"9AE4E8","profile_background_image_url":"http:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png","profile_background_image_url_https":"https:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png","profile_background_tile":false,"profile_image_url":"http:\/\/pbs.twimg.com\/profile_images\/2293381619\/image_normal.jpg","profile_image_url_https":"https:\/\/pbs.twimg.com\/profile_images\/2293381619\/image_normal.jpg","profile_link_color":"0000FF","profile_sidebar_border_color":"87BC44","profile_sidebar_fill_color":"E0FF92","profile_text_color":"000000","profile_use_background_image":true,"default_profile":false,"default_profile_image":false,"following":false,"follow_request_sent":false,"notifications":false}]
-    screen_name VARCHAR(999) NOT NULL,
-    name VARCHAR(9999),
-    followers_count INTEGER NOT NULL,
-    friends_count INTEGER NOT NULL, -- followees
-    verified BOOLEAN NOT NULL,
-    profile_image_url_https VARCHAR(9999),
-    location VARCHAR(9999),
-    response json,
-    modified BIGINT NOT NULL DEFAULT now_as_millis(),
-    created BIGINT NOT NULL DEFAULT now_as_millis(),
-    UNIQUE(uid), -- In theory someone could have multiple twitter accounts, so we might remove this restriction if we add support for that.
-    UNIQUE(twitter_user_id)
-);
-
-
 CREATE TABLE site_domain_whitelist(
     site_id VARCHAR(256) NOT NULL,
     domain_whitelist VARCHAR(999),
@@ -501,6 +479,26 @@ CREATE TABLE slack_bot_events (
 );
 
 
+CREATE TABLE twitter_users (
+    uid INTEGER NOT NULL REFERENCES users(uid),
+    twitter_user_id BIGINT NOT NULL,
+
+    -- Fields from here: https://api.twitter.com/1.1/users/lookup.json
+    -- NOTE: there are more fields we could fetch
+    -- [{"id":1131541,"id_str":"1131541","name":"mbjorkegren","screen_name":"mbjorkegren","location":"","profile_location":null,"description":"","url":null,"entities":{"description":{"urls":[]}},"protected":false,"followers_count":23,"friends_count":47,"listed_count":0,"created_at":"Wed Mar 14 01:52:44 +0000 2007","favourites_count":81,"utc_offset":-28800,"time_zone":"Pacific Time (US & Canada)","geo_enabled":false,"verified":false,"statuses_count":66,"lang":"en","status":{"created_at":"Wed Dec 19 00:58:20 +0000 2012","id":281201767100858369,"id_str":"281201767100858369","text":"Test http:\/\/t.co\/7D0KUDOj Two Systems","source":"\u003ca href=\"https:\/\/kindle.amazon.com\" rel=\"nofollow\"\u003eKindle\u003c\/a\u003e","truncated":false,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":null,"in_reply_to_user_id_str":null,"in_reply_to_screen_name":null,"geo":null,"coordinates":null,"place":null,"contributors":null,"retweet_count":0,"favorite_count":0,"entities":{"hashtags":[],"symbols":[],"user_mentions":[],"urls":[{"url":"http:\/\/t.co\/7D0KUDOj","expanded_url":"http:\/\/amzn.com\/k\/3a1lRsZGQzuI9xucpcrVNA","display_url":"amzn.com\/k\/3a1lRsZGQzuI\u2026","indices":[5,25]}]},"favorited":false,"retweeted":false,"possibly_sensitive":false,"lang":"en"},"contributors_enabled":false,"is_translator":false,"is_translation_enabled":false,"profile_background_color":"9AE4E8","profile_background_image_url":"http:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png","profile_background_image_url_https":"https:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png","profile_background_tile":false,"profile_image_url":"http:\/\/pbs.twimg.com\/profile_images\/2293381619\/image_normal.jpg","profile_image_url_https":"https:\/\/pbs.twimg.com\/profile_images\/2293381619\/image_normal.jpg","profile_link_color":"0000FF","profile_sidebar_border_color":"87BC44","profile_sidebar_fill_color":"E0FF92","profile_text_color":"000000","profile_use_background_image":true,"default_profile":false,"default_profile_image":false,"following":false,"follow_request_sent":false,"notifications":false}]
+    screen_name VARCHAR(999) NOT NULL,
+    name VARCHAR(9999),
+    followers_count INTEGER NOT NULL,
+    friends_count INTEGER NOT NULL, -- followees
+    verified BOOLEAN NOT NULL,
+    profile_image_url_https VARCHAR(9999),
+    location VARCHAR(9999),
+    response json,
+    modified BIGINT NOT NULL DEFAULT now_as_millis(),
+    created BIGINT NOT NULL DEFAULT now_as_millis(),
+    UNIQUE(uid), -- In theory someone could have multiple twitter accounts, so we might remove this restriction if we add support for that.
+    UNIQUE(twitter_user_id)
+);
 
 CREATE TABLE facebook_users (
     uid INTEGER NOT NULL REFERENCES users(uid),
@@ -975,10 +973,6 @@ CREATE TABLE permanentCookieZidJoins(
     UNIQUE (zid, cookie)
 );
 
-CREATE TRIGGER pid_auto
-    BEFORE INSERT ON participants
-    FOR EACH ROW -- WHEN (NEW.pid = 0 || NEW.pid = null)
-    EXECUTE PROCEDURE pid_auto();
 
 CREATE OR REPLACE FUNCTION pid_auto()
     RETURNS trigger AS $$
@@ -1019,6 +1013,11 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql STRICT;
+
+CREATE TRIGGER pid_auto
+    BEFORE INSERT ON participants
+    FOR EACH ROW -- WHEN (NEW.pid = 0 || NEW.pid = null)
+    EXECUTE PROCEDURE pid_auto();
 
 CREATE TRIGGER pid_auto_unlock
     AFTER INSERT ON participants
