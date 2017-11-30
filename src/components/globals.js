891523cd6afaaf5ca2e0448922610fcb5f018803
Mike Bjorkegren <m@bjorkegren.com>
2017-11-30 12:19:37 -0800

    disable matrix for now

--- src/components/globals.js
+++ src/components/globals.js
@@ -70,7 +70,7 @@ export const groupLabels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 export const groupSymbols = ["○", "◆", "+", "-", "◬",  "▮", ]
 
 
-export const enableMatrix = true;
+export const enableMatrix = false;
 // export const maxCommentExtremityToShow = 2; /* naive & may not be solid. should be dynamically generated from extremity array probably to pick top 20 or something */
 export const maxCommentExtremityToShow = .6; /* naive & may not be solid. should be dynamically generated from extremity array probably to pick top 20 or something */
 export const labelPadding = 40;
