434ae57c036182a4a3e40631d4d18546022222ed
Thomas Huulbæk <huulbaek@gmail.com>
2018-01-23 14:22:50 +0100

    Update da_dk.js

--- js/strings/da_dk.js
+++ js/strings/da_dk.js
@@ -14,6 +14,8 @@ s.modSubmit = "Færdig, næste kommentar";
 
 s.x_wrote = "skrev:";
 s.x_tweeted = "tweetede:";
+s.comments_remaining = "{{num_comments}} tilbage";
+s.comments_remaining2 = "{{num_comments}} tilbage";
 s.group_123 = "Gruppe:";
 s.comment_123 = "Kommentar:";
 s.majorityOpinion = "Flertallets mening";
@@ -29,6 +31,10 @@ s.helpWhatAmISeeing = "Personer der stemmer tilsvarende grupperes. Klik på en g
 s.helpWhatDoIDoTitle = " Hvad skal jeg gøre?";
 s.helpWhatDoIDo = "Stem på andres kommentaterer ved at klikke 'enig' eller 'uenig. Skriv en kommentar (hold hver kommentar til én idé). Inviter andre venner til samtalen!";
 s.writeCommentHelpText = "Hvis dit perspektiv ikke er repræsenteret, så <strong>skriv</strong> en kommentar!</i>";
+s.helpWriteListIntro = "Hvad er en god kommentar?";
+s.helpWriteListStandalone = "Enkeltstående idé";
+s.helpWriteListRaisNew = "Rejser nye perspektiver, erfaringer eller problemstillinger";
+s.helpWriteListShort = "Klar og præcis (begrænset til 140 tegn)";
 s.heresHowGroupVoted = "Her er hvordan gruppe {{GROUP_NUMBER}} stemte:";
 s.one_person = "{{x}} person";
 s.x_people = "{{x}} personer";
@@ -46,25 +52,41 @@ s.pctDisagreedLong = "{{pct}}% af alle der stemte på kommentaren {{comment_id}}
 s.pctAgreedOfGroupLong = "{{pct}}% af gruppen {{group}} som stemte på kommentaren {{comment_id}} var enige.";
 s.pctDisagreedOfGroupLong = "{{pct}}% af gruppen {{group}}som stemte på kommentaren {{comment_id}} var uenige.";
 s.commentSent = "Kommentar sendt! Andre deltagere kan se din kommentar og være enig eller uenig i den.";
+s.commentSendFailed = "Der opstod et problem ved afsendense af kommentaren.";
+s.commentSendFailedEmpty = "Der opstod et problem ved afsendense af kommentaren - bør ikke være tom.";
+s.commentSendFailedTooLong = "Der opstod et problem ved afsendense af kommentaren - den er for lang.";
+s.commentSendFailedDuplicate = "Der opstod et problem ved afsendense af kommentaren - der eksisterer en identisk.";
+s.commentErrorDuplicate = "Kopi! Kommentaren eksisterer allerede.";
+s.commentErrorConversationClosed = "Denne samtale er lukket. Der kan ikke afgives flere kommentarer.";
+s.commentIsEmpty = "Kommentar er tom";
+s.commentIsTooLong = "Kommentar er for lang";
+s.hereIsNextStatement = "Succes. Naviger op for at se næste kommentar.";
+
 s.connectFacebook = "Forbind Facebook";
 s.connectTwitter = "Forbind Twitter";
 s.connectToPostPrompt = "Forbind en identitet for at kommentere. Vi poster ikke på din tidslinje.";
 s.connectToVotePrompt = "orbind en identitet for at kommentere. Vi poster ikke på din tidslinje.";
 s.tip = "Tip:";
+s.commentWritingTipsHintsHeader = "Råd til at skrive kommentarer";
+s.tipCharLimit = "Kommentarer er begrænset til {{char_limit}} tegn.";
 s.tipCommentsRandom = "Kommentarer vises tilfældigt. Du svarer ikke direkte til nogen.";
 s.tipOneIdea = "Bryd længere kommentarer op i flere. Det gør det nemmere for andre at stemme på din kommentar.";
 s.tipNoQuestions = "Kommentarer bør udtrykke holdninger i stedet for spørgsmål. Deltagere skal enten være enige eller uenige i dine kommentarer.";
-s.notSentSinceDemo = "(ikke rigtigt, dette er en demo)";
+s.commentTooLongByChars = "Længde på kommentar overskrider grænsen med {{CHARACTERS_COUNT}} tegn.";
+s.notSentSinceDemo = "(not really, this is a demo)";
 s.submitComment = "Indsend";
 s.tipStarred = "Markeret som vigtig.";
 s.participantHelpWelcomeText = "Velkommen til en ny slags samtale - <span style='font-weight: 700;'>stem</span> på personers holdninger og <span style='font-weight: 700;'>bidrag</span> med dine egne.";
 s.participantHelpGroupsText = "Personer der stemmer lig hinanden <span style='font-weight: 700;'>grupperes.</span> Klik på en gruppe for at se, hvilke synspunkter de deler <a style='font-weight: 700; cursor: pointer; text-decoration: underline' id='helpTextGroupsExpand'>...mere</a>";
+s.participantHelpGroupsNotYetText = "Visualisering kan først ses når 7 deltagere har stemt";
 s.helpWhatAreGroupsDetail = "<p>Du har sikkert set 'anbefalede produkter' på Amazon eller 'anbefalede film' på Netflix. Tjenesterne grupperer dig med personer, der køber eller ser lignende ting, så du får anbefalet, hvad de købte eller så.</p> <p> Når du stemmer på en kommentar, bliver du grupperet med personer der stemte ligesom dig. Du kan se grupperne her. Hver gruppe er sammensat af personer med lignende holdninger. Der kan være fascinerende indsigt i dette - gå på opdagelse og se, hvad der gør grupperne unikke. </p>";
 s.socialConnectPrompt = "Du kan forbinde for at se venner og personer du følger i visualiseringen.";
 s.connectFbButton = "Forbind med Facebook";
 s.connectTwButton = "Forbind med Twitter";
 s.polis_err_reg_fb_verification_email_sent = "Check din email for et link, så vi kan verificere dig.";
 s.polis_err_reg_fb_verification_noemail_unverified = "Din Facebook-konto er ikke verificeret, Verificer den og kom tilbage..";
+s.showTranslationButton = "Aktiver oversættelse";
+s.hideTranslationButton = "Deaktiver oversættelse";
 
 s.notificationsAlreadySubscribed = "Du abonnerer på opdateringer for denne samtale.";
 s.notificationsGetNotified = "Få notifikationer når der kommer flere kommentarer:";
@@ -78,6 +100,7 @@ s.noCommentsYouVotedOnAll = "Du har stemt på alle kommentarer.";
 s.noCommentsTryWritingOne = "Hvis du har noget at tilføje, så prøv at skrive en kommentar.";
 s.convIsClosed = "Samtalen er lukket.";
 s.noMoreVotingAllowed = "Det er ikke muligt at stemme mere.";
+
 
 s.topic_good_01 = "Hvad skal vi gøre med bordtennis-rummet?";
 s.topic_good_01_reason = "åben samtale, enhver kan have en holdning til svar på dette spørgsmål";
