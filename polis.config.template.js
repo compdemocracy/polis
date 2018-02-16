da7851806b5fe95fd0fd3e5e4b02a7982ad479a2
Mike Bjorkegren <m@bjorkegren.com>
2018-02-16 13:24:42 -0800

    add SERVICE_URL to config template

--- polis.config.template.js
+++ polis.config.template.js
@@ -19,6 +19,9 @@ module.exports = {
   SCP_SUBDIR_PREPROD: 'preprod',
   SCP_SUBDIR_PROD: 'prod',
 
+  //SERVICE_URL: 'http://localhost:5000',
+  SERVICE_URL: 'https://preprod.pol.is',
+
   UPLOADER: 'scp',
   // UPLOADER: 's3',
 };
