4c84e9ac57b681dfd91c86679a333500c9153f43
claudina sarahe <cs@misscs.me>
2018-02-22 16:35:16 -0600

    Adjust logo component height to reduce nav height

--- src/components/Core/Image/Figure.js
+++ src/components/Core/Image/Figure.js
@@ -2,16 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Figure = ({ src, height, width, className}) => {
+  
   return (
-     <figure className={className}>
+    <figure className={className}>
         <img src={src} height={height}  />
       </figure>
   );
 };
 
-Figure.propTypes = {
+Figure.PropTypes = {
   src: PropTypes.string.isRequired,
+  height : PropTypes.string,
+  width : PropTypes.string,
   className: PropTypes.string
-};
+}
 
 export default Figure;
