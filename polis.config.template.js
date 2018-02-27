876b94dd994dfee3b0ace723014795b7d49d3d5e
Mike Bjorkegren <m@bjorkegren.com>
2018-02-27 13:46:16 -0800

    move facebook app id to config

--- polis.config.template.js
+++ polis.config.template.js
@@ -12,11 +12,14 @@ module.exports = {
 
   DISABLE_INTERCOM: true,
 
+  FB_APP_ID: '661042417336977',
+
   S3_BUCKET_PROD: 'pol.is',
   S3_BUCKET_PREPROD: 'preprod.pol.is',
 
   SCP_SUBDIR_PREPROD: 'preprod',
   SCP_SUBDIR_PROD: 'prod',
+
 
   UPLOADER: 'scp',
   // UPLOADER: 's3',
