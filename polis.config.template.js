1661b4b7ff5fa22db3bdea5bf75a23153b0a7ab8
Christopher T Small <metasoarous@gmail.com>
2018-09-02 22:27:19 -0700

    Update documentation to clarify relationship between client participation server and the polisServer server

--- polis.config.template.js
+++ polis.config.template.js
@@ -10,22 +10,25 @@ module.exports = {
     ".+\\.xip\\.io$",
   ],
 
-  // Switch to localhost to forward server requests through to a local server rather than a preprod server
-  //SERVICE_URL: "http://localhost:5000",
+  // Point to a polisServer instance (local recommended for dev)
+  //SERVICE_URL: "http://localhost:5000", // local server; recommended for dev
   SERVICE_URL: "https://preprod.pol.is",
 
+  // Note that this must match the participation client port specified in polisServer instance
   PORT: 5001,
 
   DISABLE_INTERCOM: true,
 
+  // must register with facebook and get a facebook app id to use the facebook auth features
   FB_APP_ID: '661042417336977',
+
+  // For data exports
 
   S3_BUCKET_PROD: 'pol.is',
   S3_BUCKET_PREPROD: 'preprod.pol.is',
 
   SCP_SUBDIR_PREPROD: 'preprod',
   SCP_SUBDIR_PROD: 'prod',
-
 
   UPLOADER: 'scp',
   // UPLOADER: 's3',
