48d292edb2d45957dd790e96257069b66d8f3c4c
colinmegill <colinmegill@gmail.com>
2020-05-22 22:40:11 -0400

    working on router, sourcemap fixed on unminifiedPreprod build

--- client-admin/src/components/conversation-admin/moderate-people.js
+++ client-admin/src/components/conversation-admin/moderate-people.js
@@ -8,7 +8,7 @@ import NoPermission from "./no-permission";
 import Radium from "radium";
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import { populateAllParticipantStores } from "../../actions";
 
 const cardPadding = 10;
@@ -18,7 +18,7 @@ const styles = {
   container: {
     backgroundColor: "rgb(240,240,247)",
     paddingTop: 10,
-    minHeight: "100vh"
+    minHeight: "100vh",
   },
   navCard: {
     margin: 20,
@@ -26,7 +26,7 @@ const styles = {
     borderRadius: cardBorderRadius,
     padding: cardPadding,
     WebkitBoxShadow: "3px 3px 6px -1px rgba(220,220,220,1)",
-    BoxShadow: "3px 3px 6px -1px rgba(220,220,220,1)"
+    BoxShadow: "3px 3px 6px -1px rgba(220,220,220,1)",
   },
 };
 
@@ -34,13 +34,13 @@ const mapStateToProps = (state) => {
   return {
     ptpt_default: state.mod_ptpt_default,
     ptpt_featured: state.mod_ptpt_featured,
-    ptpt_hidden: state.mod_ptpt_hidden
+    ptpt_hidden: state.mod_ptpt_hidden,
   };
 };
 
 const pollFrequency = 7000;
 
-@connect(state => state.zid_metadata)
+@connect((state) => state.zid_metadata)
 @connect(mapStateToProps)
 @Radium
 class ModeratePeople extends React.Component {
@@ -48,13 +48,11 @@ class ModeratePeople extends React.Component {
     return {
       navContainer: {
         margin: "10px 20px 20px 20px",
-      }
+      },
     };
   }
   loadParticipants() {
-    this.props.dispatch(
-      populateAllParticipantStores(this.props.params.conversation_id)
-    )
+    this.props.dispatch(populateAllParticipantStores(this.props.params.conversation_id));
   }
   componentWillMount() {
     this.getParticipantsRepeatedly = setInterval(() => {
@@ -69,43 +67,43 @@ class ModeratePeople extends React.Component {
   }
   render() {
     if (ComponentHelpers.shouldShowPermissionsError(this.props)) {
-      return <NoPermission/>
+      return <NoPermission />;
     }
     const styles = this.getStyles();
-    const m = "/m/"+this.props.params.conversation_id+"/participants/";
+    const m = "/m/" + this.props.params.conversation_id + "/participants/";
     return (
       <div>
-        <Flex
-          wrap="wrap"
-          justifyContent="flex-start"
-          styleOverrides={styles.navContainer}>
+        <Flex wrap="wrap" justifyContent="flex-start" styleOverrides={styles.navContainer}>
           <NavTab
             active={this.props.routes[3].path ? false : true}
             url={`/m/${this.props.params.conversation_id}/participants/`}
             text="Default"
             number={
-              this.props.ptpt_default.default_participants ?
-                this.props.ptpt_default.default_participants.length :
-                "..."
-            }/>
+              this.props.ptpt_default.default_participants
+                ? this.props.ptpt_default.default_participants.length
+                : "..."
+            }
+          />
           <NavTab
             active={this.props.routes[3].path === "featured"}
             url={`/m/${this.props.params.conversation_id}/participants/featured`}
             text="Featured"
             number={
-              this.props.ptpt_featured.featured_participants ?
-                this.props.ptpt_featured.featured_participants.length :
-                "..."
-            }/>
+              this.props.ptpt_featured.featured_participants
+                ? this.props.ptpt_featured.featured_participants.length
+                : "..."
+            }
+          />
           <NavTab
             active={this.props.routes[3].path === "hidden"}
             url={`/m/${this.props.params.conversation_id}/participants/hidden`}
             text="Hidden"
             number={
-              this.props.ptpt_hidden.hidden_participants ?
-                this.props.ptpt_hidden.hidden_participants.length :
-                "..."
-            }/>
+              this.props.ptpt_hidden.hidden_participants
+                ? this.props.ptpt_hidden.hidden_participants.length
+                : "..."
+            }
+          />
         </Flex>
         {this.props.children}
       </div>
