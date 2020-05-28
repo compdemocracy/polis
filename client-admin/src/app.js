884a8482234ffaea8cc401ab22c93980f503992c
colinmegill <colinmegill@gmail.com>
2020-05-28 17:26:13 -0400

    sidebar with react router 5, dead code removal

--- client-admin/src/app.js
+++ client-admin/src/app.js
@@ -1,4 +1,5 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/** @jsx jsx */
 
 import React from "react";
 import { connect } from "react-redux";
@@ -6,28 +7,29 @@ import { populateUserStore } from "./actions";
 
 import Radium from "radium";
 import _ from "lodash";
-import Flex from "./components/framework/flex";
 
-import { Switch, Route, Link } from "react-router-dom";
-
-import Conversations from "./components/conversations";
-
-import PasswordReset from "./components/password-reset";
-import PasswordResetInit from "./components/password-reset-init";
-import PasswordResetInitDone from "./components/password-reset-init-done";
-import SignIn from "./components/signin";
-import SignOut from "./components/signout";
-import CreateUser from "./components/createuser";
+import { Switch, Route, Link, Redirect } from "react-router-dom";
+import { Flex, Box, jsx } from "theme-ui";
 
 /* landers */
-
 import Home from "./components/landers/home";
+import TOS from "./components/landers/tos";
+import Privacy from "./components/landers/privacy";
+import PasswordReset from "./components/landers/password-reset";
+import PasswordResetInit from "./components/landers/password-reset-init";
+import PasswordResetInitDone from "./components/landers/password-reset-init-done";
+import SignIn from "./components/landers/signin";
+import SignOut from "./components/landers/signout";
+import CreateUser from "./components/landers/createuser";
 
-import TOS from "./components/tos";
-import Privacy from "./components/privacy";
-import Integrate from "./components/integrate";
-// this may become '/' defaultview - with instructions if no stats to show
-import Account from "./components/account";
+// /conversation-admin
+import ConversationAdminContainer from "./components/conversation-admin/index";
+
+import Conversations from "./components/conversations-and-account/conversations";
+import Account from "./components/conversations-and-account/account";
+import Integrate from "./components/conversations-and-account/integrate";
+
+import InteriorHeader from "./components/interior-header";
 
 @connect((state) => {
   return state.user;
@@ -223,78 +225,64 @@ class App extends React.Component {
 
     return (
       <>
-        <li>
-          <Link to="/">conversations</Link>
-        </li>
-        <li>
-          <Link to="integrate"> integrate </Link>
-        </li>
-        <li>
-          <Link to="account"> account </Link>
-        </li>
-        <li>
-          <Link to="home"> home </Link>
-        </li>
-        <li>
-          <Link to="signin"> signin </Link>
-        </li>
-        <li>
-          <Link to="signin/*"> signin </Link>
-        </li>
-        <li>
-          <Link to="signin/**/*"> signin </Link>
-        </li>
-        <li>
-          <Link to="signout"> signout </Link>
-        </li>
-        <li>
-          <Link to="signout/*"> signout </Link>
-        </li>
-        <li>
-          <Link to="signout/**/*"> signout </Link>
-        </li>
-        <li>
-          <Link to="createuser"> createuser </Link>
-        </li>
-        <li>
-          <Link to="createuser/*"> createuser </Link>
-        </li>
-        <li>
-          <Link to="createuser/**/*"> createuser </Link>
-        </li>
-        <li>
-          <Link to="pwreset/*"> pwreset </Link>
-        </li>
-        <li>
-          <Link to="pwresetinit"> pwresetinit </Link>
-        </li>
-        <li>
-          <Link to="pwresetinit/done"> pwresetinit </Link>
-        </li>
-        <li>
-          <Link to="tos"> tos </Link>
-        </li>
-        <li>
-          <Link to="privacy"> privacy </Link>
-        </li>
         <Switch>
           <Route exact path="/home" component={Home} />
-          <Route exact path="/" component={Conversations} />
-          <Route exact path="/account" component={Account} />
-
-          <Route exact path="/integrate" component={Integrate} />
 
           <Route exact path="/signin" component={SignIn} />
           <Route exact path="/signout" component={SignOut} />
           <Route exact path="/createuser" component={CreateUser} />
           <Route exact path="/createuser/*" component={CreateUser} />
-
           <Route exact path="/createuser/**/*" component={CreateUser} />
+
           <Route exact path="/pwreset/*" component={PasswordReset} />
           <Route exact path="/pwresetinit" component={PasswordResetInit} />
           <Route exact path="/pwresetinit/done" component={PasswordResetInitDone} />
           <Route exact path="/tos" component={TOS} />
           <Route exact path="/privacy" component={Privacy} />
+
+          <InteriorHeader>
+            <Route
+              render={(routeProps) => {
+                console.log(
+                  "routeprops",
+                  routeProps,
+                  routeProps.location.pathname.split("/")[0],
+                  routeProps.location.pathname.split("/")
+                );
+                if (routeProps.location.pathname.split("/")[1] === "m") {
+                  return null;
+                }
+                return (
+                  <Flex>
+                    <Box sx={{ mr: [5], p: [4], flex: "0 0 auto" }}>
+                      <Box sx={{ mb: [3] }}>
+                        <Link sx={{ variant: "links.nav" }} to={`/`}>
+                          Conversations
+                        </Link>
+                      </Box>
+                      <Box sx={{ mb: [3] }}>
+                        <Link sx={{ variant: "links.nav" }} to={`/integrate`}>
+                          Integrate
+                        </Link>
+                      </Box>
+                      <Box sx={{ mb: [3] }}>
+                        <Link sx={{ variant: "links.nav" }} to={`/account`}>
+                          Account
+                        </Link>
+                      </Box>
+                    </Box>
+                    <Box sx={{ p: [4], flex: "0 0 auto", maxWidth: "35em", mx: [4] }}>
+                      <Route exact path="/" component={Conversations} />
+                      <Route exact path="/account" component={Account} />
+                      <Route exact path="/integrate" component={Integrate} />
+                    </Box>
+                  </Flex>
+                );
+              }}
+            />
+
+            <Route path="/m/:conversation_id" component={ConversationAdminContainer} />
+          </InteriorHeader>
         </Switch>
       </>
     );
@@ -302,70 +290,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// import Container from "./components/container";
-
-// // /conversation-admin
-// import ConversationAdminContainer from "./components/conversation-admin/container";
-
-// import ConversationConfig from "./components/conversation-admin/conversation-config";
-// import ConversationStats from "./components/conversation-admin/conversation-stats";
-
-// import ModerateComments from "./components/conversation-admin/moderate-comments";
-// import ModerateCommentsTodo from "./components/conversation-admin/moderate-comments-todo";
-// import ModerateCommentsAccepted from "./components/conversation-admin/moderate-comments-accepted";
-// import ModerateCommentsRejected from "./components/conversation-admin/moderate-comments-rejected";
-// import ModerateCommentsSeed from "./components/conversation-admin/moderate-comments-seed";
-// import ModerateCommentsSeedTweet from "./components/conversation-admin/moderate-comments-seed-tweet";
-
-// import ParticipantModeration from "./components/conversation-admin/moderate-people";
-// import ParticipantModerationDefault from "./components/conversation-admin/moderate-people-default";
-// import ParticipantModerationFeatured from "./components/conversation-admin/moderate-people-featured";
-// import ParticipantModerationHidden from "./components/conversation-admin/moderate-people-hidden";
-
-// import DataExport from "./components/conversation-admin/data-export";
-// import ShareAndEmbed from "./components/conversation-admin/share-and-embed";
-// import Live from "./components/conversation-admin/live";
-// import Summary from "./components/conversation-admin/summary";
-
-// import Reports from "./components/conversation-admin/reports";
-// import ReportConfig from "./components/conversation-admin/report-config";
-// import ReportComments from "./components/conversation-admin/report-comments";
-// import ReportCommentsIncluded from "./components/conversation-admin/report-comments-included";
-// import ReportCommentsExcluded from "./components/conversation-admin/report-comments-excluded";
-// import ReportsList from "./components/conversation-admin/reports-list";
-
-//     <Route path="m/:conversation_id" component={ConversationAdminContainer}>
-//       <Route component={ConversationConfig} />
-//       <Route path="live" component={Live} />
-//       <Route path="share" component={ShareAndEmbed} />
-//       <Route path="summary" component={Summary} />
-//       <Route path="reports" component={Reports}>
-//         <Route exact component={ReportsList} />
-//         <Route path=":report_id" component={Container}>
-//           <Route exact component={ReportConfig} />
-//           <Route path="comments" component={ReportComments}>
-//             <Route exact component={ReportCommentsIncluded} />
-//             <Route path="included" component={ReportCommentsIncluded} />
-//             <Route path="excluded" component={ReportCommentsExcluded} />
-//           </Route>
-//         </Route>
-//       </Route>
-//       <Route path="reports" component={Reports} />
-//       <Route path="comments" component={ModerateComments}>
-//         <Route component={ModerateCommentsTodo} />
-//         <Route path="accepted" component={ModerateCommentsAccepted} />
-//         <Route path="rejected" component={ModerateCommentsRejected} />
-//         <Route path="seed" component={ModerateCommentsSeed} />
-//         <Route path="seed_tweet" component={ModerateCommentsSeedTweet} />
-//       </Route>
-//       <Route path="participants" component={ParticipantModeration}>
-//         <Route component={ParticipantModerationDefault} />
-//         <Route path="featured" component={ParticipantModerationFeatured} />
-//         <Route path="hidden" component={ParticipantModerationHidden} />
-//       </Route>
-//       <Route path="stats" component={ConversationStats} />
-//       <Route path="export" component={DataExport} />
-//     </Route>
-//   </Route>
-// </Switch>
