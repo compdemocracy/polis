8ffee32f59deac5b0fb9944233c6e3f1c1f035b2
Benjamin Rosas <ben@aliencyb.org>
2020-06-04 01:42:32 -0500

    home and create-user specs

---
+++ cypress/integration/client-admin/home.spec.js
@@ -0,0 +1,12 @@
+describe('Home Page', () => {
+  beforeEach(() => cy.visit(''))
+
+  it('bare Url redirects to /home', () => {
+    cy.url().should('eq', Cypress.config().baseUrl + '/home')
+  })
+
+  it('has Sign up and Sign in links', () => {
+    cy.get('#root').find('a[href="/createuser"]')
+    cy.get('#root').find('a[href="/signin"]').should('have.length', 2)
+  })
+})
