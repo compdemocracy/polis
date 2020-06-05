d4f9d163ae1427162fba101c260ee12f727f9107
colinmegill <colinmegill@gmail.com>
2020-06-05 17:08:30 -0400

    monitor view back up, d3 as dep

--- client-admin/src/components/conversation-admin/stats/conversation-stats-comments-timescale.js
+++ client-admin/src/components/conversation-admin/stats/conversation-stats-comments-timescale.js
@@ -1,15 +1,13 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import React from "react";
-import { connect } from "react-redux";
-import Radium from "radium";
+
 import _ from "lodash";
 import { VictoryChart } from "victory-chart";
 import { VictoryLine } from "victory-line";
-import { VictoryBar } from "victory-bar";
 import { VictoryAxis } from "victory-axis";
+import * as d3 from "d3";
 
-@Radium
 class CommentsTimescale extends React.Component {
   render() {
     return (
@@ -17,8 +15,8 @@ class CommentsTimescale extends React.Component {
         width={this.props.chartWidth}
         height={this.props.chartHeight}
         scale={{
-          x: d3.time.scale(this.props.data.commentTimes),
-          y: d3.scale.linear(),
+          x: d3.scaleTime(this.props.data.commentTimes),
+          y: d3.scaleLinear(),
         }}
       >
         <VictoryLine
