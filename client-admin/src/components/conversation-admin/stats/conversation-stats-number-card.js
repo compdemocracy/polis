48ad34d8c8bd9356a5fcecd03db70bf7dadd85f0
colinmegill <colinmegill@gmail.com>
2020-06-05 20:58:24 -0400

    clear seed comment, monitor view

--- client-admin/src/components/conversation-admin/stats/conversation-stats-number-card.js
+++ client-admin/src/components/conversation-admin/stats/conversation-stats-number-card.js
@@ -1,14 +1,15 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import React from "react";
+import { Box, Text } from "theme-ui";
 
 class NumberCard extends React.Component {
   render() {
     return (
-      <div>
-        <span>{this.props.datum}</span>
-        <span> {this.props.subheading} </span>
-      </div>
+      <Flex sx={{ my: [2] }}>
+        <Text sx={{ fontWeight: 700 }}>{this.props.datum}</Text>
+        <Text> {this.props.subheading} </Text>
+      </Flex>
     );
   }
 }
