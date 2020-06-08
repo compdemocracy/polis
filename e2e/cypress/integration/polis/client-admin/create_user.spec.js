d8c62da0df49fd443d4ea016c84c227c26016918
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-06-07 21:49:19 -0300

    Made tests more idiomatic and started using commands.

--- e2e/cypress/integration/polis/client-admin/create_user.spec.js
+++ e2e/cypress/integration/polis/client-admin/create_user.spec.js
@@ -1,37 +1,22 @@
 describe('Create User', () => {
-  afterEach(() => cy.visit('/signout'))
-
   beforeEach(() => {
-    cy.visit('/signout')
-    cy.visit('/createuser')
+    // Cypress doesn't believe in cleanup.
+    // See: https://docs.cypress.io/guides/references/best-practices.html#State-reset-should-go-before-each-test
+    cy.logout()
   })
 
-  before(async function () {
-    const users = await cy.fixture('users')
-    this.user = users[0]
+  before(() => {
+    cy.fixture('users.json').as('users')
   })
 
-  it('Does not create a new user with existing email address', () => {
+  it('Does not create a new user with existing email address', function () {
+    const user = this.users[0]
+
     // Create user.
-    cy.get('form').within(function () {
-      cy.get('input#createUserNameInput').type(this.user.name)
-      cy.get('input#createUserEmailInput').type(this.user.email)
-      cy.get('input#createUserPasswordInput').type(this.user.password)
-      cy.get('input#createUserPasswordRepeatInput').type(this.user.password)
-
-      cy.get('button#createUserButton').click()
-    })
+    cy.signup(user.name, user.email, user.password)
 
     // Attempt to recreate user.
-    cy.visit('/createuser')
-    cy.get('form').within(function () {
-      cy.get('input#createUserNameInput').type(this.user.name)
-      cy.get('input#createUserEmailInput').type(this.user.email)
-      cy.get('input#createUserPasswordInput').type(this.user.password)
-      cy.get('input#createUserPasswordRepeatInput').type(this.user.password)
-
-      cy.get('button#createUserButton').click()
-    })
+    cy.signup(user.name, user.email, user.password)
 
     cy.get('form').contains(
       'Email address already in use, Try logging in instead.'
@@ -39,7 +24,7 @@ describe('Create User', () => {
     cy.url().should('eq', Cypress.config().baseUrl + '/createuser')
   })
 
-  it('Creates a new user', () => {
+  it('Creates a new user', function () {
     const randomInt = Math.floor(Math.random() * 10000)
     const newUser = {
       email: `user${randomInt}@polis.test`,
@@ -53,18 +38,12 @@ describe('Create User', () => {
       url: Cypress.config().apiPath + '/auth/new'
     }).as('authNew')
 
-    cy.get('form').within(async function () {
-      cy.get('input#createUserNameInput').type(newUser.name)
-      cy.get('input#createUserEmailInput').type(newUser.email)
-      cy.get('input#createUserPasswordInput').type(newUser.password)
-      cy.get('input#createUserPasswordRepeatInput').type(newUser.password)
+    cy.signup(newUser.name, newUser.email, newUser.password)
 
-      cy.get('button#createUserButton').click()
-
-      const xhr = await cy.wait('@authNew')
+    cy.wait('@authNew').then((xhr) => {
       expect(xhr.status).to.equal(200)
-
-      cy.url().should('eq', Cypress.config().baseUrl + '/')
     })
+
+    cy.url().should('eq', Cypress.config().baseUrl + '/')
   })
 })
