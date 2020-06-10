098f1678d3b3367b94697c2452495b782204c5e8
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-06-10 00:40:44 -0300

    e2e: Added login and createConvo commands. Fixed tests for i18n.

--- e2e/cypress/integration/polis/client-participation/i18n.spec.js
+++ e2e/cypress/integration/polis/client-participation/i18n.spec.js
@@ -1,83 +1,85 @@
 describe('Interface internationalization', () => {
   let lang
-  const strings = {
-    da: 'Del dit perspektiv...',
-    de: 'Teilen Sie Ihren Standpunkt mit ...',
-    en: 'Share your perspective...',
-    es: 'Compartir su perspectiva...',
-    fr: 'Faites connaître votre point de vue...',
-    it: 'Condividi il tuo punto di vista...',
-    ja: 'あなたの考え方を共有しよう……',
-    nl: 'Geef uw mening...',
-    pt: 'Inclua seu comentário...',
-    'zh-CN': '分享您的观点...',
-    'zh-TW': '分享您的觀點...',
-  }
+
+  before(() => {
+    cy.fixture('users.json').then((users) => {
+      const user = users[0]
+      cy.createConvo(user.email, user.password)
+      cy.url('pathname').then((adminPath) => {
+        const convoPath = adminPath.replace('/m/', '/')
+        cy.wrap(convoPath).as('convoPath')
+      })
+    })
+  })
+
+  beforeEach(() => {
+    cy.fixture('writePrompt_strings.json').as('strings')
+  })
 
   it('translates into German', function () {
     lang = 'de'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into Danish', function () {
     lang = 'da'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into English', function () {
     lang = 'en'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into Spanish', function () {
     lang = 'es'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into French', function () {
     lang = 'fr'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into Italian', function () {
     lang = 'it'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into Japanese', function () {
     lang = 'ja'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into Dutch', function () {
     lang = 'nl'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into Portugese', function () {
     lang = 'pt'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into Simplified Chinese', function () {
     lang = 'zh-CN'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
   it('translates into Traditional Chinese', function () {
     lang = 'zh-TW'
-    cy.visit(`/5s3sd6nhtv?ui_lang=${lang}`)
-    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', strings[lang])
+    cy.visit(this.convoPath, {qs: {ui_lang: lang}})
+    cy.get('textarea#comment_form_textarea').should('have.attr', 'placeholder', this.strings[lang])
   })
 
 })
