3db2b2b1abc5ff487285d1f4a606addbda5ed181
Colin Megill <colinmegill@gmail.com>
2020-06-10 11:53:55 -0400

    Update client-admin/src/components/conversations-and-account/conversation.js
    Co-authored-by: Patrick Connolly <patrick.c.connolly@gmail.com>

--- client-admin/src/components/conversations-and-account/conversation.js
+++ client-admin/src/components/conversations-and-account/conversation.js
@@ -3,7 +3,7 @@ import { jsx, Text, Card } from "theme-ui";
 
 export default ({ c, i, goToConversation }) => {
   return (
-    <Card onClick={goToConversation} sx={{ cursor: "pointer", mb: [3] }} key={i}>
+    <Card onClick={goToConversation} sx={{ cursor: "pointer", "overflow-wrap": "break-word", mb: [3] }} key={i}>
       <Text sx={{ fontWeight: 700, mb: [2] }}>{c.topic}</Text>
       <Text>{c.description}</Text>
       <Text>{c.parent_url ? `Embedded on ${c.parent_url}` : null}</Text>
