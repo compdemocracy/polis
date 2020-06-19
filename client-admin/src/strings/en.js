84359b9b5409eeb5f31a7a0f40327450680fa1a2
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-06-19 17:10:02 -0300

    Bugfix.

--- client-admin/src/strings/en.js
+++ client-admin/src/strings/en.js
@@ -23,4 +23,4 @@ export default {
   "share_but_no_visible_comments_warning": "This conversation has no visible comments. We recommend you add a few comments (or moderate the comments that exist) before inviting participants, since this will help them understand what kind of comments they should submit.",
 
   "no_permission": "Your account does not have the permissions to view this page.",
-}
+};
--- client-admin/src/strings/pt-BR.js
+++ client-admin/src/strings/pt-BR.js
@@ -8,7 +8,7 @@
 export default {
   "polis_err_param_parse_failed_email": "Por favor insira um endereço de email válido.",
   "polis_err_reg_bad_email": "Por favor insira um endereço de email válido.",
-  "polis_err_param_parse_failed_password": "Por favor insira uma senha válida."
+  "polis_err_param_parse_failed_password": "Por favor insira uma senha válida.",
   "polis_err_login_unknown_user_or_password_noresults": "Login falhou: usuário/senha inválida.",
   "polis_err_login_unknown_user_or_password": "Login falhou: usuário/senha inválida.",
   "polis_err_reg_user_with_that_email_exists": "Email já cadastrado, tente fazer login.",
@@ -21,4 +21,4 @@ export default {
 
   "share_but_no_comments_warning": "Essa discussão não tem comentários. Nós recomendamos adicionar alguns comentários antes de convidar participantes. Isso irá ajudar os participantes a começar. No menu da direita, vá para a opção 'comments' e depois 'seed'.",
   "share_but_no_visible_comments_warning": "Essa discussão não tem comentários visíveis. Nós recomendamos acrescentar alguns comentários ou moderar comentários que estejam na fila antes de convidar os participantes, já que isso os ajudará a entender que tipo de comentários eles devem enviar.",
-}
+};
