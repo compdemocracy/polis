eff666646d9e917afc0062b52ebe13ebc4d8e7fc
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-06-29 10:54:10 -0300

    Fixed issue with envvars in bundlewatch config. [skip ci]

--- client-admin/.bundlewatch.config.js
+++ client-admin/.bundlewatch.config.js
@@ -6,7 +6,8 @@ module.exports = {
     // Allows bundlewatch GitHub Actions workflow to work on: pull_request, or push
     "commitSha": process.env.PR_COMMIT_SHA || process.env.PUSH_COMMIT_SHA,
     "repoBranchBase": process.env.PR_BRANCH_BASE || process.env.PUSH_BRANCH_BASE,
-    "repoCurrentBranch": process.env.PR_BRANCH || process.env.PUSH_BRANCH,
+    // Note: During push event, GITHUB_REF is the current branch.
+    "repoCurrentBranch": process.env.PR_BRANCH || process.env.GITHUB_REF,
   },
   "files": [
     {
