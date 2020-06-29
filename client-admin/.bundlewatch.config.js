ada2e9aed4b02dd425768f45c71bf4fb43795998
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-06-29 11:12:36 -0300

    Fixed bundlewatch. (Finally?) [skip ci]

--- client-admin/.bundlewatch.config.js
+++ client-admin/.bundlewatch.config.js
@@ -6,8 +6,7 @@ module.exports = {
     // Allows bundlewatch GitHub Actions workflow to work on: pull_request, or push
     "commitSha": process.env.PR_COMMIT_SHA || process.env.PUSH_COMMIT_SHA,
     "repoBranchBase": process.env.PR_BRANCH_BASE || process.env.PUSH_BRANCH_BASE,
-    // Note: During push event, GITHUB_REF is the current branch.
-    "repoCurrentBranch": process.env.PR_BRANCH || process.env.GITHUB_REF,
+    "repoCurrentBranch": process.env.PR_BRANCH || process.env.PUSH_BRANCH,
   },
   "files": [
     {
