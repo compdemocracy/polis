27ee2f45ba96a6543f421feac991c7487621f1eb
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-07-01 00:26:32 -0300

    Added auto-fixed from using babel-eslint parser.

--- client-admin/src/components/conversation-admin/comment-moderation/moderate-comments-rejected.js
+++ client-admin/src/components/conversation-admin/comment-moderation/moderate-comments-rejected.js
@@ -1,20 +1,23 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import React from "react";
-import { changeCommentStatusToAccepted, changeCommentCommentIsMeta } from "../../../actions";
-import { connect } from "react-redux";
-import Comment from "./comment";
-import Spinner from "../../framework/spinner";
-import Flex from "../../framework/flex";
+import React from 'react'
+import {
+  changeCommentStatusToAccepted,
+  changeCommentCommentIsMeta
+} from '../../../actions'
+import { connect } from 'react-redux'
+import Comment from './comment'
+import Spinner from '../../framework/spinner'
+import Flex from '../../framework/flex'
 
-@connect((state) => state.mod_comments_rejected)
+@connect(state => state.mod_comments_rejected)
 class ModerateCommentsRejected extends React.Component {
   onCommentAccepted(comment) {
-    this.props.dispatch(changeCommentStatusToAccepted(comment));
+    this.props.dispatch(changeCommentStatusToAccepted(comment))
   }
 
   toggleIsMetaHandler(comment, is_meta) {
-    this.props.dispatch(changeCommentCommentIsMeta(comment, is_meta));
+    this.props.dispatch(changeCommentCommentIsMeta(comment, is_meta))
   }
 
   createCommentMarkup() {
@@ -29,9 +32,9 @@ class ModerateCommentsRejected extends React.Component {
           toggleIsMetaHandler={this.toggleIsMetaHandler.bind(this)}
           comment={comment}
         />
-      );
-    });
-    return comments;
+      )
+    })
+    return comments
   }
 
   render() {
@@ -39,10 +42,10 @@ class ModerateCommentsRejected extends React.Component {
       <div>
         {this.props.rejected_comments !== null
           ? this.createCommentMarkup()
-          : "Loading rejected comments..."}
+          : 'Loading rejected comments...'}
       </div>
-    );
+    )
   }
 }
 
-export default ModerateCommentsRejected;
+export default ModerateCommentsRejected
