741b72d1a225a3f107879b542fcf8fb4bf589bb6
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-06-30 23:06:58 -0300

    Auto-fixed all eslint issues.

--- client-admin/src/reducers/conversation_config.js
+++ client-admin/src/reducers/conversation_config.js
@@ -1,17 +1,19 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import * as types from "../actions";
+import * as types from '../actions'
 
-const conversation_config = (state = {
-  loading: false,
-  conversation_config: null,
-  update_error: null
-}, action) => {
+const conversation_config = (
+  state = {
+    loading: false,
+    conversation_config: null,
+    update_error: null
+  },
+  action
+) => {
   switch (action.type) {
-
-  default:
-    return state;
+    default:
+      return state
   }
-};
+}
 
-export default conversation_config;
+export default conversation_config
--- client-admin/src/strings/strings.js
+++ client-admin/src/strings/strings.js
@@ -1,21 +1,20 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import en_us from "./en_us";
-import pt_br from "./pt_br"; // Temporarily until language choosing code is ported
+import en_us from './en_us'
+import pt_br from './pt_br' // Temporarily until language choosing code is ported
 
-var s = {};
+var s = {}
 
 // TODO port language choosing code
-s = en_us;
-
+s = en_us
 
 function f(key) {
   // strip whitespace from key
-  key = key.replace(/\s+$/,"").replace(/^\s+/,"");
-  if (typeof s[key] === "undefined") {
-    return key;
+  key = key.replace(/\s+$/, '').replace(/^\s+/, '')
+  if (typeof s[key] === 'undefined') {
+    return key
   }
-  return s[key];
+  return s[key]
 }
 
-export default f;
+export default f
