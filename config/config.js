1843c248db3e635478f0c6fa8164df640543e5d3
Chris Krenn <crkrenn@gmail.com>
2020-07-16 16:06:41 -0700

    schema has been moved to schema.json

--- config/config.js
+++ config/config.js
@@ -1,35 +1,49 @@
 const convict = require('convict');
-
+const fs = require('fs');
  
 // convict.addFormat(require('convict-format-with-validator').ipaddress);
- 
+
+'use strict';
+
+// 
+
+// let rawdata = fs.readFileSync('student.json');
+// let student = JSON.parse(rawdata);
+// console.log(student);
+
 // Define a schema
-var config = convict({
-  env: {
-    doc: "The application environment.",
-    format: ["production", "development", "test"],
-    default: "development",
-    env: "NODE_ENV"
-  }, 
-  akismet_antispam_api_key: {
-    doc: 'akismet_antispam_api_key (ex. a1a11111aa11)',
-    format: String,
-    default: '',
-    env: 'AKISMET_ANTISPAM_API_KEYx'
-  },
-  foo: {
-    doc: 'node-convict debug',
-    format: String,
-    default: 'foo-text',
-    env: 'FOO'
-  },
-  bar: {
-    doc: 'node-convict debug',
-    format: String,
-    default: 'bar-text',
-    env: 'BAR'
-  }
-});
+
+let rawdata = fs.readFileSync('./config/schema.json');
+let schema = JSON.parse(rawdata);
+
+var config = convict(schema);
+
+// var config = convict({
+//   env: {
+//     doc: "The application environment.",
+//     format: ["production", "development", "test"],
+//     default: "development",
+//     env: "NODE_ENV"
+//   }, 
+//   akismet_antispam_api_key: {
+//     doc: 'akismet_antispam_api_key (ex. a1a11111aa11)',
+//     format: String,
+//     default: '', 
+//     env: 'AKISMET_ANTISPAM_API_KEY'
+//   },
+//   foo: {
+//     doc: 'node-convict debug',
+//     format: String,
+//     default: 'foo-text',
+//     env: 'FOO'
+//   },
+//   bar: {
+//     doc: 'node-convict debug',
+//     format: String,
+//     default: 'bar-text',
+//     env: 'BAR'
+//   }
+// });
  
 // Load environment dependent configuration
 var env = config.get('env');
