698a6c866ab6814db39b39e525695ddc6da21e4c
Romulus Tsai <urakagi@gmail.com>
2020-07-16 20:43:32 -0300

    Finished disassembly of server.js into auth/create-user.js and auth/password.js.

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,30 @@
 const devMode = require('boolean')(get('DEV_MODE'));
 
+const domainOverride = process.env.DOMAIN_OVERRIDE || null;
+
+function getServerNameWithProtocol(req) {
+  let server = "https://pol.is";
+
+  if (domainOverride) {
+    server = req.protocol + "://" + domainOverride;
+  }
+  if (devMode) {
+    // usually localhost:5000
+    server = "http://" + req.headers.host;
+  }
+
+  if (req.headers.host.includes("preprod.pol.is")) {
+    server = "https://preprod.pol.is";
+  }
+  if (req.headers.host.includes("embed.pol.is")) {
+    server = "https://embed.pol.is";
+  }
+  if (req.headers.host.includes("survey.pol.is")) {
+    server = "https://survey.pol.is";
+  }
+  return server;
+}
+
 function get(key) {
 	return process.env[key];
 }
@@ -9,6 +34,8 @@ function isDevMode() {
 }
 
 module.exports = {
+  domainOverride,
+  getServerNameWithProtocol,
 	get,
 	isDevMode
 };
