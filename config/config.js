091b5b05f55939b17105418669ed7584a779e576
Chris Krenn <crkrenn@gmail.com>
2020-07-23 14:41:15 -0700

    starting to add convict back in

---
+++ config/config.js
@@ -0,0 +1,33 @@
+console.log('starting yaml config.js')
+
+const convict = require('convict');
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+convict.addParser({ extension: ['yml', 'yaml'], parse: yaml.safeLoad });
+
+// for additional validation options, use the following: 
+// convict.addFormat(require('convict-format-with-validator').ipaddress);
+
+'use strict';
+
+// Define a schema
+
+try {
+    let fileContents = fs.readFileSync('./config/schema.yaml', 'utf8');
+    let schema = yaml.safeLoad(fileContents);
+    var config = convict(schema);    
+} catch (e) {
+    console.log(e);
+}
+
+// Load environment dependent configuration
+var env = config.get('env');
+config.loadFile('./config/' + env + '.yaml');
+
+// Perform validation
+config.validate({allowed: 'strict'});
+ 
+module.exports = config;
+
+console.log('finishing yaml config.js')
---
+++ config/export_config.js
@@ -0,0 +1,11 @@
+// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"use strict";
+
+// // for some reason, this cannot be done in the dockerfile
+// const childProcess = require('child_process');
+// childProcess.execSync('cp /config/* ./config');
+
+var config = require('./config/config.js');
+
+console.log('aws_region:' + config.get('aws_region'));
