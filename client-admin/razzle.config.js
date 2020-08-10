1b7bf422ecbf817dbc46913281bc507debee6dad
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-09 22:25:19 -0400

    Added admin prefix and suffix on index and bundles.

--- client-admin/razzle.config.js
+++ client-admin/razzle.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   plugins: ["mdx"],
-  modify: (baseConfig, { target, dev }, webpack) => {
-    const appConfig = Object.assign({}, baseConfig)
+  modify: (config, { target, dev }, webpack) => {
+    const appConfig = config
 
-    return appConfig;
+    // When building SPA type app
+    if (!dev && target === 'web') {
+      // Rename index.html
+      appConfig.plugins.forEach((plugin, i) => {
+        if (plugin.constructor.name === 'HtmlWebpackPlugin') {
+          appConfig.plugins[i].options.filename = 'index_admin.html'
+        }
+      })
+
+      // Prefix files with `admin_` to easier to tell apart in combined file server.
+      appConfig.output.filename = 'static/js/admin_bundle.[chunkhash:8].js'
+      appConfig.output.chunkFilename =  'static/js/admin_[name].[chunkhash:8].chunk.js'
+    }
+
+    return appConfig
   },
 };
