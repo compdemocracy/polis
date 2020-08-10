03f5d2ba180ab1b461bf5562d50340a2fb98d6d6
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-10 13:05:04 -0400

    Fixed eslint issues.

--- client-admin/src/constants.js
+++ client-admin/src/constants.js
@@ -1,8 +1,8 @@
 export const domainWhitelist = [
-  "^localhost$",
-  "^192\\.168\\.1\\.140$",
-  "^pol\\.is",
-  ".+\\.pol\\.is$",
-  "^xip\\.io$",
-  ".+\\.xip\\.io$",
+  '^localhost$',
+  '^192\\.168\\.1\\.140$',
+  '^pol\\.is',
+  '.+\\.pol\\.is$',
+  '^xip\\.io$',
+  '.+\\.xip\\.io$'
 ]
