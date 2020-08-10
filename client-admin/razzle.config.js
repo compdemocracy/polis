22ef89e17df2d4a841d7e77f673ab1a8c9b3b1bd
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-09 22:50:47 -0400

    Added gzip compression into built app JS files.

--- client-admin/razzle.config.js
+++ client-admin/razzle.config.js
@@ -1,9 +1,11 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   plugins: ["mdx"],
   modify: (config, { target, dev }, webpack) => {
     const appConfig = config
 
-    // When building SPA type app
+    // When building production SPA type app.
     if (!dev && target === 'web') {
       // Rename index.html
       appConfig.plugins.forEach((plugin, i) => {
@@ -15,6 +17,14 @@ module.exports = {
       // Prefix files with `admin_` to easier to tell apart in combined file server.
       appConfig.output.filename = 'static/js/admin_bundle.[chunkhash:8].js'
       appConfig.output.chunkFilename =  'static/js/admin_[name].[chunkhash:8].chunk.js'
+
+      // Compress JS files with Gzip.
+      appConfig.plugins.push(new CompressionPlugin({
+        test: /\.js$/,
+        // Leave unmodified without gz ext.
+        // See: https://webpack.js.org/plugins/compression-webpack-plugin/#options
+        filename: '[path][query]',
+      }))
     }
 
     return appConfig
