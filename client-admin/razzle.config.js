f3f04d24cf5b197e1ae643210d47332c2d96d067
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-12 09:57:55 -0400

    Fixed eslint issues.

--- client-admin/razzle.config.js
+++ client-admin/razzle.config.js
@@ -1,5 +1,6 @@
 const CompressionPlugin = require('compression-webpack-plugin')
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 const EventHooksPlugin = require('event-hooks-webpack-plugin')
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 const S3Plugin = require('webpack-s3-plugin')
@@ -14,21 +15,23 @@ const argv = process.argv.slice(2)
 const cliArgs = mri(argv)
 
 module.exports = {
-  plugins: ["mdx"],
+  plugins: ['mdx'],
   modify: (config, { target, dev }, webpack) => {
     const appConfig = config
 
     // Help minimize bundle size due to lodash duplication.
-    appConfig.plugins.push(new LodashModuleReplacementPlugin({
-      "currying": true,
-      "flattening": true,
-      "paths": true,
-      "placeholders": true,
-      "shorthands": true,
-    }))
+    appConfig.plugins.push(
+      new LodashModuleReplacementPlugin({
+        currying: true,
+        flattening: true,
+        paths: true,
+        placeholders: true,
+        shorthands: true
+      })
+    )
 
     if (dev && target === 'web') {
-      //appConfig.plugins = [...appConfig.plugins, new BundleAnalyzerPlugin()]
+      // appConfig.plugins = [...appConfig.plugins, new BundleAnalyzerPlugin()]
     }
 
     // When building production SPA type app.
@@ -42,59 +45,65 @@ module.exports = {
 
       // Prefix files with `admin_` to easier to tell apart in combined file server.
       appConfig.output.filename = 'static/js/admin_bundle.[chunkhash:8].js'
-      appConfig.output.chunkFilename =  'static/js/admin_[name].[chunkhash:8].chunk.js'
+      appConfig.output.chunkFilename =
+        'static/js/admin_[name].[chunkhash:8].chunk.js'
 
       // Compress JS files with Gzip.
-      appConfig.plugins.push(new CompressionPlugin({
-        test: /\.js$/,
-        // Leave unmodified without gz ext.
-        // See: https://webpack.js.org/plugins/compression-webpack-plugin/#options
-        filename: '[path][query]',
-      }))
+      appConfig.plugins.push(
+        new CompressionPlugin({
+          test: /\.js$/,
+          // Leave unmodified without gz ext.
+          // See: https://webpack.js.org/plugins/compression-webpack-plugin/#options
+          filename: '[path][query]'
+        })
+      )
 
-      appConfig.plugins.push(new EventHooksPlugin({
-        afterEmit: () => {
-          console.log('Writing *.headersJson files...')
+      appConfig.plugins.push(
+        new EventHooksPlugin({
+          afterEmit: () => {
+            console.log('Writing *.headersJson files...')
 
-          function writeHeadersJson(matchGlob, headersData={}) {
-            const files = glob.sync(appConfig.output.path + '/' + matchGlob)
-            files.forEach((f, i) => {
-              const headersFilePath = f + '.headersJson'
-              fs.writeFileSync(headersFilePath, JSON.stringify(headersData));
-            })
-          }
-
-          function writeHeadersJsonHtml() {
-            const headersData = {
-              'x-amz-acl': 'public-read',
-              'Content-Type': 'text/html; charset=UTF-8',
-              'Cache-Control': 'no-cache',
+            function writeHeadersJson(matchGlob, headersData = {}) {
+              const files = glob.sync(appConfig.output.path + '/' + matchGlob)
+              files.forEach((f, i) => {
+                const headersFilePath = f + '.headersJson'
+                fs.writeFileSync(headersFilePath, JSON.stringify(headersData))
+              })
             }
-            writeHeadersJson('*.html', headersData)
-          }
 
-          function writeHeadersJsonJs() {
-            const headersData = {
-              'x-amz-acl': 'public-read',
-              'Content-Encoding': 'gzip',
-              'Content-Type': 'application/javascript',
-              'Cache-Control': 'no-transform,public,max-age=31536000,s-maxage=31536000',
+            function writeHeadersJsonHtml() {
+              const headersData = {
+                'x-amz-acl': 'public-read',
+                'Content-Type': 'text/html; charset=UTF-8',
+                'Cache-Control': 'no-cache'
+              }
+              writeHeadersJson('*.html', headersData)
             }
-            writeHeadersJson('static/js/*.js?(.map)', headersData)
-          }
 
-          function writeHeadersJsonMisc() {
-            writeHeadersJson('favicon.ico')
-          }
+            function writeHeadersJsonJs() {
+              const headersData = {
+                'x-amz-acl': 'public-read',
+                'Content-Encoding': 'gzip',
+                'Content-Type': 'application/javascript',
+                'Cache-Control':
+                  'no-transform,public,max-age=31536000,s-maxage=31536000'
+              }
+              writeHeadersJson('static/js/*.js?(.map)', headersData)
+            }
 
-          writeHeadersJsonHtml()
-          writeHeadersJsonJs()
-          writeHeadersJsonMisc()
-        }
-      }))
+            function writeHeadersJsonMisc() {
+              writeHeadersJson('favicon.ico')
+            }
+
+            writeHeadersJsonHtml()
+            writeHeadersJsonJs()
+            writeHeadersJsonMisc()
+          }
+        })
+      )
     }
 
-    if (cliArgs.deploy || cliArgs.deploy == 's3') {
+    if (cliArgs.deploy || cliArgs.deploy === 's3') {
       if (dev) {
         console.log('Deploy only possible during build. Skipped.')
       } else {
@@ -104,21 +113,25 @@ module.exports = {
         const creds = JSON.parse(fs.readFileSync('.polis_s3_creds_client.json'))
 
         // See: https://github.com/MikaAK/s3-plugin-webpack
-        appConfig.plugins.push(new S3Plugin({
-          // Despite warnings in README, deploy fails without `directory` set.
-          directory: 'build/public',
-          // Output is broken for some reason.
-          // See: https://github.com/MikaAK/s3-plugin-webpack/issues/137
-          progress: false,
-          s3Options: {
-            accessKeyId: creds.key,
-            secretAccessKey: creds.secret
-          },
-          s3UploadOptions: {
-            // Choose bucket based on `--prod` flag when running `razzle build`.
-            Bucket: cliArgs.prod ? process.env.S3_BUCKET_PROD : process.env.S3_BUCKET_PREPROD
-          }
-        }))
+        appConfig.plugins.push(
+          new S3Plugin({
+            // Despite warnings in README, deploy fails without `directory` set.
+            directory: 'build/public',
+            // Output is broken for some reason.
+            // See: https://github.com/MikaAK/s3-plugin-webpack/issues/137
+            progress: false,
+            s3Options: {
+              accessKeyId: creds.key,
+              secretAccessKey: creds.secret
+            },
+            s3UploadOptions: {
+              // Choose bucket based on `--prod` flag when running `razzle build`.
+              Bucket: cliArgs.prod
+                ? process.env.S3_BUCKET_PROD
+                : process.env.S3_BUCKET_PREPROD
+            }
+          })
+        )
       }
     }
 
