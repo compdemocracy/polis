ebb8967a3a3ea9aaec8f9ab35b3078698863dc00
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-16 19:03:06 -0400

    e2e: Migrated login and logout commands to be programmatic.

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -25,13 +25,13 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add("logout", () => {
-  cy.server()
-  cy.route('POST', Cypress.config().apiPath + '/auth/deregister')
-    .as('authLogout')
-
-  cy.visit('/signout')
-
-  cy.wait('@authLogout').its('status').should('eq', 200)
+  cy.request('POST', Cypress.config().apiPath + '/auth/deregister')
+    .then(resp => {
+      window.localStorage.removeItem('token2')
+      window.localStorage.removeItem('uid2')
+      window.localStorage.removeItem('uc')
+      window.localStorage.removeItem('e')
+    })
 })
 
 Cypress.Commands.add("signup", (name, email, password) => {
@@ -47,24 +47,40 @@ Cypress.Commands.add("signup", (name, email, password) => {
   })
 })
 
-Cypress.Commands.add("login", (email, password) => {
-  cy.logout()
-  cy.visit('/signin')
-
-  cy.server()
-  cy.route({
-    method: 'POST',
-    url: Cypress.config().apiPath + '/auth/login'
-  }).as('authLogin')
-
-  cy.get('form').within(function () {
-    cy.get('input#signinEmailInput').type(email)
-    cy.get('input#signinPasswordInput').type(password)
-
-    cy.get('button#signinButton').click()
+function loginByPassword (email, password) {
+  cy.request('POST', Cypress.config().apiPath + '/auth/login', {
+    email: email,
+    password: password
+  }).then(resp => {
+    window.localStorage.setItem('token2', resp.body.token)
+    window.localStorage.setItem('uid2', resp.body.uid)
+    window.localStorage.setItem('uc', Date.now())
+    window.localStorage.setItem('e', 1)
   })
+}
 
-  cy.wait('@authLogin').its('status').should('eq', 200)
+function loginByRole (userRole) {
+  cy.fixture('users.json').then((users) => {
+    const user = users[userRole]
+    loginByPassword(user.email, user.password)
+  })
+}
+
+Cypress.Commands.add("login", (...args) => {
+  cy.logout()
+
+  switch (args.length) {
+    case 0:
+    case 1:
+      const [userRole] = args ? args : ['participant']
+      loginByRole(userRole)
+      break
+    default:
+    case 2:
+      const [email, password, ...rest] = args
+      console.log(args)
+      loginByPassword(email, password)
+  }
 })
 
 Cypress.Commands.add("createConvo", (adminEmail, adminPassword) => {
