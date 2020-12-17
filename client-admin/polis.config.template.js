619fe2186c55b88752b60f8358f9b405a837581b
josh <joshsmith2@gmail.com>
2020-12-17 22:22:04 +0000

    Local changes

--- client-admin/polis.config.template.js
+++ client-admin/polis.config.template.js
@@ -15,7 +15,7 @@ module.exports = {
 
   //SERVICE_URL: 'http://localhost:5000',
   //SERVICE_URL: 'https://preprod.pol.is',
-  SERVICE_URL: 'https://coin.demos.co.uk',
+  SERVICE_URL: 'https://coin-panel.demos.co.uk',
 
 
   UPLOADER: 'local', // alt: s3, scp
