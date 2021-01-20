383b0e45120e7f9344a14f886b4428b31dbb9b1a
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-01-20 00:04:49 -0500

    e2e: Fixed test of defaults.

--- e2e/cypress/integration/polis/client-admin/configure.spec.js
+++ e2e/cypress/integration/polis/client-admin/configure.spec.js
@@ -22,14 +22,7 @@ describe('Conversation: Configure', () => {
 
   describe('Defaults', () => {
     before(function () {
-      cy.createConvo('moderator').then(() => {
-        cy.visit(`/m/${this.convoId}`)
-        // We must set the topic in order for "Closed" badge to display.
-        // TODO: Allow badge to display without topic set.
-        cy.get('input[data-test-id="topic"]').type('Dummy topic')
-        cy.seedComment('Some seed statement', this.convoId)
-        cy.get('input[data-test-id="is_active"]').uncheck().should('not.be.checked')
-      })
+      cy.createConvo('moderator')
     })
 
     it('creates with proper defaults', function () {
