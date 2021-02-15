73e2cc8587f655e622ffde89195b28d5ec45de24
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-02-15 18:28:10 -0500

    Fix regression of default settings on convo creation (#771)
    * e2e: Added test to check for default settings on new convo.
    
    * Bugfix: Set defaults for bool convo config fields without defaults at postgres level.
    
    * e2e: Fixed test of defaults.
    
    * Migrated new convo constants into own file.

--- e2e/cypress/integration/polis/client-admin/configure.spec.js
+++ e2e/cypress/integration/polis/client-admin/configure.spec.js
@@ -1,20 +1,45 @@
-describe('Conversation: Closing', () => {
-  before(function () {
-    cy.createConvo('moderator').then(() => {
-      cy.visit(`/m/${this.convoId}`)
-      // We must set the topic in order for "Closed" badge to display.
-      // TODO: Allow badge to display without topic set.
-      cy.get('input[data-test-id="topic"]').type('Dummy topic')
-      cy.seedComment('Some seed statement', this.convoId)
-      cy.get('input[data-test-id="is_active"]').uncheck().should('not.be.checked')
+describe('Conversation: Configure', () => {
+  describe('Closing', () => {
+    before(function () {
+      cy.createConvo('moderator').then(() => {
+        cy.visit(`/m/${this.convoId}`)
+        // We must set the topic in order for "Closed" badge to display.
+        // TODO: Allow badge to display without topic set.
+        cy.get('input[data-test-id="topic"]').type('Dummy topic')
+        cy.seedComment('Some seed statement', this.convoId)
+        cy.get('input[data-test-id="is_active"]').uncheck().should('not.be.checked')
+      })
+    })
+
+    it('responds properly to being closed', function () {
+      cy.login('participant')
+      cy.visit(`/${this.convoId}`)
+      cy.get('.no_you_vote').should('be.visible')
+      cy.get('#readReactView').should('not.be.visible')
+      cy.get('#commentFormParent').should('not.be.visible')
     })
   })
 
-  it('responds properly to being closed', function () {
-    cy.login('participant')
-    cy.visit(`/${this.convoId}`)
-    cy.get('.no_you_vote').should('be.visible')
-    cy.get('#readReactView').should('not.be.visible')
-    cy.get('#commentFormParent').should('not.be.visible')
+  describe('Defaults', () => {
+    before(function () {
+      cy.createConvo('moderator')
+    })
+
+    it('creates with proper defaults', function () {
+      cy.visit(`/m/${this.convoId}`)
+      // Customize section
+      cy.get('input[data-test-id="vis_type"]').should('not.be.checked')
+      cy.get('input[data-test-id="write_type"]').should('be.checked')
+      cy.get('input[data-test-id="help_type"]').should('be.checked')
+      cy.get('input[data-test-id="subscribe_type"]').should('be.checked')
+      cy.get('input[data-test-id="auth_opt_fb"]').should('be.checked')
+      cy.get('input[data-test-id="auth_opt_tw"]').should('be.checked')
+
+      // Schemes section
+      cy.get('input[data-test-id="is_active"]').should('be.checked')
+      cy.get('input[data-test-id="strict_moderation"]').should('not.be.checked')
+      cy.get('input[data-test-id="auth_needed_to_write"]').should('be.checked')
+      cy.get('input[data-test-id="auth_needed_to_vote"]').should('not.be.checked')
+    })
   })
 })
