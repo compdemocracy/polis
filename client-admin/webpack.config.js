d452a6e37877e777ae3e7055ad7438eb12e3faa2
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-02-16 01:03:38 -0500

    Gulp exodus: Migrate from gzip from gulp to webpack (#778)
    * (client-admin) Gulp exodus: Migrated from gulp-gzip to webpack compression plugin.
    
    * Remove vestigial use of bower.
    
    * Reduced bundlesize fail thresholds.

--- client-admin/webpack.config.js
+++ client-admin/webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require("path");
 var webpack = require("webpack");
+var CompressionPlugin = require('compression-webpack-plugin')
 var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
 module.exports = {
@@ -21,6 +22,12 @@ module.exports = {
       "paths": true,
       "placeholders": true,
       "shorthands": true
+    }),
+    new CompressionPlugin({
+      test: /\.js$/,
+      // Leave unmodified without gz ext.
+      // See: https://webpack.js.org/plugins/compression-webpack-plugin/#options
+      filename: '[path][query]',
     }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production"),
