b01fd6b31480a307b681b7fbb36dafc4c2984f1e
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-04-03 17:48:43 -0400

    combine all hebrew translations pull requests (#927)
    * Create he.js
    
    * adding support for hebrew
    
    hope this is done right
    
    * Adding test for Hebrew
    
    * Small whitespace and syntax fixups.
    
    Co-authored-by: daniel-skywalker <54578071+daniel-skywalker@users.noreply.github.com>

---
+++ client-participation/js/strings/he.js
@@ -0,0 +1,130 @@
+// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var s = {};
+
+s.agree = "מסכים";
+s.disagree = "לא מסכים";
+s.pass = "עבור / לא בטוח";
+
+s.importantCheckbox = "התגובה הזו חשובה ";
+s.howImportantPrompt = "עד כמה האמירה הזו חשובה?";
+
+s.howImportantLow = "נמוך";
+s.howImportantMedium = "בינוני";
+s.howImportantHigh = "גבוה";
+
+s.modSpam = "ספאם / זבל";
+s.modOffTopic = "לא קשור";
+s.modImportant = "חשוב";
+s.modSubmitInitialState = "דלג (אף אחד מאלה), אמירה הבאה";
+s.modSubmit = "סיימתי, אמירה הבאה";
+
+s.x_wrote = "כתב/ה:";
+s.x_tweeted = "צייצ/ה:";
+s.comments_remaining = "{{num_comments}} נותרו";
+s.comments_remaining2 = "{{num_comments}} אמירות נותרו";
+s.group_123 = "קבוצה:";
+s.comment_123 = "אמירה:";
+s.majorityOpinion = "דעת הרוב";
+s.majorityOpinionShort = "הרוב";
+s.info = "ידע";
+s.addPolisToYourSite = "<img style='height: 20px; margin: 0px 4px;' src='{{URL}}'/>";
+s.privacy = "פרטיות";
+s.TOS = "תנאי שימוש";
+s.writePrompt = "שתפו את נקודת מבטכם...";
+s.anonPerson = "אנונימי";
+s.helpWhatAmISeeingTitle = "מה אני רואה?";
+s.helpWhatAmISeeing = "אנשים שהצביעו באופן דומה נמצאים יחד בקבוצה. לחצו על קבוצה על מנת לראות אילו נקודות מבט הם חולקים.";
+s.helpWhatDoIDoTitle = " מה אני עושה?";
+s.helpWhatDoIDo = "הצביעו על אמירות של אנשים אחרים על ידי לחיצה על 'מסכים' או 'לא מסכים'. כתבו אמירה (הנוגעת לרעיון אחד מובחן). הזמינו חברים להצטרף לשיחה!";
+s.writeCommentHelpText = "האם הדעות והניסיון שלכם לא מקבלים ביטוי בשיחה? <b> הוסיפו אותם </b> בתיבה למטה.";
+s.helpWriteListIntro = "מהי אמירה טובה?";
+s.helpWriteListStandalone = "רעיון העומד בפני עצמו";
+s.helpWriteListRaisNew = "העלאה של נקודות מבט, ניסיון או נושאים חדשים";
+s.helpWriteListShort = "קצר ולעניין (מוגבל ל-140 תווים)";
+s.heresHowGroupVoted = "כך קבוצה {{GROUP_NUMBER}} הצביעה";
+s.one_person = "איש/ה {{x}}";
+s.x_people = "{{x}} אנשים";
+s.acrossAllPtpts = "מבין כלל המשתתפים:";
+s.xPtptsSawThisComment = " ראו את האמירה הזו";
+s.xOfThoseAgreed = "מתוך המשתתפים האלו הסכימו";
+s.xOfthoseDisagreed = "מתוך המשתתפים האלו לא הסכימו";
+s.opinionGroups = "קבוצות דעה";
+s.topComments = "אמירות מובילות";
+s.divisiveComments = "אמירות שנויות במחלוקת";
+s.pctAgreed = "{{pct}}% הסכימו";
+s.pctDisagreed = "{{pct}}% לא הסכימו";
+s.pctAgreedLong = "{{pct}}% ממי שהצביע על האמירה הזו {{comment_id}} הסכים.";
+s.pctAgreedOfGroup = "{{pct}}% מקבוצה {{group}} הסכים";
+s.pctDisagreedOfGroup = "{{pct}}% מקבוצה {{group}} לא הסכים";
+s.pctDisagreedLong = "{{pct}}% ממי שהצביע על האמירה הזו {{comment_id}} לא הסכים.";
+s.pctAgreedOfGroupLong = "{{pct}}% מאלו בקבוצה {{group}} שהצביעו על אמירה {{comment_id}} הסכים.";
+s.pctDisagreedOfGroupLong = "{{pct}}% מאלו בקבוצה {{group}} שהצביעו על אמירה {{comment_id}} לא הסכים.";
+s.commentSent = "האמירה נשלחה! רק משתתפים אחרים יוכלו לראות את האמירה ולהסכים או לא להסכים איתה.";
+s.commentSendFailed = "שגיאה בשליחת האמירה.";
+s.commentSendFailedEmpty = "שגיאה בשליחת האמירה - אמירה ריקה.";
+s.commentSendFailedTooLong = "שגיאה בשליחת האמירה - אמירה ארוכה מדי.";
+s.commentSendFailedDuplicate = "שגיאה בשליחת האמירה - אמרה זהה כבר קיימת במערכת.";
+s.commentErrorDuplicate = "אמירה זהה כבר קיימת בשיחה.";
+s.commentErrorConversationClosed = "השיחה הזו סגורה. לא ניתן לשלוח אמירות חדשות.";
+s.commentIsEmpty = "אמירה ריקה";
+s.commentIsTooLong = "אמירה ארוכה מדי";
+s.hereIsNextStatement = "הצבעה נקלטה. נווטו למעלה על מנת לראות את האמירה הבאה.";
+
+s.connectFacebook = "התחברות פייסבוק";
+s.connectTwitter = "התחברות טוויטר";
+s.connectToPostPrompt = "עליך להזדהות על מנת להוסיף אמירה. אנחנו לא נכתוב בשמך בפייסבוק.";
+s.connectToVotePrompt = "עליך להזדהות על מנת להצביע. אנחנו לא נכתוב בשמך בפייסבוק.";
+s.tip = "טיפ:";
+s.commentWritingTipsHintsHeader = "טיפים לכתיבת אמירות";
+s.tipCharLimit = "אמירות מוגבלות ל-{{char_limit}} תווים.";
+s.tipCommentsRandom = "זכרו, האמירות מוצגות בסדר אקראי, ואתם לא מגיבים ישירות לאמירות של משתתפים אחרים.";
+s.tipOneIdea = "Break up long statements that contain multiple ideas. This makes it easier for others to vote on your statement.";
+s.tipOneIdea = "פצלו אמירות ארוכות שכוללות כמה רעיונות. כך קל יותר לאחרים להצביע על האמירה שלכם.";
+s.tipNoQuestions = "Statements should not be in the form of a question. Participants will agree or disagree with the statements you make.";
+s.tipNoQuestions = "אל תנסחו את האמירות כשאלה. המשתתפים בדיון יוכלו להסכים או לא להסכים עם האמירות שתוסיפו.";
+s.commentTooLongByChars = "האמירה חורגת ממגבלת האורך ב-{{CHARACTERS_COUNT}} תווים.";
+s.notSentSinceDemo = "(לא באמת, זה רק דמו)";
+s.submitComment = "לשלוח";
+s.tipStarred = "סומן כחשוב.";
+s.participantHelpWelcomeText = "ברוכים הבאים לסוג חדש של שיחה - <em>הצביעו</em> על אמירות של אנשים אחרים.";
+s.participantHelpGroupsText = "אנשים שהצביעו באופן דומה <span style='font-weight: 700;'>נמצאים יחד בקבוצה.</span> לחצו על קבוצה על מנת לראות אילו נקודות מבט הם חולקים. <a style='font-weight: 700; cursor: pointer; text-decoration: underline' id='helpTextGroupsExpand'>...עוד</a>";
+s.participantHelpGroupsNotYetText = "הגרפיקות יופיעו כאשר 7 משתתפים לפחות יצביעו.";
+s.helpWhatAreGroupsDetail = "<p>ודאי הוצעו לכם 'מוצרים מומלצים' באמזון או 'סרטים מומלצים' בנטפליקס. כל אחד מהשירותים האלהמשתמש בסטטיסטיקה על מנת לקבץ את המשתמשים עם אנשים שקנו וצפו בדברים דומים להם, ואז מציע להם את מה שאותם אנשים קנו או צפו בו. </p> <p> כאשר משתמשים מצביעים על אמירה, הם מקובצים יחד עם אנשים שהצביעו כמוהם! תוכלו לראות את הקבוצות הללו למטה. כל אחת מורכבת מאנשים שחולקים דעות דומות. ישנן תובנות מרתקות לגילוי בכל שיחה. נסו ללחוץ על קבוצה וראו אילו דעות איחדו אותם, ומה הופך אותם לייחודיים.</p>";
+s.socialConnectPrompt = "תוכלו להתחבר על מנת לראות חברים ואנשים שאתם עוקבים אחריהם בהדמיה.";
+s.connectFbButton = "התחברות עם פייסבוק";
+s.connectTwButton = "התחברות עם טוויטר";
+s.polis_err_reg_fb_verification_email_sent = "יש לבדוק את המייל ללינק אימות, ואז לחזור לכאן על מנת להמשיך.";
+s.polis_err_reg_fb_verification_noemail_unverified = "חשבון הפייסבוק שלך לא מאומת. יש לאמת את כתובת האימייל עם פייסבוק ואז לחזור לכאן על מנת להמשיך.";
+s.showTranslationButton = "הפעלת תרגום של ספק צד שלישי";
+s.hideTranslationButton = "כיבוי תרגום";
+s.thirdPartyTranslationDisclaimer = "התרגום מתבצע על ידי צד שלישי";
+
+s.notificationsAlreadySubscribed = "אתם מנויים לעדכונים בנוגע לשיחה הזו.";
+s.notificationsGetNotified = "קבלו עדכונים כשעוד אמירות מתקבלות:";
+s.notificationsEnterEmail = "הוסיפו את האיימיל שלכם על מנת לקבל עדכונים כשאמירות נוספות מתקבלות:";
+s.labelEmail = "אימייל";
+s.notificationsSubscribeButton = "הרשמה";
+s.notificationsSubscribeErrorAlert = "שגיאה בהרשמה";
+s.noCommentsYet = "אין עדיין אמירות.";
+s.noCommentsYetSoWrite = "התחילו את השיחה על ידי הוספת אמירה.";
+s.noCommentsYetSoInvite = "התחילו את השיחה על ידי הזמנת משתתפים נוספים, או על ידי הוספת אמירה.";
+s.noCommentsYouVotedOnAll = "כבר הצבעתם על כל האמירות.";
+s.noCommentsTryWritingOne = "תרמו לשיחה על ידי הוספת אמירה משלכם.";
+s.convIsClosed = "השיחה הזו סגורה.";
+s.noMoreVotingAllowed = "ההצבעה נסגרה.";
+
+
+s.topic_good_01 = "מה כדאי לעשות בחדר הילדים?";
+s.topic_good_01_reason = "שאלה פתוחה, כל אחד יכול להוסיף דעה או תשובה לשאלה כזו.";
+s.topic_good_02 = "מה דעתכם על ההצעה החדשה?";
+s.topic_good_02_reason = "שאלה פתוחה, כל אחד יכול להוסיף דעה או תשובה לשאלה כזו.";
+s.topic_good_03 = "האם תוכלו לחשוב על סיבות שמאטות את ההתקדמות שלנו?";
+
+s.topic_bad_01 = "אנא דווחו מה מצבכם לקראת הפגישה";
+s.topic_bad_01_reason = "אנשים מצוותים שונים יצביעו על התגובות, אבל לא בטוח שיש להם די מידע על מנת להצביע באופן חשאי."
+s.topic_bad_02 = "מה נותר לעשות לפני כינוס הפגישה?";
+s.topic_bad_02_reason = "";
+
+
+module.exports = s;
