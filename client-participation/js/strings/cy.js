2e9b645290b776498449ee1a92825d2ca975e946
Gwilym Morus-Baird <noreply@github.com>
2021-04-07 11:07:14 -0400

    Add Welsh translation strings to participation interface.
    Commit: Patrick Connolly <patrick.c.connolly@gmail.com>

---
+++ client-participation/js/strings/cy.js
@@ -0,0 +1,128 @@
+// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var s = {};
+
+s.agree = "Cytuno";
+s.disagree = "Anghytuno";
+s.pass = "Dim diddordeb / Ansicr";
+
+s.importantCheckbox = "Mae’r sylw yma’n bwysig";
+s.howImportantPrompt = "Pa mor bwysig ydi’r sylw yma?";
+s.howImportantLow = "Llai";
+s.howImportantMedium = "Canolig";
+s.howImportantHigh = "Mwy";
+
+s.modSpam = "Spam";
+s.modOffTopic = "Amherthnasol";
+s.modImportant = "Pwysig";
+s.modSubmitInitialState = "Mynd heibio (dim o’r uchod), datganiad nesaf";
+s.modSubmit = "Cwblhawyd, datganiad nesaf";
+
+s.x_wrote = "ysgrifennodd:";
+s.x_tweeted = "trydarodd:";
+s.comments_remaining = "{{num_comments}} ar ôl";
+s.comments_remaining2 = "{{num_comments}} o ddatganiadau ar ôl";
+s.group_123 = "Grŵp:";
+s.comment_123 = "Datganiad:";
+s.majorityOpinion = "Barn y Mwyafrif";
+s.majorityOpinionShort = "Mwyafrif";
+s.info = "Gwybodaeth";
+s.addPolisToYourSite = "<img style='height: 20px; margin: 0px 4px;' src='{{URL}}'/>";
+s.privacy = "Preifatrwydd";
+s.TOS = "TOS";
+s.writePrompt = "Rhannwch eich safbwynt ...";
+s.anonPerson = "Anhysbys";
+s.helpWhatAmISeeingTitle = "Beth ydw i’n ei weld?";
+s.helpWhatAmISeeing = "Mae'r bobl sy'n pleidleisio’r un ffordd wedi’u rhoi mewn grwpiau. Cliciwch grŵp i weld pa safbwyntiau maen nhw'n eu rhannu.";
+s.helpWhatDoIDoTitle = "Beth ydw i’n ei wneud?";
+s.helpWhatDoIDo = "Pleidleisiwch ar ddatganiadau pobl eraill trwy glicio 'cytuno' neu 'anghytuno'. Ysgrifennwch ddatganiad (cadwch bob un i un syniad). Gwahoddwch eich ffrindiau i'r sgwrs!";
+s.writeCommentHelpText = "A yw eich safbwyntiau neu'ch profiadau ar goll o'r sgwrs? Os felly, <b> ychwanegwch nhw </b> yn y blwch isod.";
+s.helpWriteListIntro = "Beth sy'n gwneud datganiad da?";
+s.helpWriteListStandalone = "Syniad ar ei ben ei hun";
+s.helpWriteListRaisNew = "Codwch safbwyntiau, profiadau neu faterion newydd";
+s.helpWriteListShort = "Clir a chryno (wedi'i gyfyngu i 140 nod)";
+s.heresHowGroupVoted = "Dyma sut y pleidleisiodd Grŵp {{GROUP_NUMBER}}:";
+s.one_person = "{{x}} unigolyn";
+s.x_people = "{{x}} o bobl";
+s.acrossAllPtpts = "Ar draws yr holl gyfranogwyr:";
+s.xPtptsSawThisComment = " wedi gweld y datganiad hwn";
+s.xOfThoseAgreed = "o'r cyfranogwyr hynny wedi cytuno";
+s.xOfthoseDisagreed = "o'r cyfranogwyr hynny wedi anghytuno";
+s.opinionGroups = "Grwpiau Barn";
+s.topComments = "Datganiadau Blaenaf";
+s.divisiveComments = "Datganiadau Cynhennus";
+s.pctAgreed = "Cytunodd {{pct}}%";
+s.pctDisagreed = "Anghytunodd {{pct}}%";
+s.pctAgreedLong = "Cytunodd{{pct}}% o bawb sydd wedi pleidleisio ar ddatganiad {{comment_id}}.";
+s.pctAgreedOfGroup = "Cytunodd {{pct}}% o Grŵp {{group}}.";
+s.pctDisagreedOfGroup = "Anghytunodd {{pct}}% o Grŵp {{group}}.";
+s.pctDisagreedLong = "Anghytunodd {{pct}}% o bawb sydd wedi pleidleisio ar ddatganiad {{comment_id}}.";
+s.pctAgreedOfGroupLong = "Cytunodd {{pct}}% o’r rheiny yng Nghrŵp {{group}} a bleidleisiodd ar ddatganiad {{comment_id}}.";
+s.pctDisagreedOfGroupLong = "Anghytunodd {{pct}}% o’r rheiny yng Nghrŵp {{group}} a bleidleisiodd ar ddatganiad {{comment_id}}.";
+s.commentSent = "Cyflwynwyd eich datganiad! Dim ond cyfranogwyr eraill fydd yn gweld eich datganiad ac yn cytuno neu'n anghytuno.";
+s.commentSendFailed = "Roedd gwall wrth gyflwyno'ch datganiad.
+";
+s.commentSendFailedEmpty = "Roedd gwall wrth gyflwyno'ch datganiad - Ni ddylai'r datganiad fod yn wag.
+";
+s.commentSendFailedTooLong = "Cafwyd gwall wrth gyflwyno'ch datganiad - Mae'r datganiad yn rhy hir.";
+s.commentSendFailedDuplicate = "Roedd gwall wrth gyflwyno'ch datganiad - Mae datganiad union yr un fath yn bodoli eisoes.";
+s.commentErrorDuplicate = "Dyblygiad! Mae'r datganiad hwnnw eisoes yn bodoli.";
+s.commentErrorConversationClosed = "Mae'r sgwrs hon ar gau. Ni ellir cyflwyno datganiadau pellach.";
+s.commentIsEmpty = "Mae'r datganiad yn wag";
+s.commentIsTooLong = "Mae'r datganiad rhy hir";
+s.hereIsNextStatement = "Llwyddiant pleidleisio. Ewch i fyny i weld y datganiad nesaf.";
+s.connectFacebook = "Cysylltu Facebook";
+s.connectTwitter = "Cysylltu Twitter";
+s.connectToPostPrompt = "Cysylltwch broffil i gyflwyno datganiad. Ni fyddwn yn postio i'ch llinell amser.";
+s.connectToVotePrompt = "Cysylltwch broffil i bleidleisio. Ni fyddwn yn postio i'ch llinell amser.";
+s.tip = "Awgrym:";
+s.commentWritingTipsHintsHeader = "Awgrymiadau ar gyfer ysgrifennu datganiadau.";
+s.tipCharLimit = "Mae datganiadau wedi'u cyfyngu i {{char_limit}}  o lythrennau.";
+s.tipCommentsRandom = "Cofiwch, mae datganiadau yn cael eu harddangos ar hap ac nid ydych yn ymateb yn uniongyrchol i ddatganiadau cyfranwyr eraill.";
+s.tipOneIdea = "Rhannwch ddatganiadau hir sy'n cynnwys mwy nag un syniad. Mae hyn yn ei gwneud hi'n haws i eraill bleidleisio ar eich datganiad.";
+s.tipNoQuestions = "Ni ddylai datganiadau fod ar ffurf cwestiwn. Bydd cyfranwyr yn cytuno neu'n anghytuno â'r datganiadau a wnewch.";
+s.commentTooLongByChars = "Mae'r datganiad rhy hir gan {{CHARACTERS_COUNT}} o lythrennau.";
+s.notSentSinceDemo = "(ddim mewn gwirionedd, demo yw hwn)";
+s.submitComment = "Cyflwyno";
+s.tipStarred = "Wedi'i farcio fel rhywbeth pwysig.";
+s.participantHelpWelcomeText = "Croeso i fath newydd o sgwrs - <em> pleidleisiwch </em> ar ddatganiadau pobl eraill.";
+s.participantHelpGroupsText = "Mae'r bobl sy'n pleidleisio’r un fath  <span style='font-weight: 700;'>wedi'u grwpio.</span> Cliciwch grŵp i weld pa safbwyntiau maen nhw'n eu rhannu. <a style='font-weight: 700; cursor: pointer; text-decoration: underline' id='helpTextGroupsExpand'>...mwy</a>";
+s.participantHelpGroupsNotYetText = "Bydd y ddelwedd yn ymddangos unwaith y bydd 7 cyfrannwr wedi dechrau pleidleisio.";
+s.helpWhatAreGroupsDetail = "<p>Mae'n debyg eich bod wedi gweld 'recommended products' ar Amazon, neu 'recommended films' ar Netflix. Mae pob un o'r gwasanaethau hynny'n defnyddio ystadegau i grwpio'r defnyddiwr gyda phobl sy'n prynu ac yn gwylio pethau tebyg, yna'n dangos pethau iddyn nhw'n seiliedig ar beth ddaru'r grŵp brynu neu wylio. </p> <p> Pan fydd defnyddiwr yn pleidleisio ar ddatganiadau, maen nhw'n cael eu grwpio gyda phobl sydd wedi pleidleisio yn yr un ffordd! Gallwch weld y grwpiau hynny isod. Mae pob un yn cynnwys pobl sydd â barn debyg. Mae syniadau hynod ddiddorol i'w darganfod ym mhob sgwrs. Ewch ymlaen - cliciwch grŵp i weld beth ddaeth â nhw at ei gilydd a beth sy'n eu gwneud yn unigryw! </p>";
+s.socialConnectPrompt = "Dewiswch i gysylltu i weld ffrindiau a phobl rydych chi'n eu dilyn yn y ddelwedd.";
+s.connectFbButton = "Cysylltwch gyda Facebook";
+s.connectTwButton = "Cysylltwch gyda Twitter";
+s.polis_err_reg_fb_verification_email_sent = "Gwiriwch eich e-bost am ddolen wirio, yna dychwelwch yma i barhau.";
+s.polis_err_reg_fb_verification_noemail_unverified = "Mae eich cyfrif Facebook heb ei wirio. Gwiriwch eich cyfeiriad e-bost gyda Facebook, yna dychwelwch yma i barhau.";
+s.showTranslationButton = "Cychwyn cyfieithiad trydydd parti";
+s.hideTranslationButton = "Gorffen cyfieithiad";
+s.thirdPartyTranslationDisclaimer = "Cyfieithiad wedi ei ddarparu gan drydydd parti";
+
+s.notificationsAlreadySubscribed = "Rydych chi wedi tanysgrifio i dderbyn diweddariadau ar gyfer y sgwrs hon.";
+s.notificationsGetNotified = "Cewch eich hysbysu pan fydd mwy o ddatganiadau'n cyrraedd:";
+s.notificationsEnterEmail = "Rhowch eich cyfeiriad e-bost i gael gwybod pan fydd mwy o ddatganiadau'n cyrraedd:";
+s.labelEmail = "E-bost";
+s.notificationsSubscribeButton = "Tanysgrifwch";
+s.notificationsSubscribeErrorAlert = "Gwall wrth danysgrifo";
+s.noCommentsYet = "Nid oes unrhyw ddatganiadau eto.";
+s.noCommentsYetSoWrite = "Dechreuwch y sgwrs hon trwy ychwanegu datganiad.";
+s.noCommentsYetSoInvite = "Dechreuwch y sgwrs hon trwy wahodd mwy o gyfranogwyr, neu ychwanegu datganiad.";
+s.noCommentsYouVotedOnAll = "Rydych chi wedi pleidleisio ar yr holl ddatganiadau.";
+s.noCommentsTryWritingOne = "Os oes gennych rywbeth i'w ychwanegu, ceisiwch ysgrifennu eich datganiad eich hun.";
+s.convIsClosed = "Mae'r sgwrs hon ar gau.";
+s.noMoreVotingAllowed = "Ni chaniateir pleidleisio pellach.";
+
+
+s.topic_good_01 = "Beth ddylen ni ei wneud am yr ystafell pingpong?";
+s.topic_good_01_reason = "penagored, gall unrhyw un gael barn ar atebion i'r cwestiwn hwn";
+s.topic_good_02 = "Beth ydych chi'n ei feddwl am y cynnig newydd?";
+s.topic_good_02_reason = "penagored, gall unrhyw un gael barn ar atebion i'r cwestiwn hwn";
+s.topic_good_03 = "A allwch chi feddwl am unrhyw beth sy'n arafu’r gwaith?";
+
+s.topic_bad_01 = "pawb i adrodd ar eich parodrwydd i lansio";
+s.topic_bad_01_reason = "bydd pobl o wahanol dimau yn pleidleisio ar yr ymatebion, ond efallai na fydd ganddyn nhw ddigon o wybodaeth i bleidleisio'n hyderus.";
+s.topic_bad_02 = "beth yw ein rhwystrau lansio?";
+s.topic_bad_02_reason = "";
+
+
+module.exports = s;
