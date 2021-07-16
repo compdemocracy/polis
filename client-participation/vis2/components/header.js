12696d6de746666eb8e91bebbc1cae6ff807218d
Josh <josh.smith@demos.co.uk>
2021-07-16 15:54:46 +0000

    Restored Polis logo

--- client-participation/vis2/components/header.js
+++ client-participation/vis2/components/header.js
@@ -1,28 +1,21 @@
 import React from "react";
 import _ from "lodash";
-import Logo from "./coinLogo";
+import Logo from "./hexLogo";
 import Gear from "./gear";
 
 class Header extends React.Component {
   render() {
     return (
-      <div>
       <div style={{
         maxWidth: 768,
-        padding: "0px 10px",
-        margin: "0 auto",
+        padding: "20px 10px 0px 10px",
+        margin: "auto",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
       }}>
-        <div style={{width: "480px", 
-                     margin:"0 auto"}}>
-          {this.props.is_embedded ? "" : <Logo/>}
-          <h2 style={{textAlign: "center",
-                      fontSize: "2.2em"}}>OFFICIAL: SENSITIVE</h2>
-          {this.props.is_owner ? <Gear conversation_id={this.props.conversation_id}/> : ""}
-        </div>
-      </div>
+        {this.props.is_embedded ? "" : <Logo/>}
+        {this.props.is_owner ? <Gear conversation_id={this.props.conversation_id}/> : ""}
       </div>
     )
   }
