9bc9ae8b49dec489beb4c3b7aed20a1d9962489d
Christopher Small <metasoarous@gmail.com>
2021-07-16 13:50:02 -0700

    remove xip.io domain allowlist option for client-participation
    also add comments about options

--- client-participation/polis.config.template.js
+++ client-participation/polis.config.template.js
@@ -1,12 +1,15 @@
 module.exports = {
   domainWhitelist: [
+    // local ports
     "^localhost$",
     "^127\\.0\\.0\\.1$",
     "^192\\.168\\.1\\.140$",
+    // sample configuration for main pol.is deployment
     "^pol\\.is",
     ".+\\.pol\\.is$",
-    "^(n|x|ssl)ip\\.io$",
-    ".+\\.(n|x|ssl)ip\\.io$",
+    // These allow for local ip routing for remote dev deployment
+    "^(n|ssl)ip\\.io$",
+    ".+\\.(n|ssl)ip\\.io$",
   ],
 
   // Point to a polisServer instance (local recommended for dev)
