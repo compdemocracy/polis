88bfd4a93016e4143ca04abfdb4de7ac80e0be4e
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-12-04 22:39:14 -0500

    Added SQL migration to remove Canvas LMS features from database.

---
+++ server/postgres/migrations/000005_remove_canvas_support.sql
@@ -0,0 +1,5 @@
+DROP TABLE lti_users CASCADE;
+DROP TABLE lti_context_memberships CASCADE;
+DROP TABLE canvas_assignment_callback_info CASCADE;
+DROP TABLE canvas_assignment_conversation_info CASCADE;
+DROP TABLE lti_oauthv1_credentials CASCADE;
