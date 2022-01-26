ac32467b896332433cde4f068db393896ebb99e7
Chris Krenn <crkrenn@gmail.com>
2022-01-26 13:58:46 +0000

    updating client-participation webpack

--- client-participation/webpack.config.js
+++ client-participation/webpack.config.js
@@ -7,9 +7,9 @@ module.exports = {
     "./vis2/vis2"
   ],
   output: {
-    path: path.join(__dirname, "dist_foo"),
+    path: path.join(__dirname, "dist"),
     filename: "vis_bundle.js",
-    publicPath: "SET_THIS_FROM_GULP"
+    publicPath: "/dist/",
   },
   mode: 'production',
   optimization: {
