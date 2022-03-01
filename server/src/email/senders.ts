d0218078a8d8d4602d2fe7bdde910671d893cace
Tamas Soos <tamas.soos@toptal.com>
2022-03-01 21:33:37 +0100

    Reapplied changes in senders.ts - step 3

--- server/src/email/senders.ts
+++ server/src/email/senders.ts
@@ -25,7 +25,7 @@ function sendTextEmailWithBackup(
   text: any
 ) {
   const transportTypes = process.env.EMAIL_TRANSPORT_TYPES
-    ? process.env.EMAIL_TRANSPORT_TYPES.split(",")
+  ? config.get('email_transport_types').split(",")
     : ["aws-ses", "mailgun"];
   if (transportTypes.length < 2) {
     new Error("No backup email transport available.");
