dc36851342f347f7dc8b698ba0d7eca00325ac4a
Colin Megill <colinmegill@gmail.com>
2022-09-23 03:41:31 -0700

    prettier

--- client-report/src/components/commentsGraph/commentsGraph.js
+++ client-report/src/components/commentsGraph/commentsGraph.js
@@ -5,14 +5,15 @@ import _ from "lodash";
 import * as globals from "../globals";
 import graphUtil from "../../util/graphUtil";
 import Axes from "../graphAxes";
-// import Participants from "./graphParticipants";
-// import Hull from "./hull";
 import Comments from "./comments";
 
-const TextSegment = ({t, i}) => <tspan x="27" y={i * 14}>{t}</tspan>
+const TextSegment = ({ t, i }) => (
+  <tspan x="27" y={i * 14}>
+    {t}
+  </tspan>
+);
 
 class CommentsGraph extends React.Component {
-
   constructor(props) {
     super(props);
     this.Viewer = null;
@@ -23,12 +24,11 @@ class CommentsGraph extends React.Component {
 
   handleCommentClick(selectedComment) {
     return () => {
-      this.setState({selectedComment});
-    }
+      this.setState({ selectedComment });
+    };
   }
 
   render() {
-
     if (!this.props.math) {
       return null;
     }
@@ -46,53 +46,53 @@ class CommentsGraph extends React.Component {
     } = graphUtil(this.props.comments, this.props.math, this.props.badTids);
 
     return (
-      <div style={{position: "relative"}}>
+      <div style={{ position: "relative" }}>
         <div>
           <p style={globals.primaryHeading}> Statements </p>
           <p style={globals.paragraph}>
-          How do statements relate to each other? Did people who agreed with one comment also agree with another?
+            How do statements relate to each other? Did people who agreed with one comment also
+            agree with another?
           </p>
           <p style={globals.paragraph}>
-          In this graph, statements that met disagreement are closer together if they were voted on similarly.
-          Those statements which were voted on differently are further apart.
+            In this graph, statements that met disagreement are closer together if they were voted
+            on similarly. Those statements which were voted on differently are further apart.
           </p>
-          <p style={Object.assign({}, globals.paragraph, {fontStyle: "italic"})}>
-          This is important because it is the basis on which we will lay out and cluster participants in a 2d space in later steps
-          (closer to the statements on which they agreed). There are no meaningful axes, but there are regions of statements that lend a
-          certain personality to a given area.
+          <p style={Object.assign({}, globals.paragraph, { fontStyle: "italic" })}>
+            This is important because it is the basis on which we will lay out and cluster
+            participants in a 2d space in later steps (closer to the statements on which they
+            agreed). There are no meaningful axes, but there are regions of statements that lend a
+            certain personality to a given area.
           </p>
         </div>
-        <p style={{fontWeight: 500, maxWidth: 600, lineHeight: 1.4, minHeight: 50}}>
-          {
-            this.state.selectedComment ?
-            "#" + this.state.selectedComment.tid + ". " + this.state.selectedComment.txt :
-            "Click a statement, identified by its number, to explore regions of the graph."
-          }
+        <p style={{ fontWeight: 500, maxWidth: 600, lineHeight: 1.4, minHeight: 50 }}>
+          {this.state.selectedComment
+            ? "#" + this.state.selectedComment.tid + ". " + this.state.selectedComment.txt
+            : "Click a statement, identified by its number, to explore regions of the graph."}
         </p>
-          <svg
-            style={{
-              border: "1px solid rgb(180,180,180)",
-            }}
-            width={this.props.height ? this.props.height : globals.side}
-            height={this.props.height ? this.props.height : globals.side}>
-            {/* Comment https://bl.ocks.org/mbostock/7555321 */}
-            <g transform={`translate(${globals.side / 2}, ${15})`}>
-              <text
-                style={{
-                  fontFamily: "Georgia",
-                  fontSize: 14,
-                  fontStyle: "italic"
-                }}
-                textAnchor="middle">
-
-              </text>
-            </g>
-            <Axes xCenter={xCenter} yCenter={yCenter} report={this.props.report}/>
-            // {/*<Participants points={baseClustersScaled}/>*/}
-            {/* this.props.math["group-clusters"].map((cluster, i) => {
+        <svg
+          style={{
+            border: "1px solid rgb(180,180,180)",
+          }}
+          width={this.props.height ? this.props.height : globals.side}
+          height={this.props.height ? this.props.height : globals.side}
+        >
+          {/* Comment https://bl.ocks.org/mbostock/7555321 */}
+          <g transform={`translate(${globals.side / 2}, ${15})`}>
+            <text
+              style={{
+                fontFamily: "Georgia",
+                fontSize: 14,
+                fontStyle: "italic",
+              }}
+              textAnchor="middle"
+            ></text>
+          </g>
+          <Axes xCenter={xCenter} yCenter={yCenter} report={this.props.report} />
+          // {/*<Participants points={baseClustersScaled}/>*/}
+          {/* this.props.math["group-clusters"].map((cluster, i) => {
               return (<text x={300} y={300}> Renzi Supporters </text>)
             }) : null */}
-            {/*
+          {/*
               hulls.map((hull) => {
                 let gid = hull.group[0].gid;
                 if (_.isNumber(this.props.showOnlyGroup)) {
@@ -103,22 +103,20 @@ class CommentsGraph extends React.Component {
                 return <Hull key={gid} hull={hull}/>
               })
             */}
-            {
-              commentsPoints ?
-              <Comments
-                {...this.props}
-                handleClick={this.handleCommentClick.bind(this)}
-                points={commentsPoints}
-                xx={xx}
-                yy={yy}
-                xCenter={xCenter}
-                yCenter={yCenter}
-                xScaleup={commentScaleupFactorX}
-                yScaleup={commentScaleupFactorY}/> :
-              null
-            }
-          </svg>
-
+          {commentsPoints ? (
+            <Comments
+              {...this.props}
+              handleClick={this.handleCommentClick.bind(this)}
+              points={commentsPoints}
+              xx={xx}
+              yy={yy}
+              xCenter={xCenter}
+              yCenter={yCenter}
+              xScaleup={commentScaleupFactorX}
+              yScaleup={commentScaleupFactorY}
+            />
+          ) : null}
+        </svg>
       </div>
     );
   }
--- client-report/src/components/correlationMatrix/matrix.js
+++ client-report/src/components/correlationMatrix/matrix.js
@@ -1,24 +1,18 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import React from "react";
-// import { connect } from "react-redux";
-import Flex from "../framework/flex";
 
-import Radium from "radium";
 import _ from "lodash";
 import * as globals from "../globals";
 
 var leftOffset = 34;
 var topOffset = 60;
 
-var scale = d3.scaleLinear().domain([-1, 1]).range([0, 1])
+var scale = d3.scaleLinear().domain([-1, 1]).range([0, 1]);
 
 const square = 20;
 
-@Radium
 class Matrix extends React.Component {
-
-
   onMouseEnterCell(row, column, correlation) {
     this.setState({
       mouseOverRow: row,
@@ -43,56 +37,46 @@ class Matrix extends React.Component {
             return this.onMouseEnterCell(row, column, comment);
           }}
           width={square}
-          height={square} />
-          <text
-            x={10}
-            y={13}
-            textAnchor={"middle"}
-            fill={Math.abs(Math.floor(comment * 100)) > 30 ? "white" : "rgb(60,60,60)"}
-            style={{
-              fontFamily: globals.sans,
-              fontSize: 10,
-              pointerEvents: "none",
-            }}>
-            {Math.floor(comment * 100)}
-          </text>
+          height={square}
+        />
+        <text
+          x={10}
+          y={13}
+          textAnchor={"middle"}
+          fill={Math.abs(Math.floor(comment * 100)) > 30 ? "white" : "rgb(60,60,60)"}
+          style={{
+            fontFamily: globals.sans,
+            fontSize: 10,
+            pointerEvents: "none",
+          }}
+        >
+          {Math.floor(comment * 100)}
+        </text>
       </g>
-    )
+    );
   }
 
   makeColumn(comments, row) {
-    // <text
-    //   transform={"translate(" + (column * square + 10) + ", 25), rotate(315)"}
-    //   fill="rgba(0,0,0,0.7)"
-    //   style={{
-    //     display: row === 0 ? "block" : "none",
-    //     fontFamily: "Helvetica, sans-serif",
-    //     fontSize: 10,
-    //     fontWeight: 700
-    //   }}
-    //   >
-    //   {this.props.formatTid(this.props.tids[column])}
-    // </text>
     return comments.map((comment, column) => {
       let markup = null;
-      if (column < row) {  /* diagonal matrix */
+      if (column < row) {
+        /* diagonal matrix */
         markup = (
-          <g key={column} >
+          <g key={column}>
             {/* this translate places the top text labels where they should go, rotated */}
             {/* this translate places the columns where they should go, and creates a gutter */}
-            <g transform={"translate(" + (column * square) + ", 30)"}>
+            <g transform={"translate(" + column * square + ", 30)"}>
               {this.makeRect(comment, row, column)}
             </g>
           </g>
-        )
+        );
       } else if (column === row) {
         const comment = _.find(this.props.comments, (comment) => {
-          return comment.tid === this.props.tids[column]
-        })
+          return comment.tid === this.props.tids[column];
+        });
         markup = (
-          <g key={column} >
+          <g key={column}>
             <text
-
               onMouseEnter={() => {
                 return this.onMouseExitCell();
               }}
@@ -100,22 +84,24 @@ class Matrix extends React.Component {
                 return this.onMouseExitCell();
               }}
               transform={"translate(" + (column * square + 10) + ", 46), rotate(315)"}
-              fill={ (column === this.state.mouseOverColumn || column === this.state.mouseOverRow) ? "rgba(0,0,0,1)" : "rgba(0,0,0,0.5)"}
+              fill={
+                column === this.state.mouseOverColumn || column === this.state.mouseOverRow
+                  ? "rgba(0,0,0,1)"
+                  : "rgba(0,0,0,0.5)"
+              }
               style={{
                 fontFamily: "Helvetica, sans-serif",
                 fontSize: 10,
-                fontWeight: 400
+                fontWeight: 400,
               }}
-              >
-              {
-                comment ? comment.txt : "[[[none found]]]"
-              }
+            >
+              {comment ? comment.txt : "[[[none found]]]"}
             </text>
           </g>
-        )
+        );
       }
       return markup;
-    })
+    });
   }
 
   makeRow(comments, row) {
@@ -132,11 +118,9 @@ class Matrix extends React.Component {
     return (
       <g transform={"translate(0, " + (row * 20 + topOffset) + ")"}>
         {/* this translate moves just the colored squares over to make a gutter, not the text */}
-        <g transform={"translate("+ leftOffset +", -43)"}>
-          {this.makeColumn(comments, row)}
-        </g>
+        <g transform={"translate(" + leftOffset + ", -43)"}>{this.makeColumn(comments, row)}</g>
       </g>
-    )
+    );
   }
 
   renderMatrix() {
@@ -146,20 +130,19 @@ class Matrix extends React.Component {
       <div>
         <p style={globals.primaryHeading}>{this.props.title}</p>
         <p style={globals.paragraph}>
-          What is the chance that a participant who agreed (or disagreed) with a
-          given comment also agreed (or disagreed) with another given comment?
+          What is the chance that a participant who agreed (or disagreed) with a given comment also
+          agreed (or disagreed) with another given comment?
         </p>
         <p style={globals.paragraph}>
-          Patterns emerge when we evaluate groups of statements that tended to be voted on similarly.
+          Patterns emerge when we evaluate groups of statements that tended to be voted on
+          similarly.
         </p>
         <p style={globals.paragraph}>
-          This is an important bit of math (called a correlation matrix) that goes into making the graph above.
+          This is an important bit of math (called a correlation matrix) that goes into making the
+          graph above.
         </p>
 
-
-
-        <svg width="100%" height={side} style={{cursor: "crosshair"}}>
-
+        <svg width="100%" height={side} style={{ cursor: "crosshair" }}>
           <rect
             fill="rgba(0,0,0,0)"
             onMouseEnter={() => {
@@ -169,9 +152,12 @@ class Matrix extends React.Component {
               return this.onMouseExitCell();
             }}
             width={side}
-            height={side} />
+            height={side}
+          />
 
-          {!this.state.mouseOverCorrelation ? " " :
+          {!this.state.mouseOverCorrelation ? (
+            " "
+          ) : (
             <text
               x={300}
               y={40}
@@ -179,20 +165,19 @@ class Matrix extends React.Component {
               fill={d3.interpolatePuOr(scale(this.state.mouseOverCorrelation))}
               style={{
                 fontFamily: globals.sans,
-                fontSize: 18
-              }}>
-              {`${Math.round(this.state.mouseOverCorrelation * 1000) / 10}% chance of casting the same vote on these two statements`}
+                fontSize: 18,
+              }}
+            >
+              {`${
+                Math.round(this.state.mouseOverCorrelation * 1000) / 10
+              }% chance of casting the same vote on these two statements`}
             </text>
-          }
+          )}
 
-          <g transform={"translate(200,0), rotate(45)"/* abstract translate magic number */}>
-          {this.props.probabilities.map((comments, row) => {
-            return (
-              <g key={row}>
-                {this.makeRow(comments, row)}
-              </g>
-            )
-          })}
+          <g transform={"translate(200,0), rotate(45)" /* abstract translate magic number */}>
+            {this.props.probabilities.map((comments, row) => {
+              return <g key={row}>{this.makeRow(comments, row)}</g>;
+            })}
           </g>
         </svg>
       </div>
@@ -207,9 +192,7 @@ class Matrix extends React.Component {
     );
   }
   renderLoading() {
-    return (
-      <div>loading matrix... (may take up to a minute)</div>
-    );
+    return <div>loading matrix... (may take up to a minute)</div>;
   }
   render() {
     if (this.props.error) {
