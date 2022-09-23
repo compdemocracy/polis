ffec6177be4b430bb82d6daebfb00bf831b15f2c
Colin Megill <colinmegill@gmail.com>
2022-09-23 03:42:22 -0700

    prettier dev server

--- client-report/dev-server.js
+++ client-report/dev-server.js
@@ -9,40 +9,32 @@ var request = require("request");
 var app = express();
 var compiler = webpack(config);
 
-app.set('port', 5010);
+app.set("port", 5010);
 
-app.use('/data', express.static('data'))
-
-app.use(require("webpack-dev-middleware")(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
-app.use(require("webpack-hot-middleware")(compiler));
-
-
-
-
-
-
+app.use("/data", express.static("data"));
 
 const serviceUrl = process.env.SERVICE_URL ? process.env.SERVICE_URL : "https://preprod.pol.is";
 
 console.log("SERVICE_URL:", serviceUrl);
 
-function proxy (req, res) {
-  const hostHeader = serviceUrl.replace(/.*\/\//,"");
-  const headers =  Object.assign(req.headers, {"origin": serviceUrl, "Origin": serviceUrl, "host": hostHeader, "Host": hostHeader});
+function proxy(req, res) {
+  const hostHeader = serviceUrl.replace(/.*\/\//, "");
+  const headers = Object.assign(req.headers, {
+    origin: serviceUrl,
+    Origin: serviceUrl,
+    host: hostHeader,
+    Host: hostHeader,
+  });
 
   var x = request({
     url: serviceUrl + req.path,
     qs: req.query,
     headers: headers,
-    rejectUnauthorized:false,
+    rejectUnauthorized: false,
   });
   req.pipe(x);
   x.pipe(res);
-  x.on("error", function(err) {
+  x.on("error", function (err) {
     console.log("error", err);
     res.status(500).send("proxy error");
   });
@@ -53,14 +45,12 @@ app.get(/^\/api\//, proxy);
 app.put(/^\/api\//, proxy);
 app.post(/^\/api\//, proxy);
 
-
-
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Listen for requests
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get("port"), function () {
   var port = server.address().port;
-  console.log('Listening on port ' + port);
+  console.log("Listening on port " + port);
 });
