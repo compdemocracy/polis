598475948d21d22d68901efa2d2256bfb66fb842
Christian Weilbach <ch_weil@topiq.es>
2022-11-05 22:12:37 -0700

    Add Farsi translation.

---
+++ client-participation/js/strings/fa.js
@@ -0,0 +1,125 @@
+// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var s = {};
+
+s.agree = "موافق";
+s.disagree = "مخالف";
+s.pass = "رد کن/مطمئن نیستم";
+
+s.importantCheckbox = "این نطر مهم است";
+s.howImportantPrompt = "این گفته چقدر مهم است؟";
+s.howImportantLow = "کم";
+s.howImportantMedium = "متوسط";
+s.howImportantHigh = "زیاد";
+
+s.modSpam = "اسپم";
+s.modOffTopic = "خارج از موضوع";
+s.modImportant = "مهم";
+s.modSubmitInitialState = "رد کن (هیچ یک از موارد بالا)، گفته بعدی";
+s.modSubmit = "انجام شد، گفته بعدی";
+
+s.x_wrote = "نوشت";
+s.x_tweeted = "توییت کرد";
+s.comments_remaining = "{{تعداد نظرات}} باقی مانده";
+s.comments_remaining2 = "{{تعداد نظرات}} گفته‌های باقی مانده";
+s.group_123 = "گروه";
+s.comment_123 = "گفته";
+s.majorityOpinion = "نظر اکثریت";
+s.majorityOpinionShort = "اکثریت";
+s.info = "اطلاعات";
+s.addPolisToYourSite = "<img style='height: 20px; margin: 0px 4px;' src='{{URL}}'/>";
+s.privacy = "حریم خصوصی";
+s.TOS = "شرایط استفاده از خدمات";
+s.writePrompt = "دیدگاه خود را به اشتراک بگذارید";
+s.anonPerson = "ناشناس";
+s.helpWhatAmISeeingTitle = "من چه چیزی را  میبینم؟";
+s.helpWhatAmISeeing = "افرادی که به صورت مشابه رای میدهند در یک گروه قرار میگیرند. روی یکه گروه کلیک کنید تا ببینید چه دیدگاه های مشترکی دارند.";
+s.helpWhatDoIDoTitle = "من چه کار میکنم؟";
+s.helpWhatDoIDo = "با کلیک روی 'موافق' و 'مخالف' به گفته دیگران رای دهید. دوستان خود را به گفتگو دعوت کنید.";
+s.writeCommentHelpText = "آیا دیدگاه شما در گفتگو وجود ندارد؟ در اینصورت، آنها را در جعبه زیر <b> اضافه کنید</b>.";
+s.helpWriteListIntro = "چه عاملی باعث خوب بودن یک گفته میشود؟";
+s.helpWriteListStandalone = "ایده مستقل";
+s.helpWriteListRaisNew = "دیدگاه، تجربه یا مشکل جدیدی را مطرح کنید.";
+s.helpWriteListShort = "واضح و مختصر (محدود به ۱۴۰ کاراکتر)";
+s.heresHowGroupVoted = "در اینجا نحوه رای دادن گروه {{شماره گروه}} آمده است";
+s.one_person = "{{x}} نفر";
+s.x_people = "{{x}} نفر";
+s.acrossAllPtpts = "از میان همه شرکت کننده‌ها";
+s.xPtptsSawThisComment = "این گفته را دیده‌اند ";
+s.xOfThoseAgreed = "از شرکت کننده‌ها موافقت کردند.";
+s.xOfthoseDisagreed = "از شرکت کننده‌ها مخالفت کردند";
+s.opinionGroups = "گروه‌های فکری";
+s.topComments = "گفته‌های برتر";
+s.divisiveComments = "گفته مثبت";
+s.pctAgreed = "{{pct}}% موافق:";
+s.pctDisagreed = "{{pct}}% مخالف:";
+s.pctAgreedLong = "{{pct}}% از کسانی که به گفته {{comment_id}} رای دادند موافقت کردند.";
+s.pctAgreedOfGroup = "{{pct}}% از گروه {{group}} موافقت کردند";
+s.pctDisagreedOfGroup = "{{pct}}% از گروه {{group}} مخالفت کردند";
+s.pctDisagreedLong = "{{pct}}% از کسانی که به گفته {{comment_id}} رای دادند مخالفت کردند ";
+s.pctAgreedOfGroupLong = "{{pct}}% از کسانی که از گروه {{group}} به گفته {{comment_id}} رای دادند موافقت کردند ";
+s.pctDisagreedOfGroupLong = "{{pct}}% از کسانی که از گروه {{group}} به گفته {{comment_id}} رای دادند مخالفت کردند ";
+s.commentSent = "گفته ارسال شد! تنها شرکت کننده‌های دیگر گفته شما را میبینند و موافقت یا مخالفت میکنند.";
+s.commentSendFailed = "در ارسال گفته شما خطایی رخ داد.";
+s.commentSendFailedEmpty = "در ارسال گفته شما خطایی رخ داد - گفته نباید خالی باشد";
+s.commentSendFailedTooLong = "در ارسال گفته شما خطایی رخ داد - گفته خیلی طولانی است";
+s.commentSendFailedDuplicate = "در ارسال گفته شما خطایی رخ داد - گفته یکسانی از قبل وجود دارد.";
+s.commentErrorDuplicate = "تکراری! این گفته از قبل وجود دارد";
+s.commentErrorConversationClosed = "این گفتگو بسته شده است. گفته جدیدی قابل ارسال نیست";
+s.commentIsEmpty = "گفته خالی است";
+s.commentIsTooLong = "گفته خیلی طولانی است";
+s.hereIsNextStatement = "رای دهی موفق.";
+
+s.connectFacebook = "فیسبوک را متصل کن";
+s.connectTwitter = "توییتر را متصل کن";
+s.connectToPostPrompt = "هویتی را برای ارسال کفته متصل کن. ما به تایملاین شما چیزی ارسال نمیکنیم.";
+s.connectToVotePrompt = "هویتی را برای رای دادن متصل کن. ما به تایملاین شما چیزی ارسال نمیکنیم.";
+s.tip = "نکته";
+s.commentWritingTipsHintsHeader = "نکاتی برای نوشتن بیانیه";
+s.tipCharLimit = "بیانیه ها محدود به {{char_limit}} کاراکتر هستند";
+s.tipCommentsRandom = "لطفاً به یاد داشته باشید، گفته‌ها به صورت تصادفی نمایش داده می شوند و شما مستقیماً به اظهارات سایر شرکت کنندگان پاسخ نمی دهید:";
+s.tipOneIdea = "جملات طولانی که حاوی چندین ایده هستند را جدا کنید. این کار رای دادن به گفته شما را برای دیگران آسان تر می کند:";
+s.tipNoQuestions = "اظهارات نباید به صورت سوال باشد. شرکت کنندگان با اظهارات شما موافق یا مخالف خواهند بود:";
+s.commentTooLongByChars = "طول گفته از محدودیت کاراکتر به اندازه {{CHARACTERS_COUNT}} بیشتر است";
+s.notSentSinceDemo = "نه واقعا، این یک نمایه است";
+s.submitComment = "ارسال";
+s.tipStarred = "به عنوان مهم علامت گذاری شده است";
+s.participantHelpWelcomeText = "به نوع جدیدی از مکالمه خوش آمدید - به گفته‌های دیگران <em>رای دهید</em>:";
+s.participantHelpGroupsText = "افرادی که به طور مشابه رای می‌دهند <span style='font-weight: 700;'>گروه‌بندی می‌شوند.</span> روی گروهی کلیک کنید تا ببینید کدام دیدگاه‌ها را به اشتراک می‌گذارند. <a style='font-weight: 700; cursor: pointer; text-decoration: underline' id='helpTextGroupsExpand'>...more</a>";
+s.participantHelpGroupsNotYetText = "زمانی که 7 شرکت‌کننده رای‌گیری را آغاز کردند، تصویرسازی ظاهر می‌شود:";
+s.helpWhatAreGroupsDetail = "<p> احتمالاً \"محصولات توصیه شده\" را در آمازون یا \"فیلم های توصیه شده\" در نتفلیکس دیده اید. هر یک از این سرویس‌ها از آمار برای گروه‌بندی کاربر با افرادی استفاده می‌کنند که چیزهای مشابهی را می‌خرند و تماشا می‌کنند، سپس چیزهایی را که آن افراد خریده یا تماشا کرده‌اند به آن‌ها نشان می‌دهد.</p> <p> وقتی کاربر به بیانیه‌ها رأی می‌دهد، با افرادی گروه‌بندی می‌شوند که آن‌ها را تماشا کرده‌اند. مثل آنها رای دادند! در زیر می توانید آن گروه ها را ببینید. هر کدام متشکل از افرادی است که نظرات مشابهی دارند. در هر مکالمه بینش های جذابی برای کشف وجود دارد. ادامه دهید - روی یک گروه کلیک کنید تا ببینید چه چیزی آنها را گرد هم آورده و چه چیزی آنها را منحصر به فرد می کند! </p>";
+s.socialConnectPrompt = "به صورت اختیاری برای دیدن دوستان و افرادی که در تجسم دنبال می کنید، متصل شوید";
+s.connectFbButton = "با فیسبوک متصل شوید";
+s.connectTwButton = "با توییتر متصل شوید";
+s.polis_err_reg_fb_verification_email_sent = "لطفاً ایمیل خود را برای پیوند تأیید بررسی کنید، سپس برای ادامه به اینجا بازگردید";
+s.polis_err_reg_fb_verification_noemail_unverified = "حساب فیسبوک شما تایید نشده است. لطفاً آدرس ایمیل خود را با فیس بوک تأیید کنید، سپس برای ادامه به اینجا بازگردید";
+s.showTranslationButton = "فعال کردن ترجمه شخص ثالث";
+s.hideTranslationButton = "غیرفعال کردن ترجمه";
+
+s.notificationsAlreadySubscribed = "شما مشترک به‌روزرسانی‌های این مکالمه هستید";
+s.notificationsGetNotified = "وقتی گفته‌های جدیدی میرسد خبردار شوید.";
+s.notificationsEnterEmail = "آدرس ایمیل خود را وارد کنید که وقتی گفته هایی جدیدی میرسد خبردار شوید.";
+s.labelEmail = "ایمیل";
+s.notificationsSubscribeButton = "اشتراک";
+s.notificationsSubscribeErrorAlert = "در مشترک شدن خطایی رخ داده است";
+s.noCommentsYet = "هنوز گفته‌ای وجود ندارد";
+s.noCommentsYetSoWrite = "با اضافه کردن یک گفته این گفتگو را شروع کید";
+s.noCommentsYetSoInvite = "این گفتگو را با دعوت شرکت کننده‌های بیشتر یا اضافه کردن یک گفته شروع کنید";
+s.noCommentsYouVotedOnAll = "شما به همه گفته‌ها رای دادید";
+s.noCommentsTryWritingOne = "اگر چیزی برای افزودن دارید، سعی کنید گفته خود را بنویسید.";
+s.convIsClosed = "این گفتگو بسته شده است.";
+s.noMoreVotingAllowed = "رای دادن بیشتر مجاز نیست ";
+
+s.topic_good_01 = "در مورد اتاق پینگ پنگ باید چه کار کنیم؟:";
+s.topic_good_01_reason = "پایان باز، هر کسی میتواند در مورد پاسخهای این پرسش نظر داشته باشد.";
+s.topic_good_02 = "در مورد طرح پیشنهادی چه فکر میکنید؟";
+s.topic_good_02_reason = "پایان باز، هر کسی میتواند در مورد پاسخهای این پرسش نظر داشته باشد.";
+s.topic_good_03 = "آیا می توانید به چیزی فکر کنید که بهره وری را کاهش دهد؟";
+
+s.topic_bad_01 = "همه آمادگی خود را برای راه اندازی گزارش دهید";
+s.topic_bad_01_reason = "افراد از تیمهای مختلف در مورد واکنش‌ها رای میدهندُ ولی ممکن است به اندازه کافی دانش نداشته باشند که با اطمینان رای دهند.";
+s.topic_bad_02 = "چه چیزاهایی جلوی راه اندازی ما را میگیرند؟";
+s.topic_bad_02_reason = "";
+
+
+module.exports = s;
