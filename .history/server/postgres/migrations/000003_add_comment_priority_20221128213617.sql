efe66855adabcbcb81565a731ad8df47d4f3d723
chena11356 <achen1800@gmail.com>
2022-12-11 16:20:34 -0500

    remove weird files

--- .history/server/postgres/migrations/000003_add_comment_priority_20221128213617.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE conversations
-  ADD priority_type INTEGER NOT NULL
-    DEFAULT (0)
-
-ALTER TABLE votes 
-  ADD high_priority BOOLEAN NOT NULL 
-    DEFAULT (0)
--- .history/server/postgres/migrations/000003_add_comment_priority_20221206135340.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE conversations
-  ADD priority_type BOOLEAN NOT NULL
-    DEFAULT (false);
-
-ALTER TABLE votes 
-  ADD high_priority BOOLEAN NOT NULL 
-    DEFAULT (null);
