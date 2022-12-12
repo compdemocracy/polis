263068955758305ed773354cbdf8e75161c5beb3
Chris Krenn <crkrenn@gmail.com>
2022-12-11 18:13:09 -0800

    Common config files are working with local docker deploy

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
+# TTD: pass GIT_HASH to docker-compose
+
 
 BASEURL ?= https://127.0.0.1.sslip.io
 E2E_RUN = cd e2e; CYPRESS_BASE_URL=$(BASEURL)
+# export
+export GIT_HASH := $(shell git rev-parse --short HEAD)
 
 pull: ## Pull most recent Docker container builds (nightlies)
 	docker-compose pull
@@ -10,6 +14,22 @@ start: ## Start all Docker containers
 
 start-rebuild: ## Start all Docker containers, [re]building as needed
 	docker-compose up --detach --build
+
+stop: ## Stop all Docker containers
+	docker-compose down
+
+hash: ## Show current short hash
+	@echo Git hash: ${GIT_HASH}
+
+ENV_CONFIG = config/docker-dev-common.env
+JS_CONFIG = config/polis.config.template.common.js
+update-config: ## Copy common config files into docker directories
+	cp -f ${ENV_CONFIG} math/docker-dev.env
+	cp -f ${ENV_CONFIG} server/docker-db-dev.env
+	cp -f ${ENV_CONFIG} server/docker-dev.env
+	cp -f ${JS_CONFIG} client-admin/polis.config.template.js
+	cp -f ${JS_CONFIG} client-participation/polis.config.template.js
+	cp -f ${JS_CONFIG} client-report/polis.config.template.js
 
 e2e-install: e2e/node_modules ## Install Cypress E2E testing tools
 	$(E2E_RUN) npm install
--- client-participation/polis.config.template.js
+++ client-participation/polis.config.template.js
@@ -1,3 +1,11 @@
+
+// %%%
+// client-participation/polis.config.template.js
+// %%%
+
+// Note that this must match the participation client port specified in polisServer instance
+// rename PORT: 5001,
+
 module.exports = {
   domainWhitelist: [
     // local ports
@@ -23,7 +31,7 @@ module.exports = {
   // Note that this must match the participation client port specified in polisServer instance
   PORT: 5001,
 
-  DISABLE_INTERCOM: true,
+  // DISABLE_INTERCOM: true,
 
   // must register with facebook and get a facebook app id to use the facebook auth features
   FB_APP_ID: "661042417336977",
@@ -42,4 +50,54 @@ module.exports = {
   // Uploader settings: scp
   SCP_SUBDIR_PREPROD: "preprod",
   SCP_SUBDIR_PROD: "prod",
+// };
+
+// %%%
+// client-admin/polis.config.template.js
+// %%%
+
+// module.exports = {
+
+  // domainWhitelist: [
+  //   // local ports
+  //   "^localhost$",
+  //   "^127\\.0\\.0\\.1$",
+  //   "^192\\.168\\.1\\.140$",
+  //   // sample configuration for main pol.is deployment
+  //   "^pol\\.is",
+  //   ".+\\.pol\\.is$",
+  //   // These allow for local ip routing for remote dev deployment
+  //   "^(n|ssl)ip\\.io$",
+  //   ".+\\.(n|ssl)ip\\.io$",
+  // ],
+
+  DISABLE_INTERCOM: true,
+  DISABLE_PLANS: true,
+
+  // FB_APP_ID: '661042417336977',
+
+  //SERVICE_URL: 'http://localhost:5000',
+  // SERVICE_URL: 'https://preprod.pol.is',
+// };
+
+// %%%
+// client-report/polis.config.template.js
+// %%%
+// module.exports = {
+
+  //SERVICE_URL: 'http://localhost:5000',
+  // SERVICE_URL: 'https://preprod.pol.is',
+
+  UPLOADER: 'local', // alt: s3, scp
+
+  // Uploader settings: local
+  // LOCAL_OUTPUT_PATH: './build',
+
+  // // Uploader settings: s3
+  // S3_BUCKET_PROD: 'pol.is',
+  // S3_BUCKET_PREPROD: 'preprod.pol.is',
+
+  // // Uploader settings: scp
+  // SCP_SUBDIR_PREPROD: 'preprod',
+  // SCP_SUBDIR_PROD: 'prod',
 };
