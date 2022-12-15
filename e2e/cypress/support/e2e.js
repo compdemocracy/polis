570195a05ec91567bda1406ef56ce5ad0b223d7f
Chris Krenn <crkrenn@gmail.com>
2022-12-14 17:36:06 -0800

    added missing files

---
+++ e2e/cypress/support/e2e.js
@@ -0,0 +1,34 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+import './commands'
+
+before(() => {
+  cy.fixture('users.json').then((users) => {
+    // Ensure a default user is present for each spec.
+    // TODO: Move this into a true database seeding process.
+    for (const [type, user] of Object.entries(users)) {
+      cy.signup(user.name, user.email, user.password)
+    }
+  })
+})
+
+// Register the log collector for logging activity to terminal.
+const reporterOptions = {
+  // When to print terminal logs for tests.
+  // Options: onFail, always
+  printLogs: 'onFail'
+}
+require('cypress-terminal-report/src/installLogsCollector')(reporterOptions)
