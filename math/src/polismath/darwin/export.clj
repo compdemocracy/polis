06a1de96ba44a0f4e6cf2943820e2907a9e1d597
Christopher Small <metasoarous@gmail.com>
2023-02-16 21:33:57 -0800

    update clojure deps

--- math/src/polismath/darwin/export.clj
+++ math/src/polismath/darwin/export.clj
@@ -1,9 +1,7 @@
 ;; Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (ns polismath.darwin.export
-  (:require [taoensso.timbre.profiling :as profiling
-             :refer (pspy pspy* profile defnp p p*)]
-            [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
             [polismath.components.postgres :as db]
     ;; Probably should move the function we need out, since this should be meta only XXX
             [polismath.meta.microscope :as micro]
