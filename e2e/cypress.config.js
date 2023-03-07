0347f6a02302a59e9046d32051ea82cc33af0d8a
Bennie Rosas <ben@aliencyb.org>
2023-03-07 01:36:51 -0600

    set test TAG; use localhost by default in e2e

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require('cypress')
 
 // One way to run Cypress against a different url is to pass CYPRESS_BASE_URL env variable,
-// e.g. CYPRESS_BASE_URL=http://localhost:80 npm test
+// e.g. CYPRESS_BASE_URL=http://localhost:5000 npm test
 // See https://docs.cypress.io/guides/guides/environment-variables
 
 module.exports = defineConfig({
   blockHosts: ['*.twitter.com', '*.jsdelivr.net'],
   e2e: {
-    baseUrl: 'http://localhost:5000',
+    baseUrl: 'http://localhost',
     experimentalRunAllSpecs: true,
     video: false,
     setupNodeEvents(/*on, config*/) {
