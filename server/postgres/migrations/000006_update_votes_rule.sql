ebb89e932a5928c52a920f1bba423633991cf6c5
Christopher Small <metasoarous@gmail.com>
2023-04-09 12:14:00 -0700

    Add note about applying new migration for pg 14

--- server/postgres/migrations/000006_update_votes_rule.sql
+++ server/postgres/migrations/000006_update_votes_rule.sql
@@ -1,3 +1,8 @@
+-- This migration fixes an issue with a poorly specified rule definition in the original 000000_initial.sql schema.
+-- The old specification worked on Postgres 13, but not longer does on Postgres 14.
+-- Consequently, if you set up your Polis database prior to April 9th, 2023, and wish to upgrade to Postgres 14,
+-- you should run this migration first.
+
 DROP RULE IF EXISTS on_vote_insert_update_unique_table ON votes;
 
 CREATE RULE on_vote_insert_update_unique_table AS
