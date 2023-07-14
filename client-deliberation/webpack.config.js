6e86efec61f00dfa990437e9d723fde49428ae34
Jake Medina <jake.medina02@gmail.com>
2023-07-14 16:54:40 -0500

    polisAjax override port using ENV file

--- client-deliberation/webpack.config.js
+++ client-deliberation/webpack.config.js
@@ -19,6 +19,7 @@ var cliArgs = mri(argv)
 
 var enableTwitterWidgets = process.env.ENABLE_TWITTER_WIDGETS === 'true';
 var fbAppId = process.env.FB_APP_ID;
+var devPortOverride = process.env.DEV_PORT_OVERRIDE;
 
 module.exports = (env, options) => {
   var isDevBuild = options.mode === 'development';
@@ -72,6 +73,7 @@ module.exports = (env, options) => {
       }),
       new webpack.DefinePlugin({
         'process.env.FB_APP_ID': JSON.stringify(fbAppId),
+        'process.env.DEV_PORT_OVERRIDE': JSON.stringify(devPortOverride),
       }),
       // Only run analyzer when specified in flag.
       ...(cliArgs.analyze ? [new BundleAnalyzerPlugin({ defaultSizes: 'gzip' })] : []),
