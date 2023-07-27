d524c0e8b506a23d20311c196a9aca3d6926040b
Jake Medina <jake.medina02@gmail.com>
2023-07-27 14:36:19 -0500

    tidsToShow explanation

--- client-deliberation/src/components/Visualization2.js
+++ client-deliberation/src/components/Visualization2.js
@@ -544,7 +544,7 @@ const Visualization2 = () => {
   // Strings: copy file from client-participation?
   // math_main: cachedPcaData, comes from pca2 route but can be null?
   // comments: comes from getFancyComments()
-  // tidsToShow: tid == comment id, set to an empty array
+  // tidsToShow: if the user doesn't click on a group, then no comments in particular will be shown
   // ptptios: calls getParticipantsOfInterestIncludingSelf()
   // votesByMe: calls getVotesByMe()
   // onVoteClicked: not sure exactly what this is for??
