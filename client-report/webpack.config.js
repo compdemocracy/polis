ce32af8f3a65f8034fcd69dfbb9fb13bb7c55cd7
Bennie Rosas <ben@aliencyb.org>
2023-07-28 02:47:57 -0500

    Configure client-report webpack to do everything gulp did

--- client-report/webpack.config.js
+++ client-report/webpack.config.js
@@ -1,36 +1,71 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-var path = require("path");
-var webpack = require("webpack");
+const path = require('path');
+const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const EventHooksPlugin = require('event-hooks-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const writeHeadersJsonTask = require('./writeHeadersJsonTask');
 
 module.exports = {
-  // devtool: "source-map",
-  entry: ["./src/index"],
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "report_bundle.js",
-    publicPath: "/dist/",
+    filename: 'report_bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   plugins: [
+    new CompressionPlugin({
+      // It appears that nothing is currently using these gzipped files.
+      // So we're just going to generate it with the .gz extension and not
+      // delete the original.
+      // TODO: Decide if we want to use these gzipped files.
+      test: /\.js$/,
+      // filename: '[path][base]',
+      // deleteOriginalAssets: true,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/favicon.ico', to: 'favicon.ico' },
+      ],
+    }),
+    new EventHooksPlugin({
+      afterEmit: () => writeHeadersJsonTask(),
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index_report.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'report_style.[contenthash].css',
+    }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production"),
       "process.env.SERVICE_URL": JSON.stringify(process.env.SERVICE_URL),
-    }),
-    // new webpack.optimize.UglifyJsPlugin({  https://stackoverflow.com/questions/69468568/typeerror-webpack-optimize-uglifyjsplugin-is-not-a-constructor
-    //   compressor: {
-    //     warnings: false,
-    //   },
-    // }),
+    })
   ],
-  module: {
-    rules: [
-      { test: /\.json$/, type: "json" },
-      {
-        test: /\.(js)$/,
-        include: path.join(__dirname, "src"),
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-    ],
+  performance: {
+    // TODO: Find and remove orphan modules; Reduce bundle size.
+    hints: 'warning', // 'error' for errors, 'warning' for warnings, false to disable
+    maxAssetSize: 505000, // Size limit in bytes, default is 250000 (250 KB)
+    maxEntrypointSize: 500000, // Size limit in bytes, default is 250000 (250 KB)
   },
 };
