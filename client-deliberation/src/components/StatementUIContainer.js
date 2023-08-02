b8bd6e1b3410d37efb8f70a1656e75cd7356db3c
Jake Medina <jake.medina02@gmail.com>
2023-08-02 15:24:56 -0500

    made StatementUIContainer better

--- client-deliberation/src/components/StatementUIContainer.js
+++ client-deliberation/src/components/StatementUIContainer.js
@@ -3,8 +3,26 @@ import { Box } from "theme-ui";
 
 const StatementUIContainer = (props) => {
   return (
-    <Box sx={{ variant: "statementBox" }}>
-      <Box sx={{ margin: "10px" }}>{props.children}</Box>
+    <Box sx={{ mb: "-40px" }}>
+      <Box sx={{ variant: "statementBox" }}>
+        <Box sx={{ margin: "10px" }}>{props.children}</Box>
+      </Box>
+      <Box
+        sx={{
+          variant: "statementBox.stack",
+          width: "99%",
+          top: "-26px",
+          zIndex: "-1",
+        }}
+      />
+      <Box
+        sx={{
+          variant: "statementBox.stack",
+          width: "98%",
+          top: "-52px",
+          zIndex: "-2",
+        }}
+      />
     </Box>
   );
 };
