8ab7c6a4e78f73ae10413ce41e86e81c61232aad
“Jan”Steinhauer” <insta.steini14@gmail.com>
2023-09-29 12:18:07 -0500

    added the IntegrateBox

--- client-deliberation/src/components/IndividualDeliberation.js
+++ client-deliberation/src/components/IndividualDeliberation.js
@@ -6,9 +6,16 @@ import Title from "./Title";
 import { MDXProvider } from "@theme-ui/mdx";
 import IndividualDeliberationMD from "./IndividualDeliberationMD.mdx"
 import IntegrateLink from "./IntegrateLink";
+import IntegrateBox from "./IntegrateBox";
 
 
 const IndividualDeliberation = () => {
+
+  const items = [
+    "When this embed code loads on your website, it will either create a new conversation (if one is not already associated with the string passed into PAGE_ID) or load an existing conversation.",
+    "This embed code will keep track of what conversations belongs on what pages via the data-page_id HTML attribute.",
+    "Simply replace \"PAGE_ID\", either manually or in your templates, to create new conversations and load existing ones in the right place."
+  ];
 
   return (
     <Box sx={{ maxWidth: "768px", margin: "auto", py: "20px", px: "10px"}}>
@@ -21,37 +28,8 @@ const IndividualDeliberation = () => {
       
       <IntegrateLink link={'<script async src="https://pol.is/embed.js"></script>'}></IntegrateLink>
 
-      <Box sx={{ 
-        backgroundColor: '#F1F1F1',
-        borderRadius: '8px',
-        padding: '15px',
-        position: 'relative',
-        color: 'black',
-        mb: [4],
-        
-      }}>
-         <h2 style={{marginTop: '0px', marginBottom: '10px',}}>Integration Description</h2>
-        <Box sx={{ 
-            display: 'flex', 
-            justifyContent: 'space-between',
-            alignItems: 'inital',
-          }}>
-            <Box sx={{ marginRight: '10px' }}>
-              <div>{`Copy and paste this code into your content management template. Each page (article, post) requires a unique string in the "PAGE_ID" field. This should be consistent over time and unique to each of your pages (like the article title).`}</div>
-             <ul>
-                <li>When this embed code loads on your website, it will either create a new conversation (if one is not already associated with the string passed into PAGE_ID) or load an existing conversation.
-                </li>
-                <li>This embed code will keep track of what conversations belongs on what pages via the data-page_id HTML attribute.</li>
-                <li>Simply replace "PAGE_ID", either manually or in your templates, to create new conversations and load existing ones in the right place.</li>
-             </ul>
-            </Box>
-        
-          </Box>
-      </Box>
+      <IntegrateBox heading={"Integration Description"} description={'Copy and paste this code into your content management template. Each page (article, post) requires a unique string in the "PAGE_ID" field. This should be consistent over time and unique to each of your pages (like the article title).'} items={items}></IntegrateBox>
 
-  
-{/* 
-     <Integrate></Integrate> */}
     </Box>
   );
 };
---
+++ client-deliberation/src/components/IntegrateBox.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box } from "theme-ui";
+
+const IntegrateDescription = ({ heading, description, items }) => {
+  return (
+    <Box sx={{ 
+        backgroundColor: '#F1F1F1',
+        borderRadius: '8px',
+        padding: '15px',
+        position: 'relative',
+        color: 'black',
+        mb: [4],
+      }}>
+         <h2 style={{marginTop: '0px', marginBottom: '10px'}}>{heading}</h2>
+        <Box sx={{ 
+            display: 'flex', 
+            justifyContent: 'space-between',
+            alignItems: 'initial',
+          }}>
+            <Box sx={{ marginRight: '10px' }}>
+              <div>{description}</div>
+              <ul>
+                {items.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </Box>
+          </Box>
+      </Box>
+  );
+};
+
+export default IntegrateDescription;
