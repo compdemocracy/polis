add306202bca0f562f89286adb2c34b1b4f8e6f0
Liz Barry <ebarry@gmail.com>
2024-02-26 19:44:45 -0800

    Create ar.js

---
+++ client-participation/js/strings/ar.js
@@ -0,0 +1,75 @@
+// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var s = {};
+
+// Text on the card
+
+s.participantHelpWelcomeText = "أهلاً بكم في نوع جديد من من المحادثات - <em>صوّت</em>على إفادات الأشخاص الآخرين.";
+s.agree = "	أوافق";
+s.disagree = "لا أوافق";
+s.pass = "غير متأكد / تجاوز التصويت";
+s.anonPerson = "	مجهول	";
+s.x_wrote = "كتب	:";
+s.comments_remaining = "{{num_comments}} 	متبقي	";
+
+// Text about writing your own statement
+
+s.writeCommentHelpText = "هل تعتقد أن آرائك أو تجربتك غير موجودة في هذه المحادثة ، 				? إذا كان الأمر كذلك	 <b>		قم بإضافتها</b>			ضمن هذا المربع		.";
+s.helpWriteListIntro = "	ما الذي يجعل الإفادة جيدة?";
+s.helpWriteListStandalone = "		فكرة مستقلة بذاتها";
+s.helpWriteListRaisNew = "تطرح وجهات نظر أو تجارب أو مشاكل جديدة";
+s.helpWriteListShort = "			بوضوح وإيجاز  (		أقل من ١٤٠ حرف	)";
+s.tipCommentsRandom = "الرجاء الانتباه, الإفادات تظهر بشكل عشوائي وأنت لا ترد بشكل مباشر على إفادات المشاركين الآخرين .";
+s.writePrompt = "	شارك وجهة نظرك...";
+s.submitComment = "أرسل";
+
+// Error notices
+
+s.commentSendFailed = "حدث خطأ في إرسال إفادتك.";
+s.commentSendFailedEmpty = "حدث خطأ في إرسال إفادتك	- 	الإفادة يجب ألا تكون فارغة	.";
+s.commentSendFailedTooLong = "	حدث خطأ في إرسال إفادتك	 - 	الإفادة أطول مما يجب	.";
+s.commentSendFailedDuplicate = "	حدث خطأ في إرسال إفادتك	 - 	تم إرسال إفادة مطابقة سابقاً	.";
+s.commentErrorDuplicate = "	مكرّر! 	هذه الإفادة موجودة سابقاً	.";
+s.commentErrorConversationClosed = "	تم إغلاق المحادثة	. 		 لا يمكن إرسال أي إفادات 	.";
+s.commentIsEmpty = "	الإفادة فارغة";
+s.commentIsTooLong = "الإفادة أطول مما يجب	";
+
+// For the visualization below
+
+s.group_123 = "مجموعة:";
+s.comment_123 = "إفادة :";
+s.majorityOpinion = "رأي الأغلبية ";
+s.majorityOpinionShort = "الأغلبية";
+s.info = "معلومات";
+s.helpWhatAmISeeingTitle = "ماذا أرى؟ ";
+s.helpWhatAmISeeing ="تم وضع الأشخاص المتقاربين في التصويت ضمن مجموعات. اضغط على مجموعة لرؤية وجهة النظر التي يتشاركونها. ";
+s.helpWhatDoIDoTitle = "ماذا أفعل؟";
+s.helpWhatDoIDo = ".صوت على إفادات الأشخاص الآخرين، اضغط "موافق" أو "غير موافق". اكتب إفادة (كل إفادة لفكرة واحدة). ادع أصدقاءك للمحادثة";
+s.heresHowGroupVoted = "المجموعة رقم  {{GROUP_NUMBER}} صوّتت على الشكل التالي:";
+s.one_person = "{{x}} شخص;"
+s.x_people = "{{x}} أشخاص";
+s.acrossAllPtpts = ":ضمن جميع المشاركين";
+s.xPtptsSawThisComment = "شاهدوا هذه الإفادة ";
+s.xOfThoseAgreed = "من هؤلاء المشاركين وافقوا";
+s.xOfthoseDisagreed = "من هؤلاء المشاركين لم يوافقوا";
+s.opinionGroups = "مجموعات الرأي ";
+s.topComments = "الإفادات الأولى";
+s.divisiveComments = "الإفادات الخلافية";
+s.pctAgreed = "{{pct}}% وافق";
+s.pctDisagreed = "{{pct}}% لم يوافقوا";
+s.pctAgreedLong = "{{pct}}% من جميع المشاركين صوتوا على الإفادة {{comment_id}} وافقوا";
+s.pctAgreedOfGroup = "{{pct}}% من مجموعة  {{group}}  وافقوا ";
+s.pctDisagreedOfGroup = "{{pct}}% من مجموعة {{group}} لم يوافقوا";
+s.pctDisagreedLong = "{{pct}}% من جميع المشاركين صوتوا على الإفادة {{comment_id}} غير موافقين";
+s.pctAgreedOfGroupLong = "{{pct}}% من مجموعة {{group}} الذين صوتوا على إفادة {{comment_id}} وافقوا";
+s.pctDisagreedOfGroupLong = "{{pct}}% من مجموعة  {{group}} الذين صوتوا على هذه الإفادة  {{comment_id}} وافقوا";
+
+// Footer
+
+s.privacy = "الخصوصية";
+s.TOS = "شروط الخدمة"; 
+  
+
+
+
+
