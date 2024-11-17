2bc01e8988d01e954c4ddc2bed332991ce04ff22
tevko <tim@devzero.io>
2024-11-17 15:05:41 -0600

    add basic reports test

--- e2e/cypress/e2e/client-admin/reports.cy.js
+++ e2e/cypress/e2e/client-admin/reports.cy.js
@@ -1,5 +1,31 @@
 describe('Reports', function () {
   beforeEach(function () {
+    // Disable matrix functionality to avoid assertExists errors
+    cy.intercept('GET', '/api/v3/math/pca2*', {
+      statusCode: 200,
+      body: {
+        'base-clusters': [],
+        consensus: {},
+        'group-aware-consensus': {},
+        'group-clusters': [],
+        'group-votes': [],
+        'n-cmts': 0,
+        'user-vote-counts': [],
+        'votes-base': [],
+        center: [],
+        'comment-extremity': [],
+        'comment-projection': [],
+        comps: [],
+        repness: [],
+        tids: [],
+        pca: {
+          center: [],
+          'comment-extremity': [],
+          'comment-projection': [],
+          comps: [],
+        },
+      }, // or minimal math data structure if needed
+    }).as('getMath')
     cy.intercept('GET', '/api/v3/conversations*').as('getConversations')
     cy.intercept('GET', '/api/v3/reports*').as('getReports')
     cy.intercept('GET', '/api/v3/comments*').as('getComments')
@@ -16,7 +42,10 @@ describe('Reports', function () {
       // pause for ten seconds to allow the button to be visible
       cy.pause()
       cy.contains('button', 'Create report url').click()
-      cy.get('a').should('exist')
+      cy.get('a[href*="/report/"]').then(($link) => {
+        cy.visit($link.attr('href'))
+        cy.get('[data-testid="reports-overview"]').should('exist')
+      })
     })
   })
 })
