99c86b4addc5fcec7c958bf8a057acd603339bd7
Julien Cornebise <julien@cornebise.com>
2025-01-31 09:36:03 +0000

    Makefile for the tests and support partial runs
    The makefile will avoid having to remember the test syntax, making them
    frictionless to run.
    The partial runs are a way to keep focusing on just the output of
    whichever part we want -- although it doesn't save the biggest
    drag: clojure launch time.

--- math/test/test_runner.clj
+++ math/test/test_runner.clj
@@ -12,25 +12,45 @@
             [utils-test]
             [ptpt-stats-test]
             [pythonport-test]
-            [clojure.test :as test]))
+            [clojure.test :as test]
+            [clojure.string :as str]))
 
+(def all-test-namespaces
+  '[cluster-tests
+    conversation-test
+    index-hash-test
+    named-matrix-test
+    pca-test
+    silhouette-test
+    stats-test
+    utils-test
+    ptpt-stats-test
+    pythonport-test])
+
+(defn parse-test-names
+  "Convert command line args into test namespace symbols.
+   Each arg should be the name of a test namespace without the '-test' suffix.
+   e.g., 'utils' will run utils-test"
+  [args]
+  (if (empty? args)
+    all-test-namespaces
+    (mapv #(symbol (str % "-test")) args)))
 
 (defn -main
-  "Run all the pure tests for polisapp. The one integration test is in conv-man-tests, and should be run separately (and
-  needs to be cleaned up to run on a separate poller system)"
-  []
-  (apply
-    test/run-tests
-    '[cluster-tests
-      conversation-test
-      index-hash-test
-      named-matrix-test
-      pca-test
-      silhouette-test
-      stats-test
-      utils-test
-      ptpt-stats-test
-      pythonport-test]))
+  "Run tests for polisapp. If no arguments are provided, runs all tests.
+   Otherwise, runs only the specified test namespaces.
+   
+   Usage:
+     clojure -M -m test-runner          # run all tests
+     clojure -M -m test-runner utils    # run only utils-test
+     clojure -M -m test-runner utils pythonport  # run utils-test and pythonport-test
+   
+   Note: The integration test in conv-man-tests should be run separately as it
+   needs to be cleaned up to run on a separate poller system."
+  [& args]
+  (let [test-namespaces (parse-test-names args)]
+    (println "Running tests:" (str/join ", " test-namespaces))
+    (apply test/run-tests test-namespaces)))
 
 ;(-main)
 ;(test/run-tests 'conversation-test)
