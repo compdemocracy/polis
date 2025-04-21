4aba975a558480e602f19ab0174b55abb4874628
Jake <mubok0212@gmail.com>
2025-04-21 18:10:47 +0900

    add ko.js

---
+++ client-participation/js/strings/ko.js
@@ -0,0 +1,203 @@
+// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var s = {};
+
+// Text on the card
+
+s.participantHelpWelcomeText =
+  "새로운 종류의 대화에 오신 것을 환영합니다 — </b>투표</b>하세요 다른 사람들의 의견에 — </b>많을수록 좋습니다.</b>";
+
+s.agree = "찬성";
+s.disagree = "반대";
+s.pass = "패스 / 불확실";
+
+s.writePrompt ="당신의 관점을 공유하세요 (답장하지 않고 — 독립적인 의견을 제출하세요)";
+s.anonPerson = "익명";
+s.importantCheckbox = "중요/의미있는";
+s.importantCheckboxDesc =
+  "이 의견이 당신에게 특히 중요하거나 대화와 매우 관련이 있다고 생각하면 이 상자를 체크하세요. 투표와 관계없이 대화 분석에서 다른 투표에 비해 이 의견에 더 높은 우선순위를 부여합니다.";
+s.howImportantPrompt = "이 의견이 얼마나 중요한가요?";
+s.howImportantLow = "낮음";
+s.howImportantMedium = "중간";
+s.howImportantHigh = "높음";
+
+s.modSpam = "스팸";
+s.modOffTopic = "주제에서 벗어남";
+s.modImportant = "중요";
+s.modSubmitInitialState = "건너뛰기 (위의 것 중 하나도 아님), 다음 의견";
+s.modSubmit = "완료, 다음 의견";
+
+s.x_wrote = "작성:";
+s.x_tweeted = "트윗:";
+s.comments_remaining = "{{num_comments}}개 남음";
+s.comments_remaining2 = "{{num_comments}}개의 남은 의견";
+
+// Text about phasing
+
+s.noCommentsYet = "아직 의견이 없습니다.";
+s.noCommentsYetSoWrite = "의견을 추가하여 이 대화를 시작하세요.";
+s.noCommentsYetSoInvite =
+  "더 많은 참가자를 초대하거나 의견을 추가하여 이 대화를 시작하세요.";
+s.noCommentsYouVotedOnAll = "모든 의견에 투표하셨습니다.";
+s.noCommentsTryWritingOne =
+  "추가하고 싶은 것이 있다면, 자신의 의견을 작성해 보세요.";
+s.convIsClosed = "이 대화는 종료되었습니다.";
+s.noMoreVotingAllowed = "더 이상의 투표는 허용되지 않습니다.";
+
+// For the visualization below
+
+s.group_123 = "그룹:";
+s.comment_123 = "의견:";
+s.majorityOpinion = "다수 의견";
+s.majorityOpinionShort = "다수";
+s.info = "정보";
+
+
+s.helpWhatAmISeeingTitle = "무엇을 보고 있나요?";
+s.helpWhatAmISeeing =
+  "당신은 파란색 원으로 표현되며, 당신의 관점을 공유하는 다른 사람들과 함께 그룹화됩니다.";
+s.heresHowGroupVoted = "그룹 {{GROUP_NUMBER}}의 투표 결과입니다:";
+s.one_person = "{{x}}명";
+s.x_people = "{{x}}명";
+s.acrossAllPtpts = "모든 참가자 중:";
+s.xPtptsSawThisComment = "명이 이 의견을 보았습니다";
+s.xOfThoseAgreed = "명의 참가자가 찬성했습니다";
+s.xOfthoseDisagreed = "명의 참가자가 반대했습니다";
+s.opinionGroups = "의견 그룹";
+s.topComments = "상위 의견";
+s.divisiveComments = "분열적인 의견";
+s.pctAgreed = "{{pct}}% 찬성";
+s.pctDisagreed = "{{pct}}% 반대";
+s.pctAgreedLong =
+  "의견 {{comment_id}}에 투표한 모든 사람 중 {{pct}}%가 찬성했습니다.";
+s.pctAgreedOfGroup = "그룹 {{group}}의 {{pct}}%가 찬성했습니다";
+s.pctDisagreedOfGroup = "그룹 {{group}}의 {{pct}}%가 반대했습니다";
+s.pctDisagreedLong =
+  "의견 {{comment_id}}에 투표한 모든 사람 중 {{pct}}%가 반대했습니다.";
+s.pctAgreedOfGroupLong =
+  "그룹 {{group}}에서 의견 {{comment_id}}에 투표한 사람 중 {{pct}}%가 찬성했습니다.";
+s.pctDisagreedOfGroupLong =
+  "그룹 {{group}}에서 의견 {{comment_id}}에 투표한 사람 중 {{pct}}%가 반대했습니다.";
+s.participantHelpGroupsText =
+  "당신은 파란색 원으로 표현되며, 당신의 관점을 공유하는 다른 사람들과 함께 그룹화됩니다.";
+s.participantHelpGroupsNotYetText =
+  "7명의 참가자가 투표를 시작하면 시각화가 나타납니다";
+s.helpWhatAreGroupsDetail =
+  "<p>당신의 그룹이나 다른 그룹을 클릭하여 각 그룹의 의견을 탐색하세요.</p><p>다수 의견은 그룹 간에 가장 널리 공유되는 의견입니다.</p>";
+
+// Text about writing your own statement
+
+s.helpWhatDoIDoTitle = " 무엇을 해야 하나요?";
+s.helpWhatDoIDo =
+  "'찬성' 또는 '반대'를 클릭하여 다른 사람들의 의견에 투표하세요. 의견을 작성하세요 (각각 하나의 아이디어로 유지하세요). 친구들을 대화에 초대하세요!";
+s.writeCommentHelpText =
+  "당신의 관점이나 경험이 대화에서 빠져 있나요? 그렇다면 아래 상자에 </b>추가하세요</b> — </b>한 번에 하나씩</b>.";
+s.helpWriteListIntro = "좋은 의견이란 무엇인가요?";
+s.helpWriteListStandalone = "독립적인 아이디어";
+s.helpWriteListRaisNew = "새로운 관점, 경험 또는 이슈";
+s.helpWriteListShort = "명확하고 간결한 표현 (140자로 제한)";
+s.tip = "팁:";
+s.commentWritingTipsHintsHeader = "의견 작성 팁";
+s.tipCharLimit = "의견은 {{char_limit}}자로 제한됩니다.";
+s.tipCommentsRandom =
+  "의견은 무작위로 표시되며, 다른 사람들의 의견에 직접 답장하지 않습니다: <b>당신은 독립적인 의견을 추가하고 있습니다.<b>";
+s.tipOneIdea =
+  "여러 아이디어를 포함하는 긴 의견을 나누세요. 이렇게 하면 다른 사람들이 당신의 의견에 투표하기 쉬워집니다.";
+s.tipNoQuestions =
+  "의견은 질문 형태가 아니어야 합니다. 참가자들은 당신이 제시한 의견에 찬성하거나 반대할 것입니다.";
+s.commentTooLongByChars =
+  "의견 길이 제한을 {{CHARACTERS_COUNT}}자 초과했습니다.";
+s.submitComment = "제출";
+s.commentSent =
+  "의견이 제출되었습니다! 다른 참가자들만 당신의 의견을 보고 찬성하거나 반대할 수 있습니다.";
+
+// Error notices
+
+s.commentSendFailed = "의견을 제출하는 중 오류가 발생했습니다.";
+s.commentSendFailedEmpty =
+  "의견을 제출하는 중 오류가 발생했습니다 - 의견은 비어있지 않아야 합니다.";
+s.commentSendFailedTooLong =
+  "의견을 제출하는 중 오류가 발생했습니다 - 의견이 너무 깁니다.";
+s.commentSendFailedDuplicate =
+  "의견을 제출하는 중 오류가 발생했습니다 - 동일한 의견이 이미 존재합니다.";
+s.commentErrorDuplicate = "중복! 해당 의견이 이미 존재합니다.";
+s.commentErrorConversationClosed =
+  "이 대화는 종료되었습니다. 더 이상의 의견을 제출할 수 없습니다.";
+s.commentIsEmpty = "의견이 비어있습니다";
+s.commentIsTooLong = "의견이 너무 깁니다";
+s.hereIsNextStatement = "투표 성공. 위로 이동하여 다음 의견을 확인하세요.";
+
+// Text about connecting identity
+
+s.connectFacebook = "페이스북 연결";
+s.connectTwitter = "트위터 연결";
+s.connectToPostPrompt =
+  "의견을 제출하려면 신원을 연결하세요. 타임라인에 게시하지 않습니다.";
+s.connectToVotePrompt =
+  "투표하려면 신원을 연결하세요. 타임라인에 게시하지 않습니다.";
+s.socialConnectPrompt =
+  "시각화에서 친구와 팔로우하는 사람들을 보려면 선택적으로 연결하세요.";
+s.connectFbButton = "페이스북으로 연결";
+s.connectTwButton = "트위터로 연결";
+s.polis_err_reg_fb_verification_email_sent =
+  "이메일에서 확인 링크를 확인한 후 여기로 돌아와 계속하세요.";
+s.polis_err_reg_fb_verification_noemail_unverified =
+  "페이스북 계정이 확인되지 않았습니다. 페이스북에서 이메일 주소를 확인한 후 여기로 돌아와 계속하세요.";
+
+// Text for the third party translation that appears on the cards
+
+s.showTranslationButton = "서드파티 번역 활성화";
+s.hideTranslationButton = "번역 비활성화";
+s.thirdPartyTranslationDisclaimer = "서드파티에서 제공한 번역";
+
+// Text about notifications and subscriptions and embedding
+
+s.notificationsAlreadySubscribed =
+  "이 대화의 업데이트에 이미 구독하셨습니다.";
+s.notificationsGetNotified = "더 많은 의견이 도착하면 알림을 받으세요:";
+s.notificationsEnterEmail =
+  "더 많은 의견이 도착하면 알림을 받으려면 이메일 주소를 입력하세요:";
+s.labelEmail = "이메일";
+s.notificationsSubscribeButton = "구독";
+s.notificationsSubscribeErrorAlert = "구독 중 오류 발생";
+
+s.addPolisToYourSite =
+  "<img style='height: 20px; margin: 0px 4px;' src='{{URL}}'/>";
+
+// Footer
+
+s.privacy = "개인정보";
+s.TOS = "이용약관";
+
+// Experimental features
+
+s.importantCheckbox = "이 의견은 중요합니다";
+s.howImportantPrompt = "이 의견이 얼마나 중요한가요?";
+s.howImportantLow = "낮음";
+s.howImportantMedium = "중간";
+s.howImportantHigh = "높음";
+s.tipStarred = "중요로 표시됨.";
+
+s.modSpam = "스팸";
+s.modOffTopic = "주제에서 벗어남";
+s.modImportant = "중요";
+s.modSubmitInitialState = "건너뛰기 (위의 것 중 하나도 아님), 다음 의견";
+s.modSubmit = "완료, 다음 의견";
+
+s.topic_good_01 = "탁구실에 대해 어떻게 해야 할까요?";
+s.topic_good_01_reason =
+  "개방형 질문으로, 누구나 이 질문에 대한 답에 의견을 가질 수 있습니다";
+s.topic_good_02 = "새로운 제안에 대해 어떻게 생각하시나요?";
+s.topic_good_02_reason =
+  "개방형 질문으로, 누구나 이 질문에 대한 답에 의견을 가질 수 있습니다";
+s.topic_good_03 = "생산성을 저하시키는 것이 있다고 생각하시나요?";
+
+s.topic_bad_01 = "모두 출시 준비 상태를 보고하세요";
+s.topic_bad_01_reason =
+  "다양한 팀의 사람들이 응답에 투표할 것이지만, 자신 있게 투표할 충분한 지식이 없을 수 있습니다.";
+s.topic_bad_02 = "출시 차단 요소는 무엇인가요?";
+s.topic_bad_02_reason = "";
+
+module.exports = s;
+
+
