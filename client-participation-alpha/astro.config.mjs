6cf22665727f26b26c2411fd92457a267b4695d3
tevko <tim@devzero.io>
2025-07-08 21:36:57 -0500

    ssr

--- client-participation-alpha/astro.config.mjs
+++ client-participation-alpha/astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  
   integrations: [react()]
 });
