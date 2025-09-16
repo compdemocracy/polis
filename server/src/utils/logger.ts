23c563299038b63b27e4e6030df08461c54b509f
Bennie Rosas <ben@aliencyb.org>
2025-09-16 17:32:35 -0500

    devMode convenience var

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,6 +1,7 @@
 import { createLogger, format, transports } from "winston";
 import Config from "../config";
 
+const devMode = Config.isDevMode;
 // See https://github.com/winstonjs/winston#logging-levels
 const logLevel = Config.logLevel || "warn";
 const logToFile = Config.logToFile;
@@ -9,7 +10,7 @@ const logToFile = Config.logToFile;
 // - In dev, emit colorful, human-readable logs
 // - In prod, emit structured JSON logs friendly to Datadog
 const consoleTransport = new transports.Console({
-  format: Config.isDevMode
+  format: devMode
     ? format.combine(format.colorize(), format.simple())
     : format.combine(
         format.uncolorize(),
